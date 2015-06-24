73102638dfda7ab2ad4b82a224cd8aefe03b6a10
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:31:09 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.fragment.test/src/org/eclipse/chemclipse/msd/converter/supplier/matlab/parafac/converter/MatlabParafacPeakImportConverter_8_ITest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.converter;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import org.eclipse.chemclipse.model.core.IPeaks;
+import org.eclipse.chemclipse.msd.converter.processing.peak.IPeakImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.TestPathHelper;
+
+import junit.framework.TestCase;
+
+public class MatlabParafacPeakImportConverter_8_ITest extends TestCase {
+
+	private IPeaks peaks;
+	private IPeakImportConverterProcessingInfo processingInfo;
+	private MatlabParafacPeakImportConverter converter;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		converter = new MatlabParafacPeakImportConverter();
+		File file = new File(TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_PARAFAC_TEST_4));
+		processingInfo = converter.convert(file, new NullProgressMonitor());
+		peaks = processingInfo.getPeaks();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		super.tearDown();
+	}
+
+	public void testImport_1() {
+
+		assertEquals(2, peaks.size());
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac/src/org/eclipse/chemclipse/msd/converter/supplier/matlab/parafac/converter/MatlabParafacPeakImportConverter.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.converter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.converter.exceptions.FileIsEmptyException;
+import org.eclipse.chemclipse.converter.exceptions.FileIsNotReadableException;
+import org.eclipse.chemclipse.msd.converter.io.IPeakReader;
+import org.eclipse.chemclipse.msd.converter.peak.AbstractPeakImportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.peak.IPeakImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.processing.peak.PeakImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.internal.converter.IConstants;
+import org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.internal.converter.SpecificationValidator;
+import org.eclipse.chemclipse.msd.converter.supplier.matlab.parafac.io.MatlabParafacPeakReader;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.processing.core.IProcessingMessage;
+import org.eclipse.chemclipse.processing.core.MessageType;
+import org.eclipse.chemclipse.processing.core.ProcessingMessage;
+import org.eclipse.chemclipse.processing.core.exceptions.TypeCastException;
+
+public class MatlabParafacPeakImportConverter extends AbstractPeakImportConverter {
+
+	private static final Logger logger = Logger.getLogger(MatlabParafacPeakImportConverter.class);
+
+	@Override
+	public IPeakImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		IProcessingMessage processingMessage;
+		IPeakImportConverterProcessingInfo processingInfo = new PeakImportConverterProcessingInfo();
+		try {
+			super.validate(file);
+			file = SpecificationValidator.validateSpecification(file);
+			IPeakReader peakReader = new MatlabParafacPeakReader();
+			IPeakImportConverterProcessingInfo processingInfoReader = peakReader.read(file, monitor);
+			processingInfo.addMessages(processingInfoReader);
+			try {
+				processingInfo.setPeaks(processingInfoReader.getPeaks());
+			} catch(TypeCastException e) {
+				logger.warn(e);
+			}
+			//
+		} catch(FileNotFoundException e) {
+			logger.warn(e);
+			processingMessage = new ProcessingMessage(MessageType.ERROR, IConstants.PROCESS_INFO_DESCRIPTION, "The given file was not found: " + file.getAbsolutePath());
+			processingInfo.addMessage(processingMessage);
+		} catch(FileIsNotReadableException e) {
+			logger.warn(e);
+			processingMessage = new ProcessingMessage(MessageType.ERROR, IConstants.PROCESS_INFO_DESCRIPTION, "The given file is not readable: " + file.getAbsolutePath());
+			processingInfo.addMessage(processingMessage);
+		} catch(FileIsEmptyException e) {
+			logger.warn(e);
+			processingMessage = new ProcessingMessage(MessageType.ERROR, IConstants.PROCESS_INFO_DESCRIPTION, "The given file is empty: " + file.getAbsolutePath());
+			processingInfo.addMessage(processingMessage);
+		} catch(IOException e) {
+			logger.warn(e);
+			processingMessage = new ProcessingMessage(MessageType.ERROR, IConstants.PROCESS_INFO_DESCRIPTION, "There has gone something wrong reading the file: " + file.getAbsolutePath());
+			processingInfo.addMessage(processingMessage);
+		}
+		return processingInfo;
+	}
+}
