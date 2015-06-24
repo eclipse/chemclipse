a84bd16c379be7defa8ed911de6158f6373e8afd
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:23:08 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.massbank/src/org/eclipse/chemclipse/msd/converter/supplier/massbank/converter/MassBankExportConverter.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.massbank.converter;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.msd.converter.massspectrum.AbstractMassSpectrumExportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.massspectrum.IMassSpectrumExportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.model.core.IMassSpectra;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+
+public class MassBankExportConverter extends AbstractMassSpectrumExportConverter {
+
+	@Override
+	public IMassSpectrumExportConverterProcessingInfo convert(File file, IScanMSD massSpectrum, boolean append, IProgressMonitor monitor) {
+
+		return null;
+	}
+
+	@Override
+	public IMassSpectrumExportConverterProcessingInfo convert(File file, IMassSpectra massSpectra, boolean append, IProgressMonitor monitor) {
+
+		return null;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.massbank/src/org/eclipse/chemclipse/msd/converter/supplier/massbank/converter/MassBankImportConverter.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.massbank.converter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.converter.exceptions.FileIsEmptyException;
+import org.eclipse.chemclipse.converter.exceptions.FileIsNotReadableException;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.msd.converter.io.IMassSpectraReader;
+import org.eclipse.chemclipse.msd.converter.massspectrum.AbstractMassSpectrumImportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.massspectrum.IMassSpectrumImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.processing.massspectrum.MassSpectrumImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.supplier.massbank.internal.converter.SpecificationValidator;
+import org.eclipse.chemclipse.msd.converter.supplier.massbank.io.MassBankReader;
+import org.eclipse.chemclipse.msd.model.core.IMassSpectra;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+
+public class MassBankImportConverter extends AbstractMassSpectrumImportConverter {
+
+	private static final Logger logger = Logger.getLogger(MassBankImportConverter.class);
+	private static final String DESCRIPTION = "MassBank MassSpectrum Import";
+
+	@Override
+	public IMassSpectrumImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		IMassSpectrumImportConverterProcessingInfo processingInfo = new MassSpectrumImportConverterProcessingInfo();
+		/*
+		 * Checks if the file is null or empty ...
+		 */
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			try {
+				file = SpecificationValidator.validateSpecification(file);
+				IMassSpectraReader massSpectraReader = new MassBankReader();
+				IMassSpectra massSpectra = massSpectraReader.read(file, monitor);
+				processingInfo.setMassSpectra(massSpectra);
+			} catch(FileNotFoundException e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "The file couldn't be found: " + file.getAbsolutePath());
+			} catch(FileIsNotReadableException e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "The file is not readable: " + file.getAbsolutePath());
+			} catch(FileIsEmptyException e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "The file is empty: " + file.getAbsolutePath());
+			} catch(IOException e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has gone completely wrong: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+}
