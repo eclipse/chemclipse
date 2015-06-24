aab7bf29101fef37b7af5eefeab7ecb96c6186f3
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:19:57 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.openchromx.fragment.test/src/org/eclipse/chemclipse/msd/converter/supplier/openchromx/converter/ChromatogramExportConverter_1_ITest.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.openchromx.converter;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.model.history.EditInformation;
+import org.eclipse.chemclipse.msd.converter.chromatogram.ChromatogramConverterMSD;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.IChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.supplier.openchromx.TestPathHelper;
+import org.eclipse.chemclipse.msd.converter.supplier.openchromx.converter.ChromatogramExportConverter;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.exceptions.TypeCastException;
+
+import junit.framework.TestCase;
+
+public class ChromatogramExportConverter_1_ITest extends TestCase {
+
+	private IChromatogramMSD chromatogram;
+	private File importFile;
+	private File exportFile;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		importFile = new File(TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_1_AGILENT));
+		IChromatogramMSDImportConverterProcessingInfo processingInfo = ChromatogramConverterMSD.convert(importFile, new NullProgressMonitor());
+		chromatogram = processingInfo.getChromatogram();
+		chromatogram.setMiscInfo("Hello Test Misc Info");
+		chromatogram.getEditHistory().add(new EditInformation("I have done </Chromatogram> something"));
+		chromatogram.getEditHistory().add(new EditInformation("me too"));
+		exportFile = new File(TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_EXPORT_TEST));
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		chromatogram = null;
+		super.tearDown();
+	}
+
+	public void testExport_1() {
+
+		ChromatogramExportConverter converter = new ChromatogramExportConverter();
+		IChromatogramExportConverterProcessingInfo processingInfo = converter.convert(exportFile, chromatogram, new NullProgressMonitor());
+		try {
+			File file = processingInfo.getFile();
+			assertNotNull(file);
+		} catch(TypeCastException e) {
+			assertTrue("TypeCastException", false);
+		}
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.openchromx/src/org/eclipse/chemclipse/msd/converter/supplier/openchromx/converter/ChromatogramExportConverter.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.openchromx.converter;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
+import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
+import org.eclipse.chemclipse.msd.converter.supplier.openchromx.internal.converter.SpecificationValidator;
+import org.eclipse.chemclipse.msd.converter.supplier.openchromx.internal.support.IConstants;
+import org.eclipse.chemclipse.msd.converter.supplier.openchromx.io.ChromatogramWriter;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+
+public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter {
+
+	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
+	private static final String DESCRIPTION = "ChemClipseX Export Converter";
+
+	@Override
+	public IChromatogramExportConverterProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+
+		IChromatogramExportConverterProcessingInfo processingInfo = new ChromatogramExportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		file = SpecificationValidator.validateSpecification(file);
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		/*
+		 * Don't process if errors have occurred.
+		 */
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			monitor.subTask(IConstants.EXPORT_CHROMATOGRAM);
+			IChromatogramMSDWriter writer = new ChromatogramWriter();
+			try {
+				writer.writeChromatogram(file, chromatogram, monitor);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+			processingInfo.setFile(file);
+		}
+		return processingInfo;
+	}
+}
