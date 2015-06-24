5998f7464ecc6409807c10e3bf65d5cee547a256
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:22:57 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.animl/src/org/eclipse/chemclipse/msd/converter/supplier/animl/converter/ChromatogramExportConverter.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.animl.converter;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
+import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
+import org.eclipse.chemclipse.msd.converter.supplier.animl.internal.converter.IConstants;
+import org.eclipse.chemclipse.msd.converter.supplier.animl.internal.converter.SpecificationValidator;
+import org.eclipse.chemclipse.msd.converter.supplier.animl.io.ChromatogramWriter;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+
+public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter {
+
+	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
+
+	@Override
+	public IChromatogramExportConverterProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+
+		IChromatogramExportConverterProcessingInfo processingInfo = new ChromatogramExportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		file = SpecificationValidator.validateSpecification(file);
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		/*
+		 * Don't process if errors have occurred.
+		 */
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			IChromatogramMSDWriter writer = new ChromatogramWriter();
+			monitor.subTask(IConstants.EXPORT_CHROMATOGRAM);
+			try {
+				writer.writeChromatogram(file, chromatogram, monitor);
+				processingInfo.setFile(file);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(IConstants.EXPORT_CHROMATOGRAM, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.animl/src/org/eclipse/chemclipse/msd/converter/supplier/animl/converter/ChromatogramImportConverter.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.animl.converter;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.ChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.IChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.supplier.animl.internal.converter.IConstants;
+
+public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter {
+
+	@Override
+	public IChromatogramMSDImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		IChromatogramMSDImportConverterProcessingInfo processingInfo = new ChromatogramMSDImportConverterProcessingInfo();
+		processingInfo.addErrorMessage(IConstants.IMPORT_CHROMATOGRAM, "It's not possible to import AnIML chromatograms yet.");
+		return processingInfo;
+	}
+
+	@Override
+	public IChromatogramOverviewImportConverterProcessingInfo convertOverview(File file, IProgressMonitor monitor) {
+
+		IChromatogramOverviewImportConverterProcessingInfo processingInfo = new ChromatogramOverviewImportConverterProcessingInfo();
+		processingInfo.addErrorMessage(IConstants.IMPORT_CHROMATOGRAM, "It's not possible to import AnIML chromatograms yet.");
+		return processingInfo;
+	}
+}
