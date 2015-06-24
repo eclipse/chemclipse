d5f9fd302bd9961a07fcea9ebb7ad423f6142816
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:36:53 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/xpass/core/HighPassMassSpectrumFilter.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.chemclipse.chromatogram.filter.result.ResultStatus;
+import org.eclipse.chemclipse.chromatogram.msd.filter.core.massspectrum.AbstractMassSpectrumFilter;
+import org.eclipse.chemclipse.chromatogram.msd.filter.processing.IMassSpectrumFilterProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.msd.filter.processing.MassSpectrumFilterProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.msd.filter.result.IMassSpectrumFilterResult;
+import org.eclipse.chemclipse.chromatogram.msd.filter.result.MassSpectrumFilterResult;
+import org.eclipse.chemclipse.chromatogram.msd.filter.settings.IMassSpectrumFilterSettings;
+import org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.preferences.PreferenceSupplier;
+import org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.settings.IXPassMassSpectrumFilterSettings;
+import org.eclipse.chemclipse.model.comparator.SortOrder;
+import org.eclipse.chemclipse.msd.model.core.IIon;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.msd.model.core.comparator.IonAbundanceComparator;
+import org.eclipse.chemclipse.processing.core.MessageType;
+import org.eclipse.chemclipse.processing.core.ProcessingMessage;
+
+public class HighPassMassSpectrumFilter extends AbstractMassSpectrumFilter {
+
+	private static final String DESCRIPTION = "High Pass Mass Spectrum Filter";
+
+	@Override
+	public IMassSpectrumFilterProcessingInfo applyFilter(List<IScanMSD> massSpectra, IMassSpectrumFilterSettings massSpectrumFilterSettings, IProgressMonitor monitor) {
+
+		IMassSpectrumFilterProcessingInfo processingInfo = new MassSpectrumFilterProcessingInfo();
+		processingInfo.addMessages(validate(massSpectra, massSpectrumFilterSettings));
+		if(processingInfo.hasErrorMessages()) {
+			return processingInfo;
+		}
+		/*
+		 * Apply the filter.
+		 */
+		if(massSpectrumFilterSettings instanceof IXPassMassSpectrumFilterSettings) {
+			//
+			IXPassMassSpectrumFilterSettings xpassMassSpectrumFilterSettings = (IXPassMassSpectrumFilterSettings)massSpectrumFilterSettings;
+			int numberHighest = xpassMassSpectrumFilterSettings.getNumberHighest();
+			//
+			for(IScanMSD massSpectrum : massSpectra) {
+				List<IIon> ions = massSpectrum.getIons();
+				Collections.sort(ions, new IonAbundanceComparator(SortOrder.DESC));
+				List<IIon> ionsToRemove = new ArrayList<IIon>();
+				int counter = 0;
+				for(IIon ion : ions) {
+					if(counter >= numberHighest) {
+						ionsToRemove.add(ion);
+					}
+					counter++;
+				}
+				//
+				ions.removeAll(ionsToRemove);
+			}
+			//
+			processingInfo.addMessage(new ProcessingMessage(MessageType.INFO, DESCRIPTION, "The mass spectrum has been optimized successfully."));
+		} else {
+			processingInfo.addErrorMessage(DESCRIPTION, "The filter settings instance is not a type of: " + IXPassMassSpectrumFilterSettings.class);
+		}
+		//
+		IMassSpectrumFilterResult massSpectrumFilterResult = new MassSpectrumFilterResult(ResultStatus.OK, "The high pass filter has been applied successfully.");
+		processingInfo.setMassSpectrumFilterResult(massSpectrumFilterResult);
+		return processingInfo;
+	}
+
+	@Override
+	public IMassSpectrumFilterProcessingInfo applyFilter(IScanMSD massSpectrum, IMassSpectrumFilterSettings massSpectrumFilterSettings, IProgressMonitor monitor) {
+
+		List<IScanMSD> massSpectra = new ArrayList<IScanMSD>();
+		massSpectra.add(massSpectrum);
+		return applyFilter(massSpectra, massSpectrumFilterSettings, monitor);
+	}
+
+	@Override
+	public IMassSpectrumFilterProcessingInfo applyFilter(IScanMSD massSpectrum, IProgressMonitor monitor) {
+
+		List<IScanMSD> massSpectra = new ArrayList<IScanMSD>();
+		massSpectra.add(massSpectrum);
+		IMassSpectrumFilterSettings massSpectrumFilterSettings = PreferenceSupplier.getMassSpectrumFilterSettings();
+		return applyFilter(massSpectra, massSpectrumFilterSettings, monitor);
+	}
+
+	@Override
+	public IMassSpectrumFilterProcessingInfo applyFilter(List<IScanMSD> massSpectra, IProgressMonitor monitor) {
+
+		IMassSpectrumFilterSettings massSpectrumFilterSettings = PreferenceSupplier.getMassSpectrumFilterSettings();
+		return applyFilter(massSpectra, massSpectrumFilterSettings, monitor);
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/xpass/settings/XPassMassSpectrumFilterSettings.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.settings;
+
+import org.eclipse.chemclipse.chromatogram.msd.filter.settings.AbstractMassSpectrumFilterSettings;
+
+public class XPassMassSpectrumFilterSettings extends AbstractMassSpectrumFilterSettings implements IXPassMassSpectrumFilterSettings {
+
+	private int numberHighest;
+	private int numberLowest;
+
+	@Override
+	public int getNumberHighest() {
+
+		return numberHighest;
+	}
+
+	@Override
+	public void setNumberHighest(int numberHighest) {
+
+		this.numberHighest = numberHighest;
+	}
+
+	@Override
+	public int getNumberLowest() {
+
+		return numberLowest;
+	}
+
+	@Override
+	public void setNumberLowest(int numberLowest) {
+
+		this.numberLowest = numberLowest;
+	}
+}
