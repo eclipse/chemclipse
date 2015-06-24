a495e49d34940e3ca86dcc30e18ad8f71f2e4df3
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:30:02 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.csd.filter.supplier.zeroset.ui/src/org/eclipse/chemclipse/chromatogram/csd/filter/supplier/zeroset/ui/handlers/FilterHandler.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.csd.filter.supplier.zeroset.ui.handlers;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Display;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+
+import org.eclipse.chemclipse.chromatogram.csd.filter.supplier.zeroset.ui.modifier.FilterModifier;
+import org.eclipse.chemclipse.csd.model.core.selection.IChromatogramSelectionCSD;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
+import org.eclipse.chemclipse.progress.core.InfoType;
+import org.eclipse.chemclipse.progress.core.StatusLineLogger;
+import org.eclipse.chemclipse.support.events.IChemClipseEvents;
+
+public class FilterHandler implements EventHandler {
+
+	private static final Logger logger = Logger.getLogger(FilterHandler.class);
+	private static IChromatogramSelection chromatogramSelection;
+
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part) {
+
+		/*
+		 * Run the handler.
+		 */
+		if(chromatogramSelection != null) {
+			final Display display = Display.getCurrent();
+			StatusLineLogger.setInfo(InfoType.MESSAGE, "Start CSD ZeroSet Filter");
+			/*
+			 * Do the operation.<br/> Open a progress monitor dialog.
+			 */
+			if(chromatogramSelection instanceof IChromatogramSelectionCSD) {
+				IChromatogramSelectionCSD chromatogramSelectionFID = (IChromatogramSelectionCSD)chromatogramSelection;
+				IRunnableWithProgress runnable = new FilterModifier(chromatogramSelectionFID);
+				ProgressMonitorDialog monitor = new ProgressMonitorDialog(display.getActiveShell());
+				try {
+					/*
+					 * Use true, true ... instead of false, true ... if the progress bar
+					 * should be shown in action.
+					 */
+					monitor.run(true, true, runnable);
+				} catch(InvocationTargetException e) {
+					logger.warn(e);
+				} catch(InterruptedException e) {
+					logger.warn(e);
+				}
+				StatusLineLogger.setInfo(InfoType.MESSAGE, "CSD ZeroSet Filter finished");
+			}
+		}
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+
+		if(event.getTopic().equals(IChemClipseEvents.TOPIC_CHROMATOGRAM_CSD_UPDATE_CHROMATOGRAM_SELECTION)) {
+			chromatogramSelection = (IChromatogramSelectionCSD)event.getProperty(IChemClipseEvents.PROPERTY_CHROMATOGRAM_SELECTION);
+		}
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.csd.filter.supplier.zeroset.ui/src/org/eclipse/chemclipse/chromatogram/csd/filter/supplier/zeroset/ui/modifier/FilterModifier.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.csd.filter.supplier.zeroset.ui.modifier;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+
+import org.eclipse.chemclipse.csd.model.core.selection.IChromatogramSelectionCSD;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IScan;
+
+public class FilterModifier implements IRunnableWithProgress {
+
+	private IChromatogramSelectionCSD chromatogramSelection;
+
+	public FilterModifier(IChromatogramSelectionCSD chromatogramSelection) {
+
+		this.chromatogramSelection = chromatogramSelection;
+	}
+
+	@Override
+	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+
+		/*
+		 * THIS IS A TEST CASE, PLEASE COMBINE WITH FILTER EXTENSION POINT!
+		 */
+		IChromatogram chromatogram = chromatogramSelection.getChromatogram();
+		float minSignal = chromatogram.getMinSignal();
+		if(minSignal < 0) {
+			float delta = minSignal * -1;
+			for(IScan scan : chromatogram.getScans()) {
+				float adjustedIntensity = scan.getTotalSignal() + delta;
+				scan.adjustTotalSignal(adjustedIntensity);
+			}
+		}
+		/*
+		 * Update the retention times.
+		 */
+		chromatogramSelection.reset();
+		chromatogramSelection.update(true);
+	}
+}
