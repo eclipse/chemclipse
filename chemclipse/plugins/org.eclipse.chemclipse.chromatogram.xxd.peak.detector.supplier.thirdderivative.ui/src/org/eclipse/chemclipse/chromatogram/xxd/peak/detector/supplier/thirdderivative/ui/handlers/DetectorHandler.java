f5b4743a092e95cf099fba294d5544344cc9899a
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:33:50 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.ui/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/thirdderivative/ui/handlers/DetectorHandler.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.ui.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
+import org.eclipse.chemclipse.progress.core.InfoType;
+import org.eclipse.chemclipse.progress.core.StatusLineLogger;
+import org.eclipse.chemclipse.rcp.app.ui.handlers.PerspectiveSwitchHandler;
+import org.eclipse.chemclipse.support.events.IChemClipseEvents;
+import org.eclipse.chemclipse.support.events.IPerspectiveAndViewIds;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+
+public class DetectorHandler implements EventHandler {
+
+	private static IChromatogramSelectionMSD chromatogramSelection;
+
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part) {
+
+		/*
+		 * Try to select and show the perspective and view.
+		 */
+		PerspectiveSwitchHandler.focusPerspectiveAndView(IPerspectiveAndViewIds.PERSPECTIVE_PEAKS_MSD, IPerspectiveAndViewIds.VIEW_PEAK_LIST_MSD);
+		/*
+		 * Get the actual cursor, create a new wait cursor and show the wait
+		 * cursor.<br/> Show the origin cursor when finished.<br/> Use the
+		 * Display.asyncExec instances to show the messages properly.<br/> I
+		 * really don't like the GUI stuff, maybe there is a smarter way to
+		 * inform the user.<br/> So, i thought also on Eclipse "jobs", but there
+		 * will be problems, when the operation method updates the
+		 * chromatogramSelection.<br/> A progress bar would be also applicable,
+		 * but the IProgressMonitorDialog (monitor.beginTask(TASK_NAME,
+		 * IProgressMonitor.UNKNOWN) didn't showed a progress under linux.
+		 */
+		if(chromatogramSelection != null) {
+			StatusLineLogger.setInfo(InfoType.MESSAGE, "Done: Peaks detected");
+		}
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+
+		if(event.getTopic().equals(IChemClipseEvents.TOPIC_CHROMATOGRAM_MSD_UPDATE_CHROMATOGRAM_SELECTION)) {
+			chromatogramSelection = (IChromatogramSelectionMSD)event.getProperty(IChemClipseEvents.PROPERTY_CHROMATOGRAM_SELECTION);
+		}
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/thirdderivative/settings/ThirdDerivativePeakDetectorSettings.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.settings;
+
+import org.eclipse.chemclipse.chromatogram.msd.peak.detector.settings.AbstractPeakDetectorMSDSettings;
+
+public class ThirdDerivativePeakDetectorSettings extends AbstractPeakDetectorMSDSettings implements IThirdDerivativePeakDetectorSettings {
+
+	private boolean includeBackground = false;
+	private float minimumSignalToNoiseRatio;
+
+	@Override
+	public boolean isIncludeBackground() {
+
+		return includeBackground;
+	}
+
+	@Override
+	public void setIncludeBackground(boolean includeBackground) {
+
+		this.includeBackground = includeBackground;
+	}
+
+	@Override
+	public float getMinimumSignalToNoiseRatio() {
+
+		return minimumSignalToNoiseRatio;
+	}
+
+	@Override
+	public void setMinimumSignalToNoiseRatio(float minimumSignalToNoiseRatio) {
+
+		this.minimumSignalToNoiseRatio = minimumSignalToNoiseRatio;
+	}
+}
