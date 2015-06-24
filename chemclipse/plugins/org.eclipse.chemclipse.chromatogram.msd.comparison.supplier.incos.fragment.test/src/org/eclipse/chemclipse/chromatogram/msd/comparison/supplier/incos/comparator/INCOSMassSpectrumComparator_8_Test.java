9517cd8a61b3fa75bd8dd9eb33767267cb60e140
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-06-24 10:22:42 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.incos.fragment.test/src/org/eclipse/chemclipse/chromatogram/msd/comparison/supplier/incos/comparator/INCOSMassSpectrumComparator_8_Test.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.incos.comparator;
+
+import org.eclipse.chemclipse.chromatogram.msd.comparison.processing.IMassSpectrumComparatorProcessingInfo;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.msd.model.core.identifier.massspectrum.IMassSpectrumComparisonResult;
+
+public class INCOSMassSpectrumComparator_8_Test extends MassSpectrumSetTestCase {
+
+	private INCOSMassSpectrumComparator comparator;
+	private IMassSpectrumComparatorProcessingInfo processingInfo;
+	private IMassSpectrumComparisonResult result;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		//
+		IScanMSD unknown = problemC1.getMassSpectrum();
+		IScanMSD reference = problemC2.getMassSpectrum();
+		//
+		comparator = new INCOSMassSpectrumComparator();
+		processingInfo = comparator.compare(unknown, reference);
+		result = processingInfo.getMassSpectrumComparisonResult();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		super.tearDown();
+	}
+
+	public void test1() {
+
+		assertFalse(processingInfo.hasErrorMessages());
+	}
+
+	public void test2() {
+
+		assertEquals(0.0f, result.getMatchFactor());
+	}
+
+	public void test3() {
+
+		assertEquals(0.0f, result.getReverseMatchFactor());
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.incos/src/org/eclipse/chemclipse/chromatogram/msd/comparison/supplier/incos/results/INCOSMassSpectrumComparisonResult.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.incos.results;
+
+import org.eclipse.chemclipse.msd.model.core.identifier.massspectrum.AbstractMassSpectrumComparisonResult;
+
+public class INCOSMassSpectrumComparisonResult extends AbstractMassSpectrumComparisonResult {
+
+	public INCOSMassSpectrumComparisonResult(float matchFactor, float reverseMatchFactor) {
+
+		super(matchFactor, reverseMatchFactor);
+	}
+}
