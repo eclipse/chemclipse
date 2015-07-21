2d58d05752ef285f19a50b97270277a1808e354d
Janos Binder <janos.binder@openchrom.net>
2015-07-21 18:07:20 +0200

    Added two files to the model

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/INamedScanMSD.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Janos Binder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.model.core;
+
+
+public interface INamedScanMSD extends IScanMSD {
+
+	String getSubstanceName();
+
+	void setSubstanceName(String substanceName);
+
+	String getOriginName();
+
+	void setOriginName(String originName);
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/NamedScanMSD.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Janos Binder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.model.core;
+
+import org.eclipse.chemclipse.msd.model.implementation.ScanMSD;
+
+public class NamedScanMSD extends ScanMSD implements INamedScanMSD {
+
+	private static final long serialVersionUID = -6525787873272881014L;
+	String substanceName;
+	String originName;
+
+	public NamedScanMSD() {
+
+		super();
+	}
+
+	@Override
+	public String getOriginName() {
+
+		return originName;
+	}
+
+	@Override
+	public void setOriginName(String originName) {
+
+		this.originName = originName;
+	}
+
+	@Override
+	public String getSubstanceName() {
+
+		return substanceName;
+	}
+
+	@Override
+	public void setSubstanceName(String substanceName) {
+
+		this.substanceName = substanceName;
+	}
+}
