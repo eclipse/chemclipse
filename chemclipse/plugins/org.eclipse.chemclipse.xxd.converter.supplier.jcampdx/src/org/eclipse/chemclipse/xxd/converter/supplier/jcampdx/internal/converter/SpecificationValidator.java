730cf9b020e3c3609717f67c6f43cd5a122c1988
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-08-04 08:38:42 +0200

    I've renamed the JcampDX plugin. It supports various formats, hence it's not restricted to MSD.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/xxd/converter/supplier/jcampdx/internal/converter/SpecificationValidator.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.xxd.converter.supplier.jcampdx.internal.converter;
+
+import java.io.File;
+
+public class SpecificationValidator {
+
+	/**
+	 * Use only static methods.
+	 */
+	private SpecificationValidator() {
+
+	}
+
+	/**
+	 * Validates the given chromatogram file.<br/>
+	 * If the file is denoted only by a directory path, /CHROMATOGRAM.JDX will
+	 * be added. E.g.: /home/user/chrom will be validated to
+	 * /home/user/chrom/CHROMATOGRAM.JDX
+	 * 
+	 * @param file
+	 */
+	public static File validateSpecification(File file) {
+
+		if(file == null) {
+			return null;
+		}
+		/*
+		 * Validate
+		 */
+		File validFile;
+		String path = file.getAbsolutePath().toUpperCase();
+		if(file.isDirectory()) {
+			validFile = new File(file.getAbsolutePath() + File.separator + "CHROMATOGRAM.JDX");
+		} else {
+			if(path.endsWith(".")) {
+				validFile = new File(file.getAbsolutePath() + "JDX");
+			} else if(!path.endsWith(".JDX")) {
+				validFile = new File(file.getAbsolutePath() + ".JDX");
+			} else {
+				validFile = file;
+			}
+		}
+		return validFile;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/xxd/converter/supplier/jcampdx/model/VendorIon.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2015 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.xxd.converter.supplier.jcampdx.model;
+
+import org.eclipse.chemclipse.model.exceptions.AbundanceLimitExceededException;
+import org.eclipse.chemclipse.msd.model.core.AbstractScanIon;
+import org.eclipse.chemclipse.msd.model.exceptions.IonLimitExceededException;
+
+public class VendorIon extends AbstractScanIon implements IVendorIon {
+
+	/**
+	 * Renew the serialVersionUID any time you have changed some fields or
+	 * methods.
+	 */
+	private static final long serialVersionUID = -5630600980417389698L;
+	// A max value for abundance
+	public static final float MIN_ABUNDANCE = 0.0f;
+	public static final float MAX_ABUNDANCE = Float.MAX_VALUE;
+	// A max value for ion of
+	public static final double MIN_ION = 1.0d;
+	public static final double MAX_ION = 20000.0d;
+
+	public VendorIon(double ion, boolean ignoreAbundanceLimit) throws IonLimitExceededException {
+
+		super(ion, ignoreAbundanceLimit);
+	}
+
+	public VendorIon(double ion, float abundance) throws AbundanceLimitExceededException, IonLimitExceededException {
+
+		super(ion, abundance);
+	}
+
+	public VendorIon(double ion) throws IonLimitExceededException {
+
+		super(ion);
+	}
+
+	@Override
+	public float getMinPossibleAbundanceValue() {
+
+		return MIN_ABUNDANCE;
+	}
+
+	@Override
+	public float getMaxPossibleAbundanceValue() {
+
+		return MAX_ABUNDANCE;
+	}
+
+	@Override
+	public double getMinPossibleIonValue() {
+
+		return MIN_ION;
+	}
+
+	@Override
+	public double getMaxPossibleIonValue() {
+
+		return MAX_ION;
+	}
+}
