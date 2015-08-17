0e2698934f349d110e317ea52364d4cf978a4f05
Philip Wenig <philip.wenig@openchrom.net>
2015-08-17 12:43:21 +0200

    I've added the core classes to import jdx libraries.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/msd/converter/supplier/jcampdx/converter/MassSpectrumImportConverter.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.jcampdx.converter;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.msd.converter.massspectrum.AbstractMassSpectrumImportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.massspectrum.IMassSpectrumImportConverterProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class MassSpectrumImportConverter extends AbstractMassSpectrumImportConverter {
+
+	@Override
+	public IMassSpectrumImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		return null;
+	}
+}
