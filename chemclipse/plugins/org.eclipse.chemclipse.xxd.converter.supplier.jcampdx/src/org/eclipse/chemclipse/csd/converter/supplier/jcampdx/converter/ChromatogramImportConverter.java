6c9454ae965bcf63c29e0f425fd9a2554cc846e0
Philip Wenig <philip.wenig@openchrom.net>
2015-08-17 19:29:35 +0200

    I've added the model and converter classes to import CSD files.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/csd/converter/supplier/jcampdx/converter/ChromatogramImportConverter.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.csd.converter.supplier.jcampdx.converter;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.support.IConstants;
+import org.eclipse.chemclipse.csd.converter.chromatogram.AbstractChromatogramCSDImportConverter;
+import org.eclipse.chemclipse.csd.converter.io.IChromatogramCSDReader;
+import org.eclipse.chemclipse.csd.converter.processing.chromatogram.ChromatogramCSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.csd.converter.processing.chromatogram.IChromatogramCSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.csd.converter.supplier.jcampdx.io.ChromatogramReader;
+import org.eclipse.chemclipse.csd.model.core.IChromatogramCSD;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.core.IChromatogramOverview;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.chemclipse.xxd.converter.supplier.jcampdx.internal.converter.SpecificationValidator;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramImportConverter extends AbstractChromatogramCSDImportConverter {
+
+	private static final Logger logger = Logger.getLogger(ChromatogramImportConverter.class);
+	private static final String DESCRIPTION = "JCAMP-DX Import Converter";
+
+	@Override
+	public IChromatogramCSDImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		IChromatogramCSDImportConverterProcessingInfo processingInfo = new ChromatogramCSDImportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			/*
+			 * Read the chromatogram.
+			 */
+			file = SpecificationValidator.validateSpecification(file);
+			IChromatogramCSDReader reader = new ChromatogramReader();
+			monitor.subTask(IConstants.IMPORT_CHROMATOGRAM);
+			try {
+				IChromatogramCSD chromatogram = reader.read(file, monitor);
+				processingInfo.setChromatogram(chromatogram);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+
+	@Override
+	public IChromatogramOverviewImportConverterProcessingInfo convertOverview(File file, IProgressMonitor monitor) {
+
+		IChromatogramOverviewImportConverterProcessingInfo processingInfo = new ChromatogramOverviewImportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			/*
+			 * Read the chromatogram overview.
+			 */
+			file = SpecificationValidator.validateSpecification(file);
+			IChromatogramCSDReader reader = new ChromatogramReader();
+			monitor.subTask(IConstants.IMPORT_CHROMATOGRAM_OVERVIEW);
+			try {
+				IChromatogramOverview chromatogramOverview = reader.readOverview(file, monitor);
+				processingInfo.setChromatogramOverview(chromatogramOverview);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/csd/converter/supplier/jcampdx/io/ChromatogramReader.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.csd.converter.supplier.jcampdx.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.eclipse.chemclipse.converter.exceptions.FileIsEmptyException;
+import org.eclipse.chemclipse.converter.exceptions.FileIsNotReadableException;
+import org.eclipse.chemclipse.csd.converter.io.AbstractChromatogramCSDReader;
+import org.eclipse.chemclipse.csd.model.core.IChromatogramCSD;
+import org.eclipse.chemclipse.model.core.IChromatogramOverview;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramReader extends AbstractChromatogramCSDReader {
+
+	@Override
+	public IChromatogramCSD read(File file, IProgressMonitor monitor) throws FileNotFoundException, FileIsNotReadableException, FileIsEmptyException, IOException {
+
+		return null;
+	}
+
+	@Override
+	public IChromatogramOverview readOverview(File file, IProgressMonitor monitor) throws FileNotFoundException, FileIsNotReadableException, FileIsEmptyException, IOException {
+
+		return null;
+	}
+}
