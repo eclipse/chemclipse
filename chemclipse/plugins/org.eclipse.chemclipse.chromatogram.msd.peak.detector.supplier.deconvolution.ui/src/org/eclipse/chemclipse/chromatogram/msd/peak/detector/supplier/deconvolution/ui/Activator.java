46fb144592c10cc41e0860fb0f95bbb416a04c83
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-09-10 17:56:44 +0200

    I've refactored the support classes.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.ui/src/org/eclipse/chemclipse/chromatogram/msd/peak/detector/supplier/deconvolution/ui/Activator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.ui/src/org/eclipse/chemclipse/chromatogram/msd/peak/detector/supplier/deconvolution/ui/Activator.java
@@ -12,7 +12,6 @@
 package org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.ui;
 
 import org.osgi.framework.BundleContext;
-
 import org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.support.ui.activator.AbstractActivatorUI;
 
