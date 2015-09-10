484a9d45e120b04093310710a388eb973f55a7ec
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-09-10 17:56:43 +0200

    I've refactored the support classes.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.ui/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/thirdderivative/ui/Activator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.ui/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/thirdderivative/ui/Activator.java
@@ -12,7 +12,6 @@
 package org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.ui;
 
 import org.osgi.framework.BundleContext;
-
 import org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.thirdderivative.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.support.ui.activator.AbstractActivatorUI;
 
