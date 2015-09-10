e280cccc73e08442b93b12dcbe3d5b5ec7920319
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-09-10 17:56:44 +0200

    I've refactored the support classes.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.ui/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/xpass/ui/Activator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.ui/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/xpass/ui/Activator.java
@@ -12,7 +12,6 @@
 package org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.ui;
 
 import org.osgi.framework.BundleContext;
-
 import org.eclipse.chemclipse.chromatogram.msd.filter.supplier.xpass.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.support.ui.activator.AbstractActivatorUI;
 
