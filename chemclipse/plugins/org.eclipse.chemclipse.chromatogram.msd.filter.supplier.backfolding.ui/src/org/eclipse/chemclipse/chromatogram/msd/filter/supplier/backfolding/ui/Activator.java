85f42cbc0b0d34adcdec10745db03252234a8756
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-09-10 17:56:44 +0200

    I've refactored the support classes.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding.ui/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/backfolding/ui/Activator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding.ui/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/backfolding/ui/Activator.java
@@ -12,7 +12,6 @@
 package org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding.ui;
 
 import org.osgi.framework.BundleContext;
-
 import org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.support.ui.activator.AbstractActivatorUI;
 
