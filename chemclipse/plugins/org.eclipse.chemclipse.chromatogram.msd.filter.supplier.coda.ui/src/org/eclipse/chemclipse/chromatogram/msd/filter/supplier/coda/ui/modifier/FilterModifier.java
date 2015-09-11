a3e638283350aa7467797d1797d0bebede7ff316
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-09-11 15:16:02 +0200

    Handling the feedback has been refactored.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.coda.ui/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/coda/ui/modifier/FilterModifier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.coda.ui/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/coda/ui/modifier/FilterModifier.java
@@ -13,10 +13,6 @@ package org.eclipse.chemclipse.chromatogram.msd.filter.supplier.coda.ui.modifier
 
 import java.lang.reflect.InvocationTargetException;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.swt.widgets.Display;
-
 import org.eclipse.chemclipse.chromatogram.filter.processing.IChromatogramFilterProcessingInfo;
 import org.eclipse.chemclipse.chromatogram.msd.filter.core.chromatogram.ChromatogramFilter;
 import org.eclipse.chemclipse.chromatogram.msd.filter.supplier.coda.preferences.PreferenceSupplier;
@@ -26,6 +22,8 @@ import org.eclipse.chemclipse.model.processor.AbstractChromatogramProcessor;
 import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
 import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
 import org.eclipse.chemclipse.processing.ui.support.ProcessingInfoViewSupport;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 
 public class FilterModifier extends AbstractChromatogramProcessor implements IRunnableWithProgress {
 
@@ -51,18 +49,7 @@ public class FilterModifier extends AbstractChromatogramProcessor implements IRu
 			 * Apply the filter.
 			 */
 			final IChromatogramFilterProcessingInfo processingInfo = ChromatogramFilter.applyFilter((IChromatogramSelectionMSD)chromatogramSelection, chromatogramFilterSettings, FILTER_ID, monitor);
-			Display.getDefault().asyncExec(new Runnable() {
-
-				@Override
-				public void run() {
-
-					/*
-					 * Show the processing view if error messages occurred.
-					 */
-					ProcessingInfoViewSupport.showErrorInfoReminder(processingInfo);
-					ProcessingInfoViewSupport.updateProcessingInfoView(processingInfo);
-				}
-			});
+			ProcessingInfoViewSupport.updateProcessingInfo(processingInfo, true);
 		}
 	}
 
