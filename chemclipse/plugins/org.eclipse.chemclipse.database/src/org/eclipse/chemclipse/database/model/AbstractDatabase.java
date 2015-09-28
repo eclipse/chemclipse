bb16088c24a641ff62921440289ea96db9d1855a
Janos Binder <janos.binder@openchrom.net>
2015-09-28 17:47:38 +0200

    Refactored database queries

--- chemclipse/plugins/org.eclipse.chemclipse.database/src/org/eclipse/chemclipse/database/model/AbstractDatabase.java
+++ chemclipse/plugins/org.eclipse.chemclipse.database/src/org/eclipse/chemclipse/database/model/AbstractDatabase.java
@@ -12,9 +12,13 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.database.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.chemclipse.database.documents.ILibraryDescriptionDocument;
 import org.eclipse.chemclipse.database.documents.LibraryDescriptionDocument;
@@ -25,6 +29,8 @@ import org.eclipse.chemclipse.support.settings.OperatingSystemUtils;
 
 import com.orientechnologies.orient.core.db.OPartitionedDatabasePool;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.intent.OIntent;
+import com.orientechnologies.orient.core.intent.OIntentMassiveRead;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
@@ -158,7 +164,7 @@ public abstract class AbstractDatabase implements IDatabase {
 	 * @param id
 	 * @return ODocument
 	 */
-	protected ODocument queryDocumentById(String className, long id) {
+	final protected ODocument queryDocumentById(String className, long id) {
 
 		return queryDocumentByIdOnSeparateConnection(className, id);
 		/*
@@ -173,9 +179,10 @@ public abstract class AbstractDatabase implements IDatabase {
 	 * 
 	 * @param className
 	 * @param id
+	 * @param dbtx
 	 * @return ODocument
 	 */
-	protected ODocument queryDocumentById(String className, long id, ODatabaseDocumentTx dbtx) {
+	final protected ODocument queryDocumentById(String className, long id, ODatabaseDocumentTx dbtx) {
 
 		if(countCluster(className.toLowerCase()) == 0) {
 			return null;
@@ -202,6 +209,162 @@ public abstract class AbstractDatabase implements IDatabase {
 		}
 	}
 
+	/**
+	 * Selects a Document in the current database by its class name and id.
+	 * 
+	 * @param className
+	 * @param id
+	 * @param dbtx
+	 * @param queryStrings
+	 * @return ODocument
+	 */
+	final protected ODocument queryDocumentById(String className, long id, ODatabaseDocumentTx dbtx, String... queryStrings) {
+
+		if(countCluster(className.toLowerCase()) == 0) {
+			return null;
+		} else {
+			/*
+			 * Create the query
+			 */
+			int cluster = dbtx.getClusterIdByName(className);
+			StringBuilder query = new StringBuilder();
+			query.append("SELECT FROM #");
+			query.append(cluster);
+			query.append(":");
+			query.append(id);
+			for(String s : queryStrings) {
+				query.append(s);
+			}
+			/*
+			 * Execute
+			 */
+			List<ODocument> results = null;
+			results = dbtx.query(new OSQLSynchQuery<ODocument>(query.toString()));
+			if(results.size() == 1) {
+				return results.get(0);
+			} else {
+				return null;
+			}
+		}
+	}
+
+	final protected List<ODocument> queryDocumentsByRecordIds(String className, Set<Long> ids) {
+
+		return queryDocumentsByRecordIds(className, new OIntentMassiveRead(), ids);
+	}
+
+	final protected List<ODocument> queryDocumentsByRecordIds(String className, OIntent intent, Set<Long> ids) {
+
+		// List<ODocument> result = null;
+		// try (ODatabaseDocumentTx separatedb = ownerPool.acquire()) {
+		// result = queryDocumentsByRecordIds(className, separatedb, intent, ids);
+		// }
+		List<ODocument> result = queryDocumentsByRecordIds(className, getDB(), intent, ids);
+		return result;
+	}
+
+	final protected List<ODocument> queryDocumentsByRecordIds(String className, ODatabaseDocumentTx dbtx, OIntent intent, Set<Long> ids) {
+
+		if(countCluster(className.toLowerCase()) > 0) {
+			/*
+			 * Converting to [#cluster:id1, #cluster:id2 ...]
+			 */
+			int cluster = dbtx.getClusterIdByName(className);
+			StringBuilder queryBuilder = new StringBuilder();
+			queryBuilder.append("SELECT FROM [");
+			Iterator<Long> idIterator = ids.iterator();
+			while(idIterator.hasNext()) {
+				queryBuilder.append("#");
+				queryBuilder.append(cluster);
+				queryBuilder.append(":");
+				queryBuilder.append(idIterator.next());
+				if(idIterator.hasNext()) {
+					queryBuilder.append(", ");
+				}
+			}
+			queryBuilder.append("]");
+			String queryString = queryBuilder.toString();
+			/*
+			 * Supporting intent
+			 */
+			if(intent != null) {
+				dbtx.declareIntent(intent);
+			}
+			List<ODocument> documents = dbtx.query(new OSQLSynchQuery<ODocument>(queryString));
+			/*
+			 * WORKAROUND to bypass local cache
+			 */
+			List<ODocument> returnDocuments = new ArrayList<ODocument>();
+			for(ODocument document : documents) {
+				ODocument reloadedDocument = dbtx.load(document.getIdentity(), null, true);
+				returnDocuments.add(reloadedDocument);
+			}
+			if(intent != null) {
+				dbtx.declareIntent(null);
+			}
+			return returnDocuments;
+		} else {
+			return Collections.<ODocument> emptyList();
+		}
+	}
+
+	final protected List<ODocument> queryDocumentsByClassName(String className, String... queryStrings) {
+
+		/*
+		 * Assume by default that we read a lot of stuff
+		 */
+		return queryDocumentsByClassName(className, new OIntentMassiveRead(), queryStrings);
+	}
+
+	final protected List<ODocument> queryDocumentsByClassName(String className, OIntent intent, String... queryStrings) {
+
+		// List<ODocument> result = null;
+		// try (ODatabaseDocumentTx separatedb = ownerPool.acquire()) {
+		// result = queryDocumentsByClassName(className, separatedb, intent, queryStrings);
+		// }
+		List<ODocument> result = queryDocumentsByClassName(className, getDB(), intent, queryStrings);
+		return result;
+	}
+
+	final protected List<ODocument> queryDocumentsByClassName(String className, ODatabaseDocumentTx dbtx, String... queryStrings) {
+
+		return queryDocumentsByClassName(className, dbtx, new OIntentMassiveRead(), queryStrings);
+	}
+
+	final protected List<ODocument> queryDocumentsByClassName(String className, ODatabaseDocumentTx dbtx, OIntent intent, String... queryStrings) {
+
+		if(countCluster(className.toLowerCase()) > 0) {
+			StringBuilder queryBuilder = new StringBuilder();
+			queryBuilder.append("SELECT FROM ");
+			queryBuilder.append(className);
+			for(String s : queryStrings) {
+				queryBuilder.append(s);
+			}
+			String queryString = queryBuilder.toString();
+			/*
+			 * Supporting intent
+			 */
+			if(intent != null) {
+				dbtx.declareIntent(intent);
+			}
+			List<ODocument> documents = dbtx.query(new OSQLSynchQuery<ODocument>(queryString));
+			/*
+			 * WORKAROUND to bypass local cache
+			 */
+			List<ODocument> returnDocuments = new ArrayList<ODocument>();
+			for(ODocument document : documents) {
+				ODocument reloadedDocument = dbtx.load(document.getIdentity(), null, true);
+				returnDocuments.add(reloadedDocument);
+			}
+			if(intent != null) {
+				dbtx.declareIntent(null);
+			}
+			return returnDocuments;
+		} else {
+			return Collections.<ODocument> emptyList();
+		}
+	}
+
 	// ----------------------------------------------------------------------------------
 	/**
 	 * Selects a Document in the current database on a separate connection by its class name and id.
@@ -211,7 +374,7 @@ public abstract class AbstractDatabase implements IDatabase {
 	 * @param id
 	 * @return ODocument
 	 */
-	protected ODocument queryDocumentByIdOnSeparateConnection(String className, long id) {
+	final protected ODocument queryDocumentByIdOnSeparateConnection(String className, long id) {
 
 		ODocument result = null;
 		try (ODatabaseDocumentTx separatedb = ownerPool.acquire()) {
