5a17d5335f45468605f7e1ec185e2e96a1bde959
dmariano <dnmariano24@yahoo.com>
2015-11-23 23:25:49 -0800

    Bug 479513 - I worked with Ralphie to add Error Residue Tab that would display error residue chart. Added reload button functionality
    Change-Id: Ic6e0c7fa8af1592435adb421bad9b60459a03ca3
    Signed-off-by: dmariano <dnmariano24@yahoo.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/ui/editors/PcaEditor.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/ui/editors/PcaEditor.java
@@ -133,6 +133,7 @@ public class PcaEditor {
 	private Spinner principleComponentSpinner;
 	private Table peakListIntensityTable;
 	private InteractiveChartExtended scorePlotChart;
+	private InteractiveChartExtended errorResidueChart;
 	private Spinner spinnerPCx;
 	private Spinner spinnerPCy;
 	/*
@@ -247,6 +248,8 @@ public class PcaEditor {
 		// 3
 		scorePlotPageIndex = 3;
 		createScorePlotPage();
+		// 4
+		createErrorResiduePage();
 	}
 
 	// --------------------------------------------------------------------------------------------------------------Overview Page
@@ -658,6 +661,7 @@ public class PcaEditor {
 			reloadPeakListIntensityTable();
 			updateSpinnerPCMaxima();
 			reloadScorePlotChart();
+			reloadErrorResidueChart();
 			/*
 			 * Activate the score plot chart.
 			 */
@@ -1175,6 +1179,143 @@ public class PcaEditor {
 		}
 	}
 
+	private void createErrorResiduePage() {
+
+		TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
+		tabItem.setText("Error Residues");
+		//
+		Composite composite = new Composite(tabFolder, SWT.NONE);
+		composite.setLayout(new FillLayout());
+		//
+		Composite parent = new Composite(composite, SWT.NONE);
+		parent.setLayout(new GridLayout(1, true));
+		parent.setLayoutData(GridData.FILL_BOTH);
+		/*
+		 * Selection of the plotted PCs
+		 */
+		Composite spinnerComposite = new Composite(parent, SWT.NONE);
+		spinnerComposite.setLayout(new GridLayout(5, false));
+		spinnerComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		//
+		GridData gridData = new GridData();
+		gridData.widthHint = 50;
+		gridData.heightHint = 20;
+		/*
+		 * Plot the Error residue chart chart.
+		 */
+		Composite chartComposite = new Composite(parent, SWT.NONE);
+		chartComposite.setLayout(new GridLayout(1, true));
+		chartComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		Button button = new Button(spinnerComposite, SWT.PUSH);
+		button.setText("Reload Error Chart");
+		button.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+				reloadErrorResidueChart();
+			}
+		});
+		//
+		errorResidueChart = new InteractiveChartExtended(chartComposite, SWT.NONE);
+		errorResidueChart.setLayoutData(new GridData(GridData.FILL_BOTH));
+		errorResidueChart.getTitle().setText("Error Residue Chart");
+		errorResidueChart.getTitle().setForeground(COLOR_BLACK);
+		//
+		errorResidueChart.setBackground(COLOR_WHITE);
+		errorResidueChart.getLegend().setVisible(false);
+		//
+		errorResidueChart.getAxisSet().getXAxis(0).getTitle().setText("Sample Names");
+		errorResidueChart.getAxisSet().getXAxis(0).getTitle().setForeground(COLOR_BLACK);
+		errorResidueChart.getAxisSet().getXAxis(0).getTick().setForeground(COLOR_BLACK);
+		//
+		errorResidueChart.getAxisSet().getYAxis(0).getTitle().setText("Error Values(10^-6)");
+		errorResidueChart.getAxisSet().getYAxis(0).getTitle().setForeground(COLOR_BLACK);
+		errorResidueChart.getAxisSet().getYAxis(0).getTick().setForeground(COLOR_BLACK);
+		//
+		String[] tempCategories = {" ", " ", " "};
+		errorResidueChart.getAxisSet().getXAxis(0).setCategorySeries(tempCategories);
+		errorResidueChart.getAxisSet().getXAxis(0).enableCategory(true);
+		IPlotArea plotArea = (IPlotArea)errorResidueChart.getPlotArea();
+		/*
+		 * Plot a marker at zero.
+		 */
+		plotArea.addCustomPaintListener(new ICustomPaintListener() {
+
+			@Override
+			public void paintControl(PaintEvent e) {
+
+				Range xRange = errorResidueChart.getAxisSet().getXAxes()[0].getRange();
+				Range yRange = errorResidueChart.getAxisSet().getYAxes()[0].getRange();
+				/*
+				 * Mark the zero lines if possible.
+				 * Otherwise draw the marker in half width.
+				 */
+				if(xRange.lower < 0 && xRange.upper > 0 && yRange.lower < 0 && yRange.upper > 0) {
+					Rectangle rectangle = errorResidueChart.getPlotArea().getClientArea();
+					int width = rectangle.width;
+					int height = rectangle.height;
+					int xWidth;
+					int yHeight;
+					/*
+					 * Dependent where the zero values are.
+					 * xDelta and yDelta can't be zero -> protect from division by zero.
+					 */
+					double xDelta = xRange.upper - xRange.lower;
+					double yDelta = yRange.upper - yRange.lower;
+					double xDiff = xRange.lower * -1; // lower is negative
+					double yDiff = yRange.upper;
+					double xPart = ((100 / xDelta) * xDiff) / 100; // percent -> 0.0 - 1.0
+					double yPart = ((100 / yDelta) * yDiff) / 100; // percent -> 0.0 - 1.0
+					xWidth = (int)(width * xPart);
+					yHeight = (int)(height * yPart);
+					/*
+					 * Draw the line.
+					 */
+					e.gc.setForeground(COLOR_BLACK);
+					e.gc.drawLine(xWidth, 0, xWidth, height); // Vertical line through zero
+					e.gc.drawLine(0, yHeight, width, yHeight); // Horizontal line through zero
+				}
+			}
+
+			@Override
+			public boolean drawBehindSeries() {
+
+				return false;
+			}
+		});
+		/*
+		 * Plot the series name above the entry.
+		 */
+		plotArea.addCustomPaintListener(new ICustomPaintListener() {
+
+			@Override
+			public void paintControl(PaintEvent e) {
+
+				ISeriesSet seriesSet = errorResidueChart.getSeriesSet();
+				ISeries[] series = seriesSet.getSeries();
+				for(ISeries serie : series) {
+					String label = serie.getId();
+					Point point = serie.getPixelCoordinates(0);
+					/*
+					 * Draw the label
+					 */
+					Point labelSize = e.gc.textExtent(label);
+					e.gc.setForeground(COLOR_BLACK);
+					e.gc.drawText("", (int)(point.x - labelSize.x / 2.0d), (int)(point.y - labelSize.y - SYMBOL_SIZE / 2.0d), true);
+				}
+			}
+
+			@Override
+			public boolean drawBehindSeries() {
+
+				return false;
+			}
+		});
+		//
+		tabItem.setControl(composite);
+	}
+
 	private void reloadScorePlotChart() {
 
 		if(scorePlotChart != null) {
@@ -1186,6 +1327,8 @@ public class PcaEditor {
 			for(ISeries serie : series) {
 				seriesSet.deleteSeries(serie.getId());
 			}
+			String[] fileNames = new String[pcaResults.getPcaResultMap().entrySet().size()];
+			int count = 0;
 			/*
 			 * Data
 			 */
@@ -1194,6 +1337,7 @@ public class PcaEditor {
 				 * Create the series.
 				 */
 				String name = entry.getKey().getName();
+				fileNames[count] = name;
 				ILineSeries scatterSeries = (ILineSeries)scorePlotChart.getSeriesSet().createSeries(SeriesType.LINE, name);
 				scatterSeries.setLineStyle(LineStyle.NONE);
 				scatterSeries.setSymbolSize(SYMBOL_SIZE);
@@ -1236,6 +1380,62 @@ public class PcaEditor {
 			scorePlotChart.getAxisSet().adjustRange();
 			scorePlotChart.redraw();
 			scorePlotChart.update();
+		}
+	}
+
+	private void reloadErrorResidueChart() {
+
+		if(errorResidueChart != null) {
+			/*
+			 * Delete all other series.
+			 */
+			ISeriesSet seriesSet = errorResidueChart.getSeriesSet();
+			ISeries[] series = seriesSet.getSeries();
+			for(ISeries serie : series) {
+				seriesSet.deleteSeries(serie.getId());
+			}
+			String[] fileNames = new String[pcaResults.getPcaResultMap().entrySet().size()];
+			int count = 0;
+			/*
+			 * Data
+			 */
+			double[] errorResidue = new double[pcaResults.getPcaResultMap().size()];
+			int counter = 0;
+			errorResidueChart.getAxisSet().getXAxis(0).getTitle().setText("Sample Names");
+			errorResidueChart.getAxisSet().getYAxis(0).getTitle().setText("Error Values(10^-6)");
+			for(ISample key : pcaResults.getPcaResultMap().keySet()) {
+				IPcaResult temp = pcaResults.getPcaResultMap().get(key);
+				// Done to better display error values
+				errorResidue[counter] = temp.getErrorMemberShip() * Math.pow(10, 6);
+				counter++;
+			}
+			for(Map.Entry<ISample, IPcaResult> entry : pcaResults.getPcaResultMap().entrySet()) {
+				/*
+				 * Create the series.
+				 */
+				String name = entry.getKey().getName();
+				fileNames[count] = name;
+				count++;
+			}
+			ILineSeries scatterSeries = (ILineSeries)errorResidueChart.getSeriesSet().createSeries(SeriesType.LINE, "Samples");
+			scatterSeries.setLineStyle(LineStyle.NONE);
+			scatterSeries.setSymbolSize(SYMBOL_SIZE);
+			double[] xSeries = new double[fileNames.length];
+			for(int i = 0; i < fileNames.length; i++) {
+				xSeries[i] = i + 1;
+			}
+			scatterSeries.setYSeries(errorResidue);
+			scatterSeries.setXSeries(xSeries);
+			/*
+			 * Set the color.
+			 */
+			scatterSeries.setSymbolColor(COLOR_RED);
+			scatterSeries.setSymbolType(PlotSymbolType.DIAMOND);
+			errorResidueChart.getAxisSet().getXAxis(0).setCategorySeries(fileNames);
+			errorResidueChart.getAxisSet().getXAxis(0).enableCategory(true);
+			errorResidueChart.getAxisSet().adjustRange();
+			errorResidueChart.redraw();
+			errorResidueChart.update();
 		}
 	}
 }
