2e2355d1558605bce2b1d96409dbaddfcfee5f50
Janos Binder <janosbinder@hotmail.com>
2016-02-05 17:59:45 +0100

    Added settings to database
    Signed-off-by: Janos Binder <janosbinder@hotmail.com>

---
+++ chemclipse/plugins/org.eclipse.chemclipse.database/src/org/eclipse/chemclipse/database/documents/ISettingsDocument.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2016 Dr. Philip Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.database.documents;
+
+public interface ISettingsDocument extends IDocument {
+
+	String CLASS_NAME = "Settings";
+	/*
+	 * Fields
+	 */
+	String FIELD_TYPE = "DatabaseType";
+
+	void setType(String type);
+
+	String getType();
+}
