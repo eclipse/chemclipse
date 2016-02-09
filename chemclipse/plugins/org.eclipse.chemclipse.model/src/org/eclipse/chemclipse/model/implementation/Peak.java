6c32ccf160d962dac9aeedea5040e25639205ff4
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-02-09 09:41:13 +0100

    A new peak test has been added.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/implementation/Peak.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.model.implementation;
+
+import org.eclipse.chemclipse.model.core.AbstractPeak;
+import org.eclipse.chemclipse.model.core.IPeak;
+
+public class Peak extends AbstractPeak implements IPeak {
+}
