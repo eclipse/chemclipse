2d72c59d7d267e22fd30bf30e7973e6911449859
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-02-18 13:28:19 +0100

    The Normalizer has been moved.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/IFragmentedIonScan.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/IFragmentedIonScan.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 <a href="mailto:alexander.kerner@openchrom.net">Alexander Kerner</a>.
+ * Copyright (c) 2016 Lablicate UG (haftungsbeschränkt).
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * <a href="mailto:alexander.kerner@openchrom.net">Alexander Kerner</a> - initial API and implementation
+ * Dr. Alexander Kerner - initial API and implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.model.core;
 
