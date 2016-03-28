e7d4a0eb27bfc848029b8de49f2a3a47994b6f0e
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-03-28 17:07:24 +0200

    I've removed most of the toolbar items.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/definitions/ChromatogramDetectorType.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.ux.extension.ui.definitions;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+public class ChromatogramDetectorType extends PropertyTester {
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+
+		// System.out.println(receiver);
+		// System.out.println(property);
+		// System.out.println(args);
+		// System.out.println(expectedValue);
+		return false;
+	}
+}
