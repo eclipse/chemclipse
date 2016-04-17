75c88f625293b1cf5bf1499589e117e3895950a9
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-04-17 13:25:37 +0200

    The package handles FID data too now.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.ui/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/rtshifter/ui/preferences/FilterPreferencePage.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Philip
+ * (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.ui.preferences;
+
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.preferences.PreferenceSupplier;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.settings.ShiftDirection;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.ui.Activator;
+import org.eclipse.chemclipse.support.ui.preferences.fieldeditors.RetentionTimeMinutesFieldEditor;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class FilterPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	public FilterPreferencePage() {
+		super(GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Set the RTSifter filter settings.");
+	}
+
+	/**
+	 * Creates the field editors. Field editors are abstractions of the common
+	 * GUI blocks needed to manipulate various types of preferences. Each field
+	 * editor knows how to save and restore itself.
+	 */
+	public void createFieldEditors() {
+
+		addField(new BooleanFieldEditor(PreferenceSupplier.P_SHIFT_ALL_SCANS, "Shift all scans", getFieldEditorParent()));
+		//
+		addField(new RetentionTimeMinutesFieldEditor(PreferenceSupplier.P_MILLISECONDS_BACKWARD, "Backward (minutes)", PreferenceSupplier.MIN_RETENTION_TIME, PreferenceSupplier.MAX_RETENTION_TIME, getFieldEditorParent()));
+		addField(new RetentionTimeMinutesFieldEditor(PreferenceSupplier.P_MILLISECONDS_FAST_BACKWARD, "Fast Backward (minutes)", PreferenceSupplier.MIN_RETENTION_TIME, PreferenceSupplier.MAX_RETENTION_TIME, getFieldEditorParent()));
+		addField(new RetentionTimeMinutesFieldEditor(PreferenceSupplier.P_MILLISECONDS_FORWARD, "Forward (minutes)", PreferenceSupplier.MIN_RETENTION_TIME, PreferenceSupplier.MAX_RETENTION_TIME, getFieldEditorParent()));
+		addField(new RetentionTimeMinutesFieldEditor(PreferenceSupplier.P_MILLISECONDS_FAST_FORWARD, "Fast Forward (minutes)", PreferenceSupplier.MIN_RETENTION_TIME, PreferenceSupplier.MAX_RETENTION_TIME, getFieldEditorParent()));
+		/*
+		 * Default Shift Direction (in case of batch processing.
+		 */
+		addField(new ComboFieldEditor(PreferenceSupplier.P_DEFAULT_SHIFT_DIRECTION, "Default Shift Direction", ShiftDirection.getElements(), getFieldEditorParent()));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/rtshifter/core/ChromatogramFilter.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Philip (eselmeister) Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.core;
+
+import org.eclipse.chemclipse.chromatogram.filter.processing.ChromatogramFilterProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.filter.processing.IChromatogramFilterProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.filter.result.ChromatogramFilterResult;
+import org.eclipse.chemclipse.chromatogram.filter.result.IChromatogramFilterResult;
+import org.eclipse.chemclipse.chromatogram.filter.result.ResultStatus;
+import org.eclipse.chemclipse.chromatogram.filter.settings.IChromatogramFilterSettings;
+import org.eclipse.chemclipse.chromatogram.msd.filter.core.chromatogram.AbstractChromatogramFilterMSD;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.core.internal.support.RTShifter;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.exceptions.FilterException;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.preferences.PreferenceSupplier;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.settings.ISupplierFilterSettings;
+import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramFilter extends AbstractChromatogramFilterMSD {
+
+	@Override
+	public IChromatogramFilterProcessingInfo applyFilter(IChromatogramSelectionMSD chromatogramSelection, IChromatogramFilterSettings chromatogramFilterSettings, IProgressMonitor monitor) {
+
+		IChromatogramFilterProcessingInfo processingInfo = new ChromatogramFilterProcessingInfo();
+		processingInfo.addMessages(validate(chromatogramSelection, chromatogramFilterSettings));
+		if(processingInfo.hasErrorMessages()) {
+			return processingInfo;
+		}
+		/*
+		 * Try to shift the retention times.
+		 */
+		IChromatogramFilterResult chromatogramFilterResult;
+		try {
+			chromatogramFilterResult = new ChromatogramFilterResult(ResultStatus.OK, "The chromatogram has been shifted successfully.");
+			RTShifter.shiftRetentionTimes(chromatogramSelection, getSupplierFilterSettings(chromatogramFilterSettings));
+		} catch(FilterException e) {
+			chromatogramFilterResult = new ChromatogramFilterResult(ResultStatus.EXCEPTION, e.getMessage());
+		}
+		processingInfo.setChromatogramFilterResult(chromatogramFilterResult);
+		return processingInfo;
+	}
+
+	@Override
+	public IChromatogramFilterProcessingInfo applyFilter(IChromatogramSelectionMSD chromatogramSelection, IProgressMonitor monitor) {
+
+		IChromatogramFilterSettings chromatogramFilterSettings = PreferenceSupplier.getChromatogramFilterSettings();
+		return applyFilter(chromatogramSelection, chromatogramFilterSettings, monitor);
+	}
+
+	private ISupplierFilterSettings getSupplierFilterSettings(IChromatogramFilterSettings chromatogramFilterSettings) {
+
+		/*
+		 * Get the excluded ions instance.
+		 */
+		if(chromatogramFilterSettings instanceof ISupplierFilterSettings) {
+			return (ISupplierFilterSettings)chromatogramFilterSettings;
+		} else {
+			return PreferenceSupplier.getChromatogramFilterSettings();
+		}
+	}
+}
