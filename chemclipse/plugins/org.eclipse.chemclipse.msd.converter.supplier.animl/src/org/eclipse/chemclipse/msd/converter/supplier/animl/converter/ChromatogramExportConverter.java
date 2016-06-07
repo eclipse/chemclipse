0719137e85baec29c4d892398d443fcaa39285c6
Philip Wenig <philip.wenig@openchrom.net>
2016-06-07 16:11:23 +0200

    The chromatogram import converter handling has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.animl/src/org/eclipse/chemclipse/msd/converter/supplier/animl/converter/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.animl/src/org/eclipse/chemclipse/msd/converter/supplier/animl/converter/ChromatogramExportConverter.java
@@ -13,20 +13,20 @@ package org.eclipse.chemclipse.msd.converter.supplier.animl.converter;
 
 import java.io.File;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramExportConverterProcessingInfo;
 import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
 import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
 import org.eclipse.chemclipse.msd.converter.supplier.animl.internal.converter.IConstants;
 import org.eclipse.chemclipse.msd.converter.supplier.animl.internal.converter.SpecificationValidator;
 import org.eclipse.chemclipse.msd.converter.supplier.animl.io.ChromatogramWriter;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
-import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 
--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.animl/src/org/eclipse/chemclipse/msd/converter/supplier/animl/converter/ChromatogramImportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.animl/src/org/eclipse/chemclipse/msd/converter/supplier/animl/converter/ChromatogramImportConverter.java
@@ -13,16 +13,16 @@ package org.eclipse.chemclipse.msd.converter.supplier.animl.converter;
 
 import java.io.File;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
 import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramOverviewImportConverterProcessingInfo;
 import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramOverviewImportConverterProcessingInfo;
 import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
 import org.eclipse.chemclipse.msd.converter.processing.chromatogram.ChromatogramMSDImportConverterProcessingInfo;
 import org.eclipse.chemclipse.msd.converter.processing.chromatogram.IChromatogramMSDImportConverterProcessingInfo;
 import org.eclipse.chemclipse.msd.converter.supplier.animl.internal.converter.IConstants;
+import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter {
+public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
 
 	@Override
 	public IChromatogramMSDImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
