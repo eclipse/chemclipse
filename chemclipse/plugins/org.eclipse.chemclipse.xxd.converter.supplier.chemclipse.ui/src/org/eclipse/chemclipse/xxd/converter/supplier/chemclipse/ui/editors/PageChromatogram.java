63148196afacced560b79f0911633e6f226fb859
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-06-14 09:07:18 +0200

    The chromatogram project explorer editor has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui/src/org/eclipse/chemclipse/xxd/converter/supplier/chemclipse/ui/editors/PageChromatogram.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui/src/org/eclipse/chemclipse/xxd/converter/supplier/chemclipse/ui/editors/PageChromatogram.java
@@ -12,18 +12,25 @@
 package org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui.editors;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import javax.swing.event.HyperlinkEvent;
-
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramOverviewImportConverterProcessingInfo;
 import org.eclipse.chemclipse.csd.converter.supplier.chemclipse.converter.MagicNumberMatcherCSD;
+import org.eclipse.chemclipse.model.core.AbstractChromatogram;
+import org.eclipse.chemclipse.model.core.IChromatogramOverview;
 import org.eclipse.chemclipse.msd.converter.supplier.chemclipse.converter.MagicNumberMatcherMSD;
 import org.eclipse.chemclipse.rcp.app.ui.addons.ModelSupportAddon;
 import org.eclipse.chemclipse.rcp.ui.icons.core.ApplicationImageFactory;
 import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImage;
+import org.eclipse.chemclipse.support.text.ValueFormat;
 import org.eclipse.chemclipse.support.ui.editors.AbstractExtendedEditorPage;
 import org.eclipse.chemclipse.support.ui.editors.IExtendedEditorPage;
 import org.eclipse.chemclipse.ux.extension.ui.provider.IChromatogramEditorSupport;
 import org.eclipse.chemclipse.wsd.converter.supplier.chemclipse.converter.MagicNumberMatcherWSD;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
@@ -32,6 +39,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.widgets.ImageHyperlink;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
@@ -42,11 +50,21 @@ public class PageChromatogram extends AbstractExtendedEditorPage implements IExt
 	private File file;
 	//
 	private Label labelName;
+	private Label labelDataName;
+	private Label labelOperator;
+	private Label labelDate;
+	private Label labelScans;
+	private Label labelRTStart;
+	private Label labelRTStop;
+	//
 	private ImageHyperlink hyperlinkProcess;
 	//
 	private MagicNumberMatcherMSD magicNumberMatcherMSD;
 	private MagicNumberMatcherCSD magicNumberMatcherCSD;
 	private MagicNumberMatcherWSD magicNumberMatcherWSD;
+	//
+	private SimpleDateFormat dateFormat;
+	private DecimalFormat decimalFormat;
 
 	public PageChromatogram(Composite container) {
 		super("Chromatogram", container, true);
@@ -54,6 +72,9 @@ public class PageChromatogram extends AbstractExtendedEditorPage implements IExt
 		magicNumberMatcherMSD = new MagicNumberMatcherMSD();
 		magicNumberMatcherCSD = new MagicNumberMatcherCSD();
 		magicNumberMatcherWSD = new MagicNumberMatcherWSD();
+		//
+		dateFormat = ValueFormat.getDateFormatEnglish();
+		decimalFormat = ValueFormat.getDecimalFormatEnglish();
 	}
 
 	@Override
@@ -72,22 +93,50 @@ public class PageChromatogram extends AbstractExtendedEditorPage implements IExt
 	public void showData(File file) {
 
 		this.file = file;
+		hyperlinkProcess.setText("");
+		labelName.setText("Name:");
+		labelDataName.setText("Data Name:");
+		labelOperator.setText("Operator:");
+		labelDate.setText("Date:");
+		labelScans.setText("Scans:");
+		labelRTStart.setText("Start RT (min):");
+		labelRTStop.setText("Stop RT (min):");
+		//
 		if(file != null) {
+			//
 			labelName.setText(file.getName());
 			hyperlinkProcess.setText(file.getName());
-		} else {
-			labelName.setText("");
-			hyperlinkProcess.setText("");
+			//
+			IChromatogramOverview chromatogramOverview = getChromatogramOverview(file);
+			if(chromatogramOverview != null) {
+				labelName.setText("Name: " + chromatogramOverview.getName());
+				labelDataName.setText("Data Name: " + chromatogramOverview.getDataName());
+				labelOperator.setText("Operator: " + chromatogramOverview.getOperator());
+				Date date = chromatogramOverview.getDate();
+				if(date != null) {
+					labelDate.setText("Date: " + dateFormat.format(chromatogramOverview.getDate()));
+				} else {
+					labelDate.setText("Date: ");
+				}
+				labelScans.setText("Scans: " + Integer.toString(chromatogramOverview.getNumberOfScans()));
+				labelRTStart.setText("Start RT (min): " + decimalFormat.format(chromatogramOverview.getStartRetentionTime() / AbstractChromatogram.MINUTE_CORRELATION_FACTOR));
+				labelRTStop.setText("Stop RT (min): " + decimalFormat.format(chromatogramOverview.getStopRetentionTime() / AbstractChromatogram.MINUTE_CORRELATION_FACTOR));
+			}
 		}
 	}
 
 	private void createInfoSection(Composite parent) {
 
-		Section section = createSection(parent, 3);
+		Section section = createSection(parent, 3, "Overview", "Some header values of the file are displayed.");
 		Composite client = createClientInfo(section);
 		//
 		labelName = createLabel(client, "");
-		labelName.setLayoutData(new GridData(GridData.FILL_BOTH));
+		labelDataName = createLabel(client, "");
+		labelOperator = createLabel(client, "");
+		labelDate = createLabel(client, "");
+		labelScans = createLabel(client, "");
+		labelRTStart = createLabel(client, "");
+		labelRTStop = createLabel(client, "");
 		/*
 		 * Add the client to the section.
 		 */
@@ -116,22 +165,22 @@ public class PageChromatogram extends AbstractExtendedEditorPage implements IExt
 		imageHyperlink.setText("");
 		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
 		gridData.horizontalIndent = HORIZONTAL_INDENT;
+		gridData.verticalIndent = 20;
 		imageHyperlink.setLayoutData(gridData);
 		imageHyperlink.addHyperlinkListener(new HyperlinkAdapter() {
 
+			@Override
 			public void linkActivated(HyperlinkEvent e) {
 
-				if(file != null) {
-					/*
-					 * Use the editor to show the file.
-					 */
-					IChromatogramEditorSupport chromatogramEditorSupport = getChromatogramEditorSupport(file);
-					if(chromatogramEditorSupport != null) {
-						EModelService eModelService = ModelSupportAddon.getModelService();
-						MApplication mApplication = ModelSupportAddon.getApplication();
-						EPartService ePartService = ModelSupportAddon.getPartService();
-						chromatogramEditorSupport.openEditor(file, eModelService, mApplication, ePartService);
-					}
+				/*
+				 * Use the editor to show the file.
+				 */
+				IChromatogramEditorSupport chromatogramEditorSupport = getChromatogramEditorSupport(file);
+				if(chromatogramEditorSupport != null) {
+					EModelService eModelService = ModelSupportAddon.getModelService();
+					MApplication mApplication = ModelSupportAddon.getApplication();
+					EPartService ePartService = ModelSupportAddon.getPartService();
+					chromatogramEditorSupport.openEditor(file, eModelService, mApplication, ePartService);
 				}
 			}
 		});
@@ -159,31 +208,32 @@ public class PageChromatogram extends AbstractExtendedEditorPage implements IExt
 			return null;
 		}
 	}
-	// private IChromatogramOverview getChromatogramOverview(File file) {
-	//
-	// IChromatogramImportConverter importConverter = null;
-	// if(magicNumberMatcherMSD.checkFileFormat(file)) {
-	// /*
-	// * MSD
-	// */
-	// importConverter = new org.eclipse.chemclipse.msd.converter.supplier.chemclipse.converter.ChromatogramImportConverter();
-	// } else if(magicNumberMatcherCSD.checkFileFormat(file)) {
-	// /*
-	// * CSD
-	// */
-	// importConverter = new org.eclipse.chemclipse.csd.converter.supplier.chemclipse.converter.ChromatogramImportConverter();
-	// } else if(magicNumberMatcherWSD.checkFileFormat(file)) {
-	// /*
-	// * WSD
-	// */
-	// importConverter = new org.eclipse.chemclipse.wsd.converter.supplier.chemclipse.converter.ChromatogramImportConverter();
-	// }
-	// //
-	// if(importConverter != null) {
-	// IChromatogramOverviewImportConverterProcessingInfo processingInfo = importConverter.convertOverview(file, new NullProgressMonitor());
-	// return processingInfo.getChromatogramOverview();
-	// } else {
-	// return null;
-	// }
-	// }
+
+	private IChromatogramOverview getChromatogramOverview(File file) {
+
+		IChromatogramImportConverter importConverter = null;
+		if(magicNumberMatcherMSD.checkFileFormat(file)) {
+			/*
+			 * MSD
+			 */
+			importConverter = new org.eclipse.chemclipse.msd.converter.supplier.chemclipse.converter.ChromatogramImportConverter();
+		} else if(magicNumberMatcherCSD.checkFileFormat(file)) {
+			/*
+			 * CSD
+			 */
+			importConverter = new org.eclipse.chemclipse.csd.converter.supplier.chemclipse.converter.ChromatogramImportConverter();
+		} else if(magicNumberMatcherWSD.checkFileFormat(file)) {
+			/*
+			 * WSD
+			 */
+			importConverter = new org.eclipse.chemclipse.wsd.converter.supplier.chemclipse.converter.ChromatogramImportConverter();
+		}
+		//
+		if(importConverter != null) {
+			IChromatogramOverviewImportConverterProcessingInfo processingInfo = importConverter.convertOverview(file, new NullProgressMonitor());
+			return processingInfo.getChromatogramOverview();
+		} else {
+			return null;
+		}
+	}
 }
