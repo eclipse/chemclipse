34ceca6a9c24e0a317529ffdf4418103a95b90e7
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-06-20 09:42:24 +0200

    The Save As... handler has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/handlers/SaveAsHandler.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/handlers/SaveAsHandler.java
@@ -13,20 +13,27 @@ package org.eclipse.chemclipse.ux.extension.ui.handlers;
 
 import javax.inject.Named;
 
+import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.ux.extension.ui.editors.IChemClipseEditor;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
+@SuppressWarnings("restriction")
 public class SaveAsHandler {
+
+	private static final Logger logger = Logger.getLogger(SaveAsHandler.class);
 
 	@CanExecute
 	boolean canExecute(@Named(IServiceConstants.ACTIVE_PART) MPart part) {
 
 		if(part != null) {
-			if(part.getObject() instanceof IChemClipseEditor) {
+			if(part.isDirty() || part.getObject() instanceof IChemClipseEditor) {
 				return true;
 			}
 		}
@@ -34,16 +41,25 @@ public class SaveAsHandler {
 	}
 
 	@Execute
-	void execute(EPartService partService, @Named(IServiceConstants.ACTIVE_PART) MPart part) {
+	void execute(ECommandService commandService, EHandlerService handlerService, EPartService partService, @Named(IServiceConstants.ACTIVE_PART) MPart part) {
 
 		if(part != null) {
-			/*
-			 * Export the chromatogram as ...
-			 */
 			Object object = part.getObject();
-			if(object != null && object instanceof IChemClipseEditor) {
-				IChemClipseEditor editor = (IChemClipseEditor)object;
-				editor.saveAs();
+			if(object != null) {
+				/*
+				 * Save the chromatogram as ...
+				 */
+				if(object instanceof IChemClipseEditor) {
+					IChemClipseEditor editor = (IChemClipseEditor)object;
+					editor.saveAs();
+				} else {
+					ParameterizedCommand command = commandService.createCommand("org.eclipse.ui.file.saveAs", null); // $NON-NLS-1$
+					if(handlerService.canExecute(command)) {
+						handlerService.executeHandler(command);
+					} else {
+						logger.warn("Couldn't run the Save As... command.");
+					}
+				}
 			}
 		}
 	}
