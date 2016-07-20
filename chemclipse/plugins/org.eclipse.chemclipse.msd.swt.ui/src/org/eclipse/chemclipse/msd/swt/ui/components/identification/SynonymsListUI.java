b76344a62d17515c3aeea738cf5f9ee6c2867a88
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-07-20 19:07:39 +0200

    The synonyms list has been edited.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.swt.ui/src/org/eclipse/chemclipse/msd/swt/ui/components/identification/SynonymsListUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.swt.ui/src/org/eclipse/chemclipse/msd/swt/ui/components/identification/SynonymsListUI.java
@@ -11,82 +11,41 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.swt.ui.components.identification;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyAdapter;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.chemclipse.model.identifier.ILibraryInformation;
 import org.eclipse.chemclipse.msd.swt.ui.internal.provider.SynonymsListContentProvider;
 import org.eclipse.chemclipse.msd.swt.ui.internal.provider.SynonymsListLabelProvider;
 import org.eclipse.chemclipse.msd.swt.ui.internal.provider.SynonymsListTableComparator;
 import org.eclipse.chemclipse.support.ui.swt.ExtendedTableViewer;
+import org.eclipse.swt.widgets.Composite;
 
-public class SynonymsListUI {
+public class SynonymsListUI extends ExtendedTableViewer {
 
-	private ExtendedTableViewer tableViewer;
-	private SynonymsListTableComparator synonymListTableComparator;
 	private String[] titles = {"Synonym"};
 	private int bounds[] = {300};
 
-	public SynonymsListUI(Composite parent, int style) {
-		parent.setLayout(new FillLayout());
-		/*
-		 * E.g. Scan
-		 */
-		tableViewer = new ExtendedTableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
-		tableViewer.createColumns(titles, bounds);
-		tableViewer.setContentProvider(new SynonymsListContentProvider());
-		tableViewer.setLabelProvider(new SynonymsListLabelProvider());
-		/*
-		 * Sorting the table.
-		 */
-		synonymListTableComparator = new SynonymsListTableComparator();
-		tableViewer.setComparator(synonymListTableComparator);
-		/*
-		 * Copy and Paste of the table content.
-		 */
-		tableViewer.getTable().addKeyListener(new KeyAdapter() {
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-
-				/*
-				 * The selected content will be placed to the clipboard if the
-				 * user is using "Function + c". "Function-Key" 262144
-				 * (stateMask) + "c" 99 (keyCode)
-				 */
-				if(e.keyCode == 99 && e.stateMask == 262144) {
-					tableViewer.copyToClipboard(titles);
-				}
-			}
-		});
+	public SynonymsListUI(Composite parent) {
+		super(parent);
+		createColumns();
 	}
 
-	public void setFocus() {
+	public SynonymsListUI(Composite parent, int style) {
+		super(parent, style);
+		createColumns();
+	}
 
-		tableViewer.getControl().setFocus();
+	private void createColumns() {
+
+		createColumns(titles, bounds);
+		//
+		setLabelProvider(new SynonymsListLabelProvider());
+		setContentProvider(new SynonymsListContentProvider());
+		setComparator(new SynonymsListTableComparator());
 	}
 
 	public void update(ILibraryInformation libraryInformation, boolean forceReload) {
 
 		if(libraryInformation != null) {
-			tableViewer.setInput(libraryInformation.getSynonyms());
+			setInput(libraryInformation.getSynonyms());
 		}
-	}
-
-	public void clear() {
-
-		tableViewer.setInput(null);
-	}
-
-	public ExtendedTableViewer getTableViewer() {
-
-		return tableViewer;
-	}
-
-	public String[] getTitles() {
-
-		return titles;
 	}
 }
