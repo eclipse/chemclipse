6c79108f8b0022ebc482865be0564ebd0553b172
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-07-21 13:05:50 +0200

    The mass spectrum list has been refactored.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/views/MassSpectrumIonsListView.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/views/MassSpectrumIonsListView.java
@@ -15,29 +15,18 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
+import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
+import org.eclipse.chemclipse.msd.swt.ui.components.massspectrum.MassSpectrumIonsListUI;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
-import org.eclipse.chemclipse.msd.swt.ui.components.massspectrum.MassSpectrumIonsListUI;
-import org.eclipse.chemclipse.support.ui.swt.ExtendedTableViewer;
 
 public class MassSpectrumIonsListView extends AbstractChromatogramSelectionMSDView {
 
-	private static final String POPUP_MENU_ID = "org.eclipse.chemclipse.chromatogram.msd.ui.perspective.views.massSpectrumIonsListView.popup";
 	@Inject
 	private Composite parent;
 	private MassSpectrumIonsListUI massSpectrumIonsListUI;
@@ -51,32 +40,7 @@ public class MassSpectrumIonsListView extends AbstractChromatogramSelectionMSDVi
 	private void createControl() {
 
 		parent.setLayout(new FillLayout());
-		massSpectrumIonsListUI = new MassSpectrumIonsListUI(parent, SWT.NONE);
-		final ExtendedTableViewer tableViewer = massSpectrumIonsListUI.getTableViewer();
-		/*
-		 * Copy and Paste of the table content.
-		 */
-		tableViewer.getTable().addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-
-				if(e.keyCode == 99 && e.stateMask == 262144) {
-					/*
-					 * The selected content will be placed to the clipboard if
-					 * the user is using "Function + c". "Function-Key" 262144
-					 * (stateMask) + "c" 99 (keyCode)
-					 */
-					tableViewer.copyToClipboard(massSpectrumIonsListUI.getTitles());
-				}
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-
-			}
-		});
-		initContextMenu();
+		massSpectrumIonsListUI = new MassSpectrumIonsListUI(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
 	}
 
 	@PreDestroy
@@ -88,7 +52,7 @@ public class MassSpectrumIonsListView extends AbstractChromatogramSelectionMSDVi
 	@Focus
 	public void setFocus() {
 
-		massSpectrumIonsListUI.getTableViewer().getControl().setFocus();
+		massSpectrumIonsListUI.getControl().setFocus();
 		update(getChromatogramSelection(), false);
 	}
 
@@ -102,38 +66,5 @@ public class MassSpectrumIonsListView extends AbstractChromatogramSelectionMSDVi
 		if(doUpdate(chromatogramSelection)) {
 			massSpectrumIonsListUI.update(chromatogramSelection.getSelectedScan(), forceReload);
 		}
-	}
-
-	/*
-	 * Initialize a context menu.
-	 */
-	private void initContextMenu() {
-
-		MenuManager menuManager = new MenuManager("#PopUpMenu", POPUP_MENU_ID);
-		menuManager.setRemoveAllWhenShown(true);
-		/*
-		 * Copy to clipboard
-		 */
-		menuManager.addMenuListener(new IMenuListener() {
-
-			@Override
-			public void menuAboutToShow(IMenuManager manager) {
-
-				IAction action = new Action() {
-
-					@Override
-					public void run() {
-
-						super.run();
-						massSpectrumIonsListUI.getTableViewer().copyToClipboard(massSpectrumIonsListUI.getTitles());
-					}
-				};
-				action.setText("Copy selection to clipboard");
-				manager.add(action);
-			}
-		});
-		TableViewer tableViewer = massSpectrumIonsListUI.getTableViewer();
-		Menu menu = menuManager.createContextMenu(tableViewer.getTable());
-		tableViewer.getTable().setMenu(menu);
 	}
 }
