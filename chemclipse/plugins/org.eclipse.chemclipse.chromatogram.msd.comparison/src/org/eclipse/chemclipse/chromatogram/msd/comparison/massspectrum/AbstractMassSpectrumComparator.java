db05bc2fab2b22b1fdedda6321dfc9aa4aa52dd4
Alexander Kerner <alexanderkerner24@gmail.com>
2016-09-08 18:49:23 +0200

    Also log errors

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison/src/org/eclipse/chemclipse/chromatogram/msd/comparison/massspectrum/AbstractMassSpectrumComparator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison/src/org/eclipse/chemclipse/chromatogram/msd/comparison/massspectrum/AbstractMassSpectrumComparator.java
@@ -8,34 +8,49 @@
  * 
  * Contributors:
  * Philip (eselmeister) Wenig - initial API and implementation
+ * Alexander Kerner - implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.msd.comparison.massspectrum;
 
+import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.msd.model.core.IScanMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.chemclipse.processing.core.ProcessingInfo;
 
 public abstract class AbstractMassSpectrumComparator implements IMassSpectrumComparator {
 
-	private static final String DESCRIPTION = "MassSpectrum Comparator";
+    private static final String DESCRIPTION = "MassSpectrum Comparator";
 
-	public IProcessingInfo validate(IScanMSD unknown, IScanMSD reference) {
+    private final static Logger logger = Logger.getLogger(AbstractMassSpectrumComparator.class);
 
-		IProcessingInfo processingInfo = new ProcessingInfo();
-		if(unknown == null) {
-			processingInfo.addErrorMessage(DESCRIPTION, "The unknown mass spectum does not exists.");
-		} else {
-			if(unknown.getIons().size() == 0) {
-				processingInfo.addErrorMessage(DESCRIPTION, "There is no ion in the unknown mass spectum.");
-			}
-		}
-		if(reference == null) {
-			processingInfo.addErrorMessage(DESCRIPTION, "The reference mass spectum does not exists.");
-		} else {
-			if(reference.getIons().size() == 0) {
-				processingInfo.addErrorMessage(DESCRIPTION, "There is no ion in the reference mass spectum.");
-			}
-		}
-		return processingInfo;
+    @Override
+    public IProcessingInfo validate(final IScanMSD unknown, final IScanMSD reference) {
+
+	String msg = null;
+	final IProcessingInfo processingInfo = new ProcessingInfo();
+	if (unknown == null) {
+	    msg = "The unknown mass spectum does not exists.";
+	    logger.error(msg);
+	    processingInfo.addErrorMessage(DESCRIPTION, msg);
+	} else {
+	    if (unknown.getIons().size() == 0) {
+		msg = "There is no ion in the unknown mass spectum.";
+		logger.error(msg);
+		processingInfo.addErrorMessage(DESCRIPTION, msg);
+	    }
 	}
+	if (reference == null) {
+	    msg = "The reference mass spectum does not exists.";
+	    logger.error(msg);
+	    processingInfo.addErrorMessage(DESCRIPTION, msg);
+	} else {
+	    if (reference.getIons().size() == 0) {
+		msg = "There is no ion in the reference mass spectum.";
+		logger.error(msg);
+		processingInfo.addErrorMessage(DESCRIPTION, msg);
+	    }
+	}
+
+	return processingInfo;
+    }
 }
