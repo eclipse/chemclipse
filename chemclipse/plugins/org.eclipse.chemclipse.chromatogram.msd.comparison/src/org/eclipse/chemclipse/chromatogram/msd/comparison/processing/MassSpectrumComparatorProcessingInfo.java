e145775d1076740d2ef2f6f260e479c09c5b7f78
Alexander Kerner <alexanderkerner24@gmail.com>
2016-09-19 07:56:17 +0200

    Check for null before instanceof

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison/src/org/eclipse/chemclipse/chromatogram/msd/comparison/processing/MassSpectrumComparatorProcessingInfo.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison/src/org/eclipse/chemclipse/chromatogram/msd/comparison/processing/MassSpectrumComparatorProcessingInfo.java
@@ -16,22 +16,27 @@ import org.eclipse.chemclipse.msd.model.core.identifier.massspectrum.IMassSpectr
 import org.eclipse.chemclipse.processing.core.AbstractProcessingInfo;
 import org.eclipse.chemclipse.processing.core.exceptions.TypeCastException;
 
-public class MassSpectrumComparatorProcessingInfo extends AbstractProcessingInfo implements IMassSpectrumComparatorProcessingInfo {
+public class MassSpectrumComparatorProcessingInfo extends AbstractProcessingInfo
+	implements IMassSpectrumComparatorProcessingInfo {
 
-	@Override
-	public IMassSpectrumComparisonResult getMassSpectrumComparisonResult() throws TypeCastException {
+    @Override
+    public IMassSpectrumComparisonResult getMassSpectrumComparisonResult() throws TypeCastException {
 
-		Object object = getProcessingResult();
-		if(object instanceof IMassSpectrumComparisonResult) {
-			return (IMassSpectrumComparisonResult)object;
-		} else {
-			throw createTypeCastException("MassSpectrum Comparator", object.getClass(), IMassSpectrumComparisonResult.class);
-		}
+	Object object = getProcessingResult();
+	if (object == null) {
+	    throw new NullPointerException();
 	}
-
-	@Override
-	public void setMassSpectrumComparisonResult(IMassSpectrumComparisonResult massSpectrumComparisonResult) {
-
-		setProcessingResult(massSpectrumComparisonResult);
+	if (object instanceof IMassSpectrumComparisonResult) {
+	    return (IMassSpectrumComparisonResult) object;
+	} else {
+	    throw createTypeCastException("MassSpectrum Comparator", object.getClass(),
+		    IMassSpectrumComparisonResult.class);
 	}
+    }
+
+    @Override
+    public void setMassSpectrumComparisonResult(IMassSpectrumComparisonResult massSpectrumComparisonResult) {
+
+	setProcessingResult(massSpectrumComparisonResult);
+    }
 }
