2466b94a0d7f53cef08fcfd6c2ecaaa1d399ce00
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-12-20 11:53:36 +0100

    A build description has been added.

--- org.eclipse.chemclipse.utils/scripts/branch-projects.sh
+++ org.eclipse.chemclipse.utils/scripts/branch-projects.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #*******************************************************************************
-# Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+# Copyright (c) 2015 Lablicate GmbH.
 # 
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
--- org.eclipse.chemclipse.utils/scripts/status-projects.sh
+++ org.eclipse.chemclipse.utils/scripts/status-projects.sh
@@ -2,14 +2,16 @@
 
 #*******************************************************************************
 #
-# Copyright (c) 2014 Lablicate UG (haftungsbeschränkt)
-#
-# All rights reserved.
+# Copyright (c) 2014, 2016 Lablicate GmbH.
+# 
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
 # 
 # Contributors:
 #	Dr. Philip Wenig
 #       Dr. Janos Binder
-#
 #*******************************************************************************/
 
 STATUS_SCRIPT_GIT_FLAGS=" --short"
