8ea4b410a4a74076725f234cee5515750a611cbe
Philip Wenig <philip.wenig@openchrom.net>
2016-12-23 18:02:43 +0100

    The header info has been updated.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.ui/src/org/eclipse/chemclipse/chromatogram/msd/process/supplier/peakidentification/ui/editors/PeakIdentificationBatchProcessEditorPage.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.ui/src/org/eclipse/chemclipse/chromatogram/msd/process/supplier/peakidentification/ui/editors/PeakIdentificationBatchProcessEditorPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Philip (eselmeister) Wenig.
+ * Copyright (c) 2011, 2016 Dr. Philip Wenig.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Philip (eselmeister) Wenig - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.ui.editors;
 
@@ -59,7 +59,7 @@ import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImage;
 import org.eclipse.chemclipse.support.events.IPerspectiveAndViewIds;
 
 /**
- * @author Philip (eselmeister) Wenig
+ * @author Dr. Philip Wenig
  * 
  */
 public class PeakIdentificationBatchProcessEditorPage implements IMultiEditorPage {
--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.ui/src/org/eclipse/chemclipse/chromatogram/msd/process/supplier/peakidentification/ui/internal/runnables/PeakIdentificationBatchJobExportRunnable.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.ui/src/org/eclipse/chemclipse/chromatogram/msd/process/supplier/peakidentification/ui/internal/runnables/PeakIdentificationBatchJobExportRunnable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Philip (eselmeister) Wenig.
+ * Copyright (c) 2011, 2016 Dr. Philip Wenig.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Philip (eselmeister) Wenig - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.ui.internal.runnables;
 
@@ -26,7 +26,7 @@ import org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentificati
 import org.eclipse.chemclipse.chromatogram.msd.process.supplier.peakidentification.model.IPeakIdentificationBatchJob;
 
 /**
- * @author Philip (eselmeister) Wenig
+ * @author Dr. Philip Wenig
  * 
  */
 public class PeakIdentificationBatchJobExportRunnable implements IRunnableWithProgress {
