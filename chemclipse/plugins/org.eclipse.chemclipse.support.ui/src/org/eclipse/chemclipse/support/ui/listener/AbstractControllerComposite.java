73f67c4ab1d05a8b8c73ddbe2be0e16c2b044773
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-01-01 12:01:31 +0100

    Happy New Year 2017!

--- chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/listener/AbstractControllerComposite.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/listener/AbstractControllerComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 Lablicate GmbH.
+ * Copyright (c) 2015, 2017 Lablicate GmbH.
  * 
  * All rights reserved.
  * 
