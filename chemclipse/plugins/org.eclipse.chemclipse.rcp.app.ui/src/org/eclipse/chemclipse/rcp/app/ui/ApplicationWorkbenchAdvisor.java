5db91a2f3674a2b099c8f9c89fd63cfe58368142
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-02-09 12:54:24 +0100

    PNG images are used instead of gif now.

--- chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/ApplicationWorkbenchAdvisor.java
+++ chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/ApplicationWorkbenchAdvisor.java
@@ -54,8 +54,8 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 		final String ICONS_PATH = "icons/full/";
 		final String PATH_OBJECT = ICONS_PATH + "obj16/";
 		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
-		declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif", true);
-		declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT + "cprj_obj.gif", true);
+		declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.png", true);
+		declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT + "cprj_obj.png", true);
 	}
 
 	public String getInitialWindowPerspectiveId() {
