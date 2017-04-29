578b276e492cc8d2799a3be341168049a6c97442
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-04-29 12:13:24 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.alfassi.fragment.test/src/org/eclipse/chemclipse/chromatogram/msd/comparison/supplier/alfassi/comparator/GeometricDistanceMassSpectrumComparator_2_Test.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2017 Lablicate GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.alfassi.comparator;
+
+import org.eclipse.chemclipse.chromatogram.msd.comparison.processing.IMassSpectrumComparatorProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.msd.comparison.supplier.alfassi.comparator.MassSpectrumComparator;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.msd.model.core.identifier.massspectrum.IMassSpectrumComparisonResult;
+
+public class GeometricDistanceMassSpectrumComparator_2_Test extends MassSpectrumSetTestCase {
+
+	private MassSpectrumComparator comparator;
+	private IMassSpectrumComparatorProcessingInfo processingInfo;
+	private IMassSpectrumComparisonResult result;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		//
+		IScanMSD unknown = sinapylAclohol.getMassSpectrum();
+		IScanMSD reference = benzenepropanoicAcid.getMassSpectrum();
+		//
+		comparator = new MassSpectrumComparator();
+		processingInfo = comparator.compare(unknown, reference);
+		result = processingInfo.getMassSpectrumComparisonResult();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		super.tearDown();
+	}
+
+	public void test1() {
+
+		assertFalse(processingInfo.hasErrorMessages());
+	}
+
+	public void test2() {
+
+		assertEquals(60.377384f, result.getMatchFactor());
+	}
+
+	public void test3() {
+
+		assertEquals(59.819443f, result.getReverseMatchFactor());
+	}
+}
