0bc183c789921753152506624586329f466b5e67
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-04-29 21:49:24 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.excel/src/org/eclipse/chemclipse/msd/converter/supplier/excel/converter/ChromatogramImportConverter.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2017 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.excel.converter;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.core.IChromatogramOverview;
+import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
+import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDReader;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.ChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.IChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.supplier.excel.internal.support.IConstants;
+import org.eclipse.chemclipse.msd.converter.supplier.excel.io.ChromatogramReader;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
+
+	private static final Logger logger = Logger.getLogger(ChromatogramImportConverter.class);
+	private static final String DESCRIPTION = "Excel Import Converter";
+
+	@Override
+	public IChromatogramMSDImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		IChromatogramMSDImportConverterProcessingInfo processingInfo = new ChromatogramMSDImportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			/*
+			 * Read the chromatogram.
+			 */
+			// file = SpecificationValidator.validateSpecification(file);
+			IChromatogramMSDReader reader = new ChromatogramReader();
+			monitor.subTask(IConstants.IMPORT_EXCEL_CHROMATOGRAM);
+			try {
+				IChromatogramMSD chromatogram = reader.read(file, monitor);
+				processingInfo.setChromatogram(chromatogram);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+
+	@Override
+	public IChromatogramOverviewImportConverterProcessingInfo convertOverview(File file, IProgressMonitor monitor) {
+
+		IChromatogramOverviewImportConverterProcessingInfo processingInfo = new ChromatogramOverviewImportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			/*
+			 * Read the chromatogram overview.
+			 */
+			// file = SpecificationValidator.validateSpecification(file);
+			IChromatogramMSDReader reader = new ChromatogramReader();
+			monitor.subTask(IConstants.IMPORT_EXCEL_CHROMATOGRAM_OVERVIEW);
+			try {
+				IChromatogramOverview chromatogramOverview = reader.readOverview(file, monitor);
+				processingInfo.setChromatogramOverview(chromatogramOverview);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+}
