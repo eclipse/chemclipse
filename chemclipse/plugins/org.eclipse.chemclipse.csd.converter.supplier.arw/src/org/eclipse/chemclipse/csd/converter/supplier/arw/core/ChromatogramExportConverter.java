d140216b8f70ab05d37d680b11379e6b26c04927
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-04-29 13:19:13 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.csd.converter.supplier.arw/src/org/eclipse/chemclipse/csd/converter/supplier/arw/core/ChromatogramExportConverter.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2017 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.csd.converter.supplier.arw.core;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.csd.converter.chromatogram.AbstractChromatogramCSDExportConverter;
+import org.eclipse.chemclipse.csd.model.core.IChromatogramCSD;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramExportConverter extends AbstractChromatogramCSDExportConverter implements IChromatogramExportConverter {
+
+	@Override
+	public IChromatogramExportConverterProcessingInfo convert(File file, IChromatogramCSD chromatogram, IProgressMonitor monitor) {
+
+		IChromatogramExportConverterProcessingInfo processingInfo = new ChromatogramExportConverterProcessingInfo();
+		return processingInfo;
+	}
+}
