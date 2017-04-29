56da9f02d9762e4a5056ac0380786f86616076d3
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-04-29 21:49:49 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.gson/src/org/eclipse/chemclipse/msd/converter/supplier/gson/core/ChromatogramExportConverter.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.gson.core;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
+import org.eclipse.chemclipse.msd.converter.supplier.gson.io.ChromatogramWriter;
+import org.eclipse.chemclipse.msd.converter.supplier.gson.io.SpecificationValidator;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter implements IChromatogramExportConverter {
+
+	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
+	private static final String DESCRIPTION = "JSON Export Converter";
+
+	@Override
+	public IChromatogramExportConverterProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+
+		IChromatogramExportConverterProcessingInfo processingInfo = new ChromatogramExportConverterProcessingInfo();
+		/*
+		 * Validate the file.
+		 */
+		file = SpecificationValidator.validateSpecification(file, "json");
+		IProcessingInfo processingInfoValidate = super.validate(file);
+		/*
+		 * Don't process if errors have occurred.
+		 */
+		if(processingInfoValidate.hasErrorMessages()) {
+			processingInfo.addMessages(processingInfoValidate);
+		} else {
+			ChromatogramWriter writer = new ChromatogramWriter();
+			monitor.subTask("Export JSON chromatogram");
+			try {
+				writer.writeChromatogram(file, chromatogram, monitor);
+				processingInfo.setFile(file);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
+		return processingInfo;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.gson/src/org/eclipse/chemclipse/msd/converter/supplier/gson/core/ChromatogramImportConverter.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.gson.core;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
+import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramOverviewImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.ChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.processing.chromatogram.IChromatogramMSDImportConverterProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
+
+	private static final String DESCRIPTION = "GSON Import Converter";
+	private static final String EXPORT_ERROR = "The GSON converter is not able to import chromatograms.";
+
+	@Override
+	public IChromatogramMSDImportConverterProcessingInfo convert(File file, IProgressMonitor monitor) {
+
+		IChromatogramMSDImportConverterProcessingInfo processingInfo = new ChromatogramMSDImportConverterProcessingInfo();
+		processingInfo.addErrorMessage(DESCRIPTION, EXPORT_ERROR);
+		return processingInfo;
+	}
+
+	@Override
+	public IChromatogramOverviewImportConverterProcessingInfo convertOverview(File file, IProgressMonitor monitor) {
+
+		IChromatogramOverviewImportConverterProcessingInfo processingInfo = new ChromatogramOverviewImportConverterProcessingInfo();
+		processingInfo.addErrorMessage(DESCRIPTION, EXPORT_ERROR);
+		return processingInfo;
+	}
+}
