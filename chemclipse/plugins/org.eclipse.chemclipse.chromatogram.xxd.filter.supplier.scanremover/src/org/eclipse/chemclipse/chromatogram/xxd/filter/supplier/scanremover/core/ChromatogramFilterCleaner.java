0bbe994af1f68a88b2fa7ced94bec59929051cdb
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-04-30 14:20:38 +0200

    New initial commit.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.scanremover/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/scanremover/core/ChromatogramFilterCleaner.java
@@ -0,0 +1,125 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.scanremover.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.chemclipse.chromatogram.filter.core.chromatogram.AbstractChromatogramFilter;
+import org.eclipse.chemclipse.chromatogram.filter.processing.ChromatogramFilterProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.filter.processing.IChromatogramFilterProcessingInfo;
+import org.eclipse.chemclipse.chromatogram.filter.result.ChromatogramFilterResult;
+import org.eclipse.chemclipse.chromatogram.filter.result.IChromatogramFilterResult;
+import org.eclipse.chemclipse.chromatogram.filter.result.ResultStatus;
+import org.eclipse.chemclipse.chromatogram.filter.settings.IChromatogramFilterSettings;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.scanremover.exceptions.FilterException;
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.scanremover.preferences.PreferenceSupplier;
+import org.eclipse.chemclipse.csd.model.core.IScanCSD;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IScan;
+import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.processing.core.MessageType;
+import org.eclipse.chemclipse.processing.core.ProcessingMessage;
+import org.eclipse.chemclipse.wsd.model.core.IScanWSD;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/**
+ * This filter removes empty scans.
+ *
+ */
+public class ChromatogramFilterCleaner extends AbstractChromatogramFilter {
+
+	@Override
+	public IChromatogramFilterProcessingInfo applyFilter(IChromatogramSelection chromatogramSelection, IChromatogramFilterSettings chromatogramFilterSettings, IProgressMonitor monitor) {
+
+		IChromatogramFilterProcessingInfo processingInfo = new ChromatogramFilterProcessingInfo();
+		/*
+		 * Validate the settings.
+		 */
+		processingInfo.addMessages(validate(chromatogramSelection, chromatogramFilterSettings));
+		if(processingInfo.hasErrorMessages()) {
+			return processingInfo;
+		}
+		/*
+		 * Try to remove the given scans.
+		 */
+		IChromatogramFilterResult chromatogramFilterResult;
+		try {
+			applyChromatogramCleanerFilter(chromatogramSelection, monitor);
+			processingInfo.addMessage(new ProcessingMessage(MessageType.INFO, "Chromatogram Cleaner", "Empty scans have been removed successfully."));
+			chromatogramFilterResult = new ChromatogramFilterResult(ResultStatus.OK, "Empty scans have been removed successfully.");
+		} catch(FilterException e) {
+			chromatogramFilterResult = new ChromatogramFilterResult(ResultStatus.EXCEPTION, e.getMessage());
+		}
+		processingInfo.setChromatogramFilterResult(chromatogramFilterResult);
+		return processingInfo;
+	}
+
+	@Override
+	public IChromatogramFilterProcessingInfo applyFilter(IChromatogramSelection chromatogramSelection, IProgressMonitor monitor) {
+
+		IChromatogramFilterSettings chromatogramFilterSettings = PreferenceSupplier.getChromatogramFilterSettings();
+		return applyFilter(chromatogramSelection, chromatogramFilterSettings, monitor);
+	}
+
+	private void applyChromatogramCleanerFilter(IChromatogramSelection chromatogramSelection, IProgressMonitor monitor) throws FilterException {
+
+		IChromatogram chromatogram = chromatogramSelection.getChromatogram();
+		int startScan = chromatogram.getScanNumber(chromatogramSelection.getStartRetentionTime());
+		int stopScan = chromatogram.getScanNumber(chromatogramSelection.getStopRetentionTime());
+		List<Integer> scansToRemove = new ArrayList<Integer>();
+		/*
+		 * Iterate through all selected scans and mark those to be removed.
+		 */
+		for(int scan = startScan; scan <= stopScan; scan++) {
+			IScan chromatogramScan = chromatogram.getScan(scan);
+			if(chromatogramScan instanceof IScanMSD) {
+				/*
+				 * MSD
+				 */
+				if(((IScanMSD)chromatogramScan).getNumberOfIons() == 0) {
+					monitor.subTask("Remove scan from chromatogram: " + scan);
+					scansToRemove.add(scan);
+				}
+			} else if(chromatogramScan instanceof IScanCSD) {
+				/*
+				 * CSD
+				 */
+				if(((IScanCSD)chromatogramScan).getTotalSignal() == 0) {
+					monitor.subTask("Remove scan from chromatogram: " + scan);
+					scansToRemove.add(scan);
+				}
+			} else if(chromatogramScan instanceof IScanWSD) {
+				/*
+				 * WSD
+				 */
+				if(((IScanWSD)chromatogramScan).getScanSignals().size() == 0) {
+					monitor.subTask("Remove scan from chromatogram: " + scan);
+					scansToRemove.add(scan);
+				}
+			}
+		}
+		/*
+		 * Use a remove counter, because each time a scan will be removed, the chromatogram contains one scan less.
+		 */
+		int removeCounter = 0;
+		for(Integer scan : scansToRemove) {
+			scan -= removeCounter;
+			chromatogram.removeScan(scan);
+			removeCounter++;
+		}
+		//
+		chromatogram.recalculateScanNumbers();
+		chromatogramSelection.reset();
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.scanremover/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/scanremover/settings/SupplierFilterSettings.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2017 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.scanremover.settings;
+
+import org.eclipse.chemclipse.chromatogram.filter.settings.AbstractChromatogramFilterSettings;
+
+public class SupplierFilterSettings extends AbstractChromatogramFilterSettings implements ISupplierFilterSettings {
+
+	private String scanRemoverPattern;
+
+	@Override
+	public String getScanRemoverPattern() {
+
+		return scanRemoverPattern;
+	}
+
+	@Override
+	public void setScanRemoverPattern(String scanRemoverPattern) {
+
+		if(scanRemoverPattern != null) {
+			this.scanRemoverPattern = scanRemoverPattern;
+		}
+	}
+}
