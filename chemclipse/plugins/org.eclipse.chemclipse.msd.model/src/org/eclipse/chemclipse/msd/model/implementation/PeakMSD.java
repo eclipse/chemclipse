135cce7a5c97272151517d852ad3a196b8d0855a
Alexander Kerner <alexanderkerner24@gmail.com>
2017-06-28 16:13:11 +0200

    [fix] xident#22

--- chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/implementation/PeakMSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/implementation/PeakMSD.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2013, 2017 Lablicate GmbH.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
@@ -40,5 +40,6 @@ public class PeakMSD extends AbstractPeakMSD implements IPeakMSD {
 		setPeakType(template.getPeakType());
 		setQuantifierDescription(template.getQuantifierDescription());
 		setSuggestedNumberOfComponents(template.getSuggestedNumberOfComponents());
+		addAllTargets(template.getTargets());
 	}
 }
