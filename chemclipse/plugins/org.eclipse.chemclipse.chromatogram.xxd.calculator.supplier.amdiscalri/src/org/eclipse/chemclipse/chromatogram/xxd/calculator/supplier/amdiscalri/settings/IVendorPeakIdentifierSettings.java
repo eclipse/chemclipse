ddfbb9fb104288c4d81c87c76d82e3db5023c05d
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-10-02 15:40:51 +0200

    The identifier settings support has been added.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/amdiscalri/settings/IVendorPeakIdentifierSettings.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri.settings;
+
+import org.eclipse.chemclipse.chromatogram.msd.identifier.settings.IPeakIdentifierSettings;
+import org.eclipse.chemclipse.chromatogram.msd.identifier.supplier.file.settings.IFileIdentifierSettings;
+
+public interface IVendorPeakIdentifierSettings extends IPeakIdentifierSettings, IFileIdentifierSettings {
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/amdiscalri/settings/VendorPeakIdentifierSettings.java
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri.settings;
+
+import org.eclipse.chemclipse.chromatogram.msd.identifier.settings.AbstractPeakIdentifierSettings;
+import org.eclipse.chemclipse.chromatogram.msd.identifier.supplier.file.settings.IVendorPeakIdentifierSettings;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+
+public class VendorPeakIdentifierSettings extends AbstractPeakIdentifierSettings implements IVendorPeakIdentifierSettings {
+
+	@JsonProperty(value = "Mass Spectra Files", defaultValue = "")
+	@JsonPropertyDescription(value = "Use a semicolon to separate the path of several files.") // see FileListUtil()
+	private String massSpectraFiles = "";
+	@JsonProperty(value = "Pre-Optimization", defaultValue = "false")
+	private boolean usePreOptimization = false;
+	@JsonProperty(value = "Threshold Pre-Optimization", defaultValue = "0.12")
+	private double thresholdPreOptimization = 0.12;
+	@JsonProperty(value = "Number of Targets", defaultValue = "15")
+	private int numberOfTargets = 15;
+	@JsonProperty(value = "Min Match Factor", defaultValue = "80.0")
+	private float minMatchFactor = 80.0f;
+	@JsonProperty(value = "Min Reverse Match Factor", defaultValue = "80.0")
+	private float minReverseMatchFactor = 80.0f;
+	@JsonProperty(value = "Add Unknown m/z List Target", defaultValue = "true")
+	private boolean addUnknownMzListTarget = true;
+	@JsonProperty(value = "Alternate Identifier Id", defaultValue = "")
+	private String alternateIdentifierId = "";
+
+	@Override
+	public String getMassSpectraFiles() {
+
+		return massSpectraFiles;
+	}
+
+	@Override
+	public void setMassSpectraFiles(String massSpectraFiles) {
+
+		this.massSpectraFiles = massSpectraFiles;
+	}
+
+	@Override
+	public boolean isUsePreOptimization() {
+
+		return usePreOptimization;
+	}
+
+	@Override
+	public void setUsePreOptimization(boolean usePreOptimization) {
+
+		this.usePreOptimization = usePreOptimization;
+	}
+
+	@Override
+	public double getThresholdPreOptimization() {
+
+		return thresholdPreOptimization;
+	}
+
+	@Override
+	public void setThresholdPreOptimization(double thresholdPreOptimization) {
+
+		this.thresholdPreOptimization = thresholdPreOptimization;
+	}
+
+	@Override
+	public int getNumberOfTargets() {
+
+		return numberOfTargets;
+	}
+
+	@Override
+	public void setNumberOfTargets(int numberOfTargets) {
+
+		this.numberOfTargets = numberOfTargets;
+	}
+
+	@Override
+	public float getMinMatchFactor() {
+
+		return minMatchFactor;
+	}
+
+	@Override
+	public void setMinMatchFactor(float minMatchFactor) {
+
+		this.minMatchFactor = minMatchFactor;
+	}
+
+	@Override
+	public float getMinReverseMatchFactor() {
+
+		return minReverseMatchFactor;
+	}
+
+	@Override
+	public void setMinReverseMatchFactor(float minReverseMatchFactor) {
+
+		this.minReverseMatchFactor = minReverseMatchFactor;
+	}
+
+	@Override
+	public boolean isAddUnknownMzListTarget() {
+
+		return addUnknownMzListTarget;
+	}
+
+	@Override
+	public void setAddUnknownMzListTarget(boolean addUnknownMzListTarget) {
+
+		this.addUnknownMzListTarget = addUnknownMzListTarget;
+	}
+
+	@Override
+	public String getAlternateIdentifierId() {
+
+		return alternateIdentifierId;
+	}
+
+	@Override
+	public void setAlternateIdentifierId(String alternateIdentifierId) {
+
+		this.alternateIdentifierId = alternateIdentifierId;
+	}
+}
