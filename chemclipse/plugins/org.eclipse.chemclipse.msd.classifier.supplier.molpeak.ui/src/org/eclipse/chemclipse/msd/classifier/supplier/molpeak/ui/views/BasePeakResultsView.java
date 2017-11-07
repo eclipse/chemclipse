95935e9500a7499070ddc2566a8a388fb14df3f2
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-11-07 17:19:40 +0100

    The extended table viewer has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.classifier.supplier.molpeak.ui/src/org/eclipse/chemclipse/msd/classifier/supplier/molpeak/ui/views/BasePeakResultsView.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.classifier.supplier.molpeak.ui/src/org/eclipse/chemclipse/msd/classifier/supplier/molpeak/ui/views/BasePeakResultsView.java
@@ -29,21 +29,12 @@ import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Menu;
 
 public class BasePeakResultsView extends AbstractChromatogramSelectionMSDView {
 
-	private static final String POPUP_MENU_ID = "org.eclipse.chemclipse.msd.classifier.supplier.molpeak.ui.views.molPeakResultsView.popup";
 	@Inject
 	private Composite parent;
 	private ExtendedTableViewer tableViewer;
@@ -68,30 +59,6 @@ public class BasePeakResultsView extends AbstractChromatogramSelectionMSDView {
 		 */
 		MolPeakResultsTableComparator wncResultsTableComparator = new MolPeakResultsTableComparator();
 		tableViewer.setComparator(wncResultsTableComparator);
-		/*
-		 * Copy and Paste of the table content.
-		 */
-		tableViewer.getTable().addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-
-				/*
-				 * The selected content will be placed to the clipboard if the
-				 * user is using "Function + c". "Function-Key" 262144
-				 * (stateMask) + "c" 99 (keyCode)
-				 */
-				if(e.keyCode == 99 && e.stateMask == 262144) {
-					tableViewer.copyToClipboard(titles);
-				}
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-
-			}
-		});
-		initContextMenu();
 	}
 
 	@PreDestroy
@@ -136,35 +103,5 @@ public class BasePeakResultsView extends AbstractChromatogramSelectionMSDView {
 		if(isPartVisible() && ligninRatios != null) {
 			tableViewer.setInput(ligninRatios);
 		}
-	}
-
-	// -----------------------------------------private methods
-	/*
-	 * Initialize a context menu.
-	 */
-	private void initContextMenu() {
-
-		MenuManager menuManager = new MenuManager("#PopUpMenu", POPUP_MENU_ID);
-		menuManager.setRemoveAllWhenShown(true);
-		menuManager.addMenuListener(new IMenuListener() {
-
-			@Override
-			public void menuAboutToShow(IMenuManager manager) {
-
-				IAction action = new Action() {
-
-					@Override
-					public void run() {
-
-						super.run();
-						tableViewer.copyToClipboard(titles);
-					}
-				};
-				action.setText("Copy selection to clipboard");
-				manager.add(action);
-			}
-		});
-		Menu menu = menuManager.createContextMenu(tableViewer.getTable());
-		tableViewer.getTable().setMenu(menu);
 	}
 }
