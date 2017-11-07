9381d16735a0af56191f634a692d00030e3e4c70
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-11-07 17:19:39 +0100

    The extended table viewer has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui/src/org/eclipse/chemclipse/chromatogram/xxd/classifier/supplier/durbinwatson/ui/views/DurbinWatsonResultsView.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui/src/org/eclipse/chemclipse/chromatogram/xxd/classifier/supplier/durbinwatson/ui/views/DurbinWatsonResultsView.java
@@ -15,21 +15,6 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
-import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Menu;
 import org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.result.IChromatogramResultDurbinWatson;
 import org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.result.IDurbinWatsonClassifierResult;
 import org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui.internal.provider.ClassifierResultContentProvider;
@@ -39,6 +24,13 @@ import org.eclipse.chemclipse.model.core.IChromatogramResult;
 import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
 import org.eclipse.chemclipse.support.ui.swt.ExtendedTableViewer;
 import org.eclipse.chemclipse.ux.extension.msd.ui.views.AbstractChromatogramSelectionMSDView;
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
 
 public class DurbinWatsonResultsView extends AbstractChromatogramSelectionMSDView {
 
@@ -68,30 +60,6 @@ public class DurbinWatsonResultsView extends AbstractChromatogramSelectionMSDVie
 		 */
 		classifierResultTableComparator = new ClassifierResultTableComparator();
 		tableViewer.setComparator(classifierResultTableComparator);
-		/*
-		 * Copy and Paste of the table content.
-		 */
-		tableViewer.getTable().addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-
-				/*
-				 * The selected content will be placed to the clipboard if the
-				 * user is using "Function + c". "Function-Key" 262144
-				 * (stateMask) + "c" 99 (keyCode)
-				 */
-				if(e.keyCode == 99 && e.stateMask == 262144) {
-					tableViewer.copyToClipboard(titles);
-				}
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-
-			}
-		});
-		initContextMenu();
 	}
 
 	@PreDestroy
@@ -135,31 +103,5 @@ public class DurbinWatsonResultsView extends AbstractChromatogramSelectionMSDVie
 		if(isPartVisible() && durbinWatsonClassifierResult != null) {
 			tableViewer.setInput(durbinWatsonClassifierResult);
 		}
-	}
-
-	private void initContextMenu() {
-
-		MenuManager menuManager = new MenuManager("#PopUpMenu", POPUP_MENU_ID);
-		menuManager.setRemoveAllWhenShown(true);
-		menuManager.addMenuListener(new IMenuListener() {
-
-			@Override
-			public void menuAboutToShow(IMenuManager manager) {
-
-				IAction action = new Action() {
-
-					@Override
-					public void run() {
-
-						super.run();
-						tableViewer.copyToClipboard(titles);
-					}
-				};
-				action.setText("Copy selection to clipboard");
-				manager.add(action);
-			}
-		});
-		Menu menu = menuManager.createContextMenu(tableViewer.getTable());
-		tableViewer.getTable().setMenu(menu);
 	}
 }
