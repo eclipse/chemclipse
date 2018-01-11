25ad9c21e9e44163b757fb2f6f1189f43c587a1e
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-01-11 11:55:12 +0100

    A database import/export converter extension point has been added.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter/src/org/eclipse/chemclipse/msd/converter/database/AbstractDatabaseExportConverter.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.database;
+
+import org.eclipse.chemclipse.converter.core.AbstractExportConverter;
+import org.eclipse.chemclipse.msd.converter.processing.database.DatabaseExportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.converter.processing.database.IDatabaseExportConverterProcessingInfo;
+import org.eclipse.chemclipse.msd.model.core.IMassSpectra;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingMessage;
+import org.eclipse.chemclipse.processing.core.MessageType;
+import org.eclipse.chemclipse.processing.core.ProcessingMessage;
+
+public abstract class AbstractDatabaseExportConverter extends AbstractExportConverter implements IDatabaseExportConverter {
+
+	@Override
+	public IDatabaseExportConverterProcessingInfo validate(IScanMSD massSpectrum) {
+
+		IDatabaseExportConverterProcessingInfo processingInfo = new DatabaseExportConverterProcessingInfo();
+		if(massSpectrum == null) {
+			IProcessingMessage processingMessage = new ProcessingMessage(MessageType.ERROR, "Mass Spectra Export", "The is no mass spectrum to export.");
+			processingInfo.addMessage(processingMessage);
+		}
+		return processingInfo;
+	}
+
+	@Override
+	public IDatabaseExportConverterProcessingInfo validate(IMassSpectra massSpectra) {
+
+		IDatabaseExportConverterProcessingInfo processingInfo = new DatabaseExportConverterProcessingInfo();
+		if(massSpectra == null) {
+			IProcessingMessage processingMessage = new ProcessingMessage(MessageType.ERROR, "Mass Spectra Export", "The are no mass spectra to export.");
+			processingInfo.addMessage(processingMessage);
+		}
+		return processingInfo;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter/src/org/eclipse/chemclipse/msd/converter/processing/database/DatabaseImportConverterProcessingInfo.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.processing.database;
+
+import org.eclipse.chemclipse.msd.model.core.IMassSpectra;
+import org.eclipse.chemclipse.processing.core.AbstractProcessingInfo;
+import org.eclipse.chemclipse.processing.core.exceptions.TypeCastException;
+
+public class DatabaseImportConverterProcessingInfo extends AbstractProcessingInfo implements IDatabaseImportConverterProcessingInfo {
+
+	@Override
+	public IMassSpectra getMassSpectra() throws TypeCastException {
+
+		Object object = getProcessingResult();
+		if(object instanceof IMassSpectra) {
+			return (IMassSpectra)object;
+		} else {
+			throw createTypeCastException("Database Import Converter", object.getClass(), IMassSpectra.class);
+		}
+	}
+
+	@Override
+	public void setMassSpectra(IMassSpectra massSpectra) {
+
+		setProcessingResult(massSpectra);
+	}
+}
