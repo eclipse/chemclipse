a6b20da3fb4ed07eab28ded9f4b1367c8bf0c392
Lorenz Gerber <lorenzottogerber@gmail.com>
2018-02-14 11:58:14 +0100

    #530530 - iterative part of nipals coded, untested
    Change-Id: Icde6e00a070334ac0d9fc19f34aaea5bced20492
    Signed-off-by: Lorenz Gerber <lorenzottogerber@gmail.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/model/PcaCalculatorNipals.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/model/PcaCalculatorNipals.java
@@ -13,6 +13,7 @@ package org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.model;
 
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
+import org.ejml.ops.NormOps;
 
 public class PcaCalculatorNipals extends AbstractPcaCalculator {
 
@@ -28,15 +29,24 @@ public class PcaCalculatorNipals extends AbstractPcaCalculator {
 		double scoreDotOld = 0;
 		double scoreDotNew = 0;
 		DenseMatrix64F p = new DenseMatrix64F(numberOfVariables, 1);
-		DenseMatrix64F t = CommonOps.extract(E, 0, numberOfVariables, 0, 1);
-		DenseMatrix64F pMatrix = new DenseMatrix64F(numberOfVariables);
-		DenseMatrix64F tMatrix = new DenseMatrix64F(numberOfSamples);
+		DenseMatrix64F t = CommonOps.extract(E, 0, numberOfSamples, 0, 1);
+		DenseMatrix64F pMatrix = new DenseMatrix64F(numberOfVariables, 1);
+		DenseMatrix64F tMatrix = new DenseMatrix64F(numberOfSamples, 1);
 		// Iterate over number of components
 		for(int i = 0; i < numComps; i++) {
 			// do.. iterate until threshold reached
 			do {
 				scoreDotOld = scoreDotNew;
 				double tMultiplied = 1 / CommonOps.dot(t, t);
+				CommonOps.extract(t, 0, numberOfSamples, 0, 1, tMatrix, 0, 0);
+				DenseMatrix64F E_t = new DenseMatrix64F(numberOfVariables, numberOfSamples);
+				CommonOps.transpose(E, E_t);
+				CommonOps.mult(tMultiplied, E_t, tMatrix, p);
+				NormOps.normalizeF(p);
+				double pMultiplied = 1 / CommonOps.dot(p, p);
+				CommonOps.extract(p, 0, numberOfVariables, 0, 1, pMatrix, 0, 0);
+				CommonOps.mult(pMultiplied, E, pMatrix, t);
+				scoreDotNew = CommonOps.dot(t, t);
 			} while(Math.abs(scoreDotOld - scoreDotNew) > threshold);
 			// write scores, loadings of current component
 		}
