b2ce0f399ccee18f4fabb8e98acf5ffd4c8186ea
Alexander Kerner <alexanderkerner24@gmail.com>
2018-03-06 16:08:27 +0100

    Generify lazy-loading Properties

--- chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingStringProperty.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingStringProperty.java
@@ -9,16 +9,9 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.fx.ui;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.concurrent.Task;
-
-public abstract class LazyLoadingStringProperty extends SimpleStringProperty {
+public abstract class LazyLoadingStringProperty extends LazyLoadingProperty<String> {
 
 	public static final String DEFAULT_LOADING_STRING = "Loading..";
-
-	// private final String loadingString = DEFAULT_LOADING_STRING;
-
-	private boolean loaded = false;
 
 	public LazyLoadingStringProperty(final String loadingString) {
 		setValue(loadingString);
@@ -28,60 +21,9 @@ public abstract class LazyLoadingStringProperty extends SimpleStringProperty {
 		setValue(DEFAULT_LOADING_STRING);
 	}
 
-	public boolean isLoaded() {
-		return loaded;
-	}
-
-	protected void setLoaded(final boolean loaded) {
-		this.loaded = loaded;
-	}
-
-	/**
-	 * Is called after the background task's finished (success or failure). Override
-	 * if needed. E.g. to bind the value afterwards.
-	 */
-	protected void afterLoaded() {
-		// nothing
-
-	}
-
 	@Override
-	public String getValue() {
-		if (!loaded) {
-			startLoadingService();
-		}
-		return super.getValue();
+	protected String getFailedValue(final Throwable t) {
+		return t.getLocalizedMessage();
 	}
 
-	/**
-	 * Starts the {@link Task} that will calculate this Property's value in the
-	 * background.
-	 */
-	protected void startLoadingService() {
-		setLoaded(true);
-		final Task<String> s = LazyLoadingStringProperty.this.createTask();
-
-		LazyLoadingThreads.getExecutorService().submit(s);
-
-		s.setOnFailed(e -> {
-			setValue(s.getException().getLocalizedMessage());
-			afterLoaded();
-		});
-
-		s.setOnSucceeded(e -> {
-			setValue(s.getValue());
-			afterLoaded();
-
-		});
-
-	}
-
-	/**
-	 * Returns a {@link Task} that will calculate this Property's value in the
-	 * background.
-	 *
-	 * @return a {@link Task} that will calculate this Property's value in the
-	 *         background.
-	 */
-	protected abstract Task<String> createTask();
 }
