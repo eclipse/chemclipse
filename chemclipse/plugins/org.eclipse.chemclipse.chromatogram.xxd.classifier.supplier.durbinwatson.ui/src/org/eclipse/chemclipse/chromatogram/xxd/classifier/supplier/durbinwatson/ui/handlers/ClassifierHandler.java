c702d546b111014b4d3b0623394360d490bfbf95
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-04-30 18:41:21 +0200

    The classifier perspective has been removed.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui/src/org/eclipse/chemclipse/chromatogram/xxd/classifier/supplier/durbinwatson/ui/handlers/ClassifierHandler.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui/src/org/eclipse/chemclipse/chromatogram/xxd/classifier/supplier/durbinwatson/ui/handlers/ClassifierHandler.java
@@ -15,6 +15,12 @@ import java.lang.reflect.InvocationTargetException;
 
 import javax.inject.Named;
 
+import org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui.internal.runnables.ClassifierEvaluator;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
+import org.eclipse.chemclipse.progress.core.InfoType;
+import org.eclipse.chemclipse.progress.core.StatusLineLogger;
+import org.eclipse.chemclipse.support.events.IChemClipseEvents;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -24,15 +30,6 @@ import org.eclipse.swt.widgets.Display;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
 
-import org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui.internal.runnables.ClassifierEvaluator;
-import org.eclipse.chemclipse.logging.core.Logger;
-import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
-import org.eclipse.chemclipse.progress.core.InfoType;
-import org.eclipse.chemclipse.progress.core.StatusLineLogger;
-import org.eclipse.chemclipse.rcp.app.ui.handlers.PerspectiveSwitchHandler;
-import org.eclipse.chemclipse.support.events.IChemClipseEvents;
-import org.eclipse.chemclipse.support.events.IPerspectiveAndViewIds;
-
 public class ClassifierHandler implements EventHandler {
 
 	private static final Logger logger = Logger.getLogger(ClassifierHandler.class);
@@ -41,11 +38,6 @@ public class ClassifierHandler implements EventHandler {
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part) {
 
-		/*
-		 * Try to select and show the perspective and view.
-		 */
-		String viewId = "org.eclipse.chemclipse.chromatogram.xxd.classifier.supplier.durbinwatson.ui.part.dw";
-		PerspectiveSwitchHandler.focusPerspectiveAndView(IPerspectiveAndViewIds.PERSPECTIVE_CLASSIFIER, viewId);
 		/*
 		 * Run the handler.
 		 */
