dcc4d3f8b282d15c0cb3e76e1975e6f92b9fce24
Alexander Kerner <alexanderkerner24@gmail.com>
2018-05-23 17:43:48 +0200

    JavaDoc

--- chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingStringProperty.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingStringProperty.java
@@ -9,32 +9,38 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.fx.ui;
 
+/**
+ *
+ * @author Alexander Kerner
+ *
+ */
 public abstract class LazyLoadingStringProperty extends LazyLoadingObjectProperty<String> {
 
 	public static final String DEFAULT_LOADING_STRING = "Loading..";
 
 	public LazyLoadingStringProperty(final Object bean, final String name, final String initialValue) {
-		super(bean, name, initialValue);
 
+		super(bean, name, initialValue);
 	}
 
 	public LazyLoadingStringProperty(final Object bean, final String name) {
-		super(bean, name);
 
+		super(bean, name);
 	}
 
 	public LazyLoadingStringProperty(final String initialValue) {
-		super(initialValue);
 
+		super(initialValue);
 	}
 
 	public LazyLoadingStringProperty() {
+
 		this(DEFAULT_LOADING_STRING);
 	}
 
 	@Override
 	protected String getFailedValue(final Throwable t) {
+
 		return t.getLocalizedMessage();
 	}
-
 }
