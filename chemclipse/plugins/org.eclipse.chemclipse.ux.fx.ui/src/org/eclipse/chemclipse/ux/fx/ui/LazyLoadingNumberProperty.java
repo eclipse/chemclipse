1bb3a4423414f17f547834eaab2409a962b3c1b9
Alexander Kerner <alexanderkerner24@gmail.com>
2018-06-02 23:25:40 +0200

    Fix

--- chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingNumberProperty.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingNumberProperty.java
@@ -1,8 +1,13 @@
 package org.eclipse.chemclipse.ux.fx.ui;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public abstract class LazyLoadingNumberProperty extends LazyLoadingObjectProperty<Number> {
 
+	private static final Logger logger = Logger.getLogger(LazyLoadingNumberProperty.class);
 	public static final Number DEFAULT_LOADING_VALUE = -2;
+	public static final Number DEFAULT_ERROR_VALUE = -3;
 
 	public LazyLoadingNumberProperty() {
 
@@ -22,5 +27,14 @@ public abstract class LazyLoadingNumberProperty extends LazyLoadingObjectPropert
 	public LazyLoadingNumberProperty(final Object bean, final String name) {
 
 		super(bean, name);
+	}
+
+	@Override
+	protected Number getFailedValue(final Throwable t) {
+
+		if(logger.isEnabledFor(Level.ERROR)) {
+			logger.error(t.getLocalizedMessage(), t);
+		}
+		return DEFAULT_ERROR_VALUE;
 	}
 }
