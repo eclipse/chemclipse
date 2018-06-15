2b92390b8a665c46f7f4ae8683430aa209479138
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-06-15 16:27:42 +0200

    DEVELOP-679 - Clean PerspectiveChooserDialog / Remove Unused Perspectives

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.edit.supplier.snip.ui/src/org/eclipse/chemclipse/chromatogram/xxd/edit/supplier/snip/ui/handlers/FilterHandlerAllPeaks.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.edit.supplier.snip.ui/src/org/eclipse/chemclipse/chromatogram/xxd/edit/supplier/snip/ui/handlers/FilterHandlerAllPeaks.java
@@ -15,6 +15,12 @@ import java.lang.reflect.InvocationTargetException;
 
 import javax.inject.Named;
 
+import org.eclipse.chemclipse.chromatogram.xxd.edit.supplier.snip.ui.internal.handlers.FilterRunnable;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
+import org.eclipse.chemclipse.progress.core.InfoType;
+import org.eclipse.chemclipse.progress.core.StatusLineLogger;
+import org.eclipse.chemclipse.support.events.IChemClipseEvents;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -23,15 +29,6 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Display;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
-
-import org.eclipse.chemclipse.chromatogram.xxd.edit.supplier.snip.ui.internal.handlers.FilterRunnable;
-import org.eclipse.chemclipse.logging.core.Logger;
-import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
-import org.eclipse.chemclipse.progress.core.InfoType;
-import org.eclipse.chemclipse.progress.core.StatusLineLogger;
-import org.eclipse.chemclipse.rcp.app.ui.handlers.PerspectiveSwitchHandler;
-import org.eclipse.chemclipse.support.events.IChemClipseEvents;
-import org.eclipse.chemclipse.support.events.IPerspectiveAndViewIds;
 
 public class FilterHandlerAllPeaks implements EventHandler {
 
@@ -42,9 +39,6 @@ public class FilterHandlerAllPeaks implements EventHandler {
 	public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part) {
 
 		if(chromatogramSelection != null) {
-			//
-			PerspectiveSwitchHandler.focusPerspectiveAndView(IPerspectiveAndViewIds.PERSPECTIVE_PEAKS_MSD, IPerspectiveAndViewIds.VIEW_PEAK_MSD);
-			//
 			final Display display = Display.getCurrent();
 			StatusLineLogger.setInfo(InfoType.MESSAGE, "Start SNIP Filter All Peaks");
 			IRunnableWithProgress runnable = new FilterRunnable(chromatogramSelection, false);
