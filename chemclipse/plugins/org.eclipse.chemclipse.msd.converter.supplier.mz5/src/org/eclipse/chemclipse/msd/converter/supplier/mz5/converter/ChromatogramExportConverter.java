7548851057b18c019cbcfdb4d923074cf65a8f2a
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-06-21 15:11:53 +0200

    DEVELOP-689 - part I

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mz5/src/org/eclipse/chemclipse/msd/converter/supplier/mz5/converter/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mz5/src/org/eclipse/chemclipse/msd/converter/supplier/mz5/converter/ChromatogramExportConverter.java
@@ -14,18 +14,18 @@ package org.eclipse.chemclipse.msd.converter.supplier.mz5.converter;
 import java.io.File;
 
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
-import org.eclipse.chemclipse.converter.processing.chromatogram.ChromatogramExportConverterProcessingInfo;
-import org.eclipse.chemclipse.converter.processing.chromatogram.IChromatogramExportConverterProcessingInfo;
 import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.chemclipse.processing.core.ProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter implements IChromatogramExportConverter {
 
 	@Override
-	public IChromatogramExportConverterProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
 
-		IChromatogramExportConverterProcessingInfo processingInfo = new ChromatogramExportConverterProcessingInfo();
+		IProcessingInfo processingInfo = new ProcessingInfo();
 		processingInfo.addErrorMessage("mzData Export Converter", "The converter needs to be implemented.");
 		return processingInfo;
 	}
