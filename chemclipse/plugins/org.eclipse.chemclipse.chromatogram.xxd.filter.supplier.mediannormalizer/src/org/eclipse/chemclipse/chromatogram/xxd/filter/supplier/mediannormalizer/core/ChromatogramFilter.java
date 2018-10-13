70d7815314716a882a940ee15d395f7269b700d3
Jan Holy <jan.holy91@gmail.com>
2018-10-13 14:03:58 +0200

    DEVELOP-786 - Generalize signal chromatogram processing
    Change-Id: Id7d80de8581d8e8cb47336d3617ffbee2ad05813
    Signed-off-by: Jan Holy <jan.holy91@gmail.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.mediannormalizer/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/mediannormalizer/core/ChromatogramFilter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.mediannormalizer/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/mediannormalizer/core/ChromatogramFilter.java
@@ -12,68 +12,24 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.mediannormalizer.core;
 
-import org.eclipse.chemclipse.chromatogram.filter.core.chromatogram.AbstractChromatogramFilter;
+import org.eclipse.chemclipse.chromatogram.filter.core.chromatogram.AbstractChromatogramSignalFilter;
 import org.eclipse.chemclipse.chromatogram.filter.result.ChromatogramFilterResult;
+import org.eclipse.chemclipse.chromatogram.filter.result.IChromatogramFilterResult;
 import org.eclipse.chemclipse.chromatogram.filter.result.ResultStatus;
 import org.eclipse.chemclipse.chromatogram.filter.settings.IChromatogramFilterSettings;
-import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.mediannormalizer.exceptions.FilterException;
 import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.mediannormalizer.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.mediannormalizer.settings.FilterSettings;
-import org.eclipse.chemclipse.model.core.IChromatogram;
-import org.eclipse.chemclipse.model.core.IScan;
 import org.eclipse.chemclipse.model.exceptions.CalculationException;
-import org.eclipse.chemclipse.model.exceptions.ChromatogramIsNullException;
 import org.eclipse.chemclipse.model.exceptions.NoTotalSignalStoredException;
-import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
-import org.eclipse.chemclipse.model.signals.ITotalScanSignalExtractor;
 import org.eclipse.chemclipse.model.signals.ITotalScanSignals;
-import org.eclipse.chemclipse.model.signals.TotalScanSignalExtractor;
 import org.eclipse.chemclipse.model.signals.TotalScanSignalsModifier;
-import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-@SuppressWarnings("rawtypes")
-public class ChromatogramFilter extends AbstractChromatogramFilter {
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public IProcessingInfo applyFilter(IChromatogramSelection chromatogramSelection, IChromatogramFilterSettings chromatogramFilterSettings, IProgressMonitor monitor) {
-
-		IProcessingInfo processingInfo = validate(chromatogramSelection, chromatogramFilterSettings);
-		if(!processingInfo.hasErrorMessages()) {
-			if(chromatogramFilterSettings instanceof FilterSettings) {
-				try {
-					applyMedianNormalizerFilter(chromatogramSelection);
-					processingInfo.setProcessingResult(new ChromatogramFilterResult(ResultStatus.OK, "The chromatogram selection was successfully normalized."));
-				} catch(FilterException e) {
-					processingInfo.setProcessingResult(new ChromatogramFilterResult(ResultStatus.EXCEPTION, e.getMessage()));
-				}
-			}
-		}
-		return processingInfo;
-	}
+public class ChromatogramFilter extends AbstractChromatogramSignalFilter {
 
 	@Override
-	public IProcessingInfo applyFilter(IChromatogramSelection chromatogramSelection, IProgressMonitor monitor) {
+	protected IChromatogramFilterResult applyFilter(ITotalScanSignals totalSignals, IChromatogramFilterSettings filterSettings, IProgressMonitor monitor) {
 
-		FilterSettings filterSettings = PreferenceSupplier.getFilterSettings();
-		return applyFilter(chromatogramSelection, filterSettings, monitor);
-	}
-
-	@SuppressWarnings("unchecked")
-	private void applyMedianNormalizerFilter(IChromatogramSelection chromatogramSelection) throws FilterException {
-
-		/*
-		 * Get the chromatogram to determine the start and stop scan of the
-		 * chromatogram selection.
-		 */
-		IChromatogram chromatogram = chromatogramSelection.getChromatogram();
-		int startScan = chromatogram.getScanNumber(chromatogramSelection.getStartRetentionTime());
-		int stopScan = chromatogram.getScanNumber(chromatogramSelection.getStopRetentionTime());
-		/*
-		 * Get the total ion signals.
-		 */
-		ITotalScanSignals totalScanSignals;
 		try {
 			/*
 			 * Try to median normalize the total ion signals. The scans of the
@@ -82,33 +38,19 @@ public class ChromatogramFilter extends AbstractChromatogramFilter {
 			 * a median normalization method in the helper class
 			 * "TotalIonSignalsModifier.java".
 			 */
-			ITotalScanSignalExtractor totalScanSignalExtractor = new TotalScanSignalExtractor(chromatogram);
-			totalScanSignals = totalScanSignalExtractor.getTotalScanSignals(startScan, stopScan);
-			TotalScanSignalsModifier.medianNormalize(totalScanSignals);
+			TotalScanSignalsModifier.medianNormalize(totalSignals);
 		} catch(NoTotalSignalStoredException e) {
-			throw new FilterException("There were no total ion signals stored.");
+			return new ChromatogramFilterResult(ResultStatus.EXCEPTION, e.getMessage());
 		} catch(CalculationException e) {
-			throw new FilterException(e.getMessage());
-		} catch(ChromatogramIsNullException e) {
-			throw new FilterException("The chromatogram must be not null.");
+			return new ChromatogramFilterResult(ResultStatus.EXCEPTION, e.getMessage());
 		}
-		/*
-		 * Iterate through all selected scans and set the calculated abundance
-		 * values.
-		 */
-		for(int scan = startScan; scan <= stopScan; scan++) {
-			IScan scanRecord = chromatogram.getScan(scan);
-			/*
-			 * Get the normalized abundance and correct it, if necessary. Take
-			 * care, some supplier e.g. Agilent are not able to save all kind of
-			 * ion and abundance values due to their data format. But in most
-			 * cases, problems will occur if the values are too high.
-			 */
-			float normalizedAbundance = totalScanSignals.getTotalScanSignal(scan).getTotalSignal();
-			if(normalizedAbundance <= 0.0f) {
-				normalizedAbundance = 0.1f;
-			}
-			scanRecord.adjustTotalSignal(normalizedAbundance);
-		}
+		return new ChromatogramFilterResult(ResultStatus.OK, "The chromatogram selection was successfully normalized.");
+	}
+
+	@Override
+	protected IChromatogramFilterResult applyFilter(ITotalScanSignals totalSignals, IProgressMonitor monitor) {
+
+		FilterSettings filterSettings = PreferenceSupplier.getFilterSettings();
+		return applyFilter(totalSignals, filterSettings, monitor);
 	}
 }
