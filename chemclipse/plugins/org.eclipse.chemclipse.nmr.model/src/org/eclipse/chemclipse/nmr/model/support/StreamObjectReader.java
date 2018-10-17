4ece172b67fdceca82e8da900982f25a3a78ae57
Jan Holy <jan.holy91@gmail.com>
2018-10-17 21:16:50 +0200

    add possibility to read object from stream, will be replace by ocb file
    Change-Id: I978c2c1f7e76050003879ee6e0e630233dee9d49
    Signed-off-by: Jan Holy <jan.holy91@gmail.com>

---
+++ chemclipse/plugins/org.eclipse.chemclipse.nmr.model/src/org/eclipse/chemclipse/nmr/model/support/StreamObjectReader.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Jan Holy - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.nmr.model.support;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+
+import org.eclipse.chemclipse.nmr.model.core.IScanNMR;
+
+public class StreamObjectReader {
+
+	public static IScanNMR readObject(ObjectInput objectInput) throws ClassNotFoundException, IOException {
+
+		return (IScanNMR)objectInput.readObject();
+	}
+}
