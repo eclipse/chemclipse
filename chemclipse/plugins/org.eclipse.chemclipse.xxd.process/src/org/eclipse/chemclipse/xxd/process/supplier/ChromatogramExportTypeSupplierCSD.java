e7cce9ed5f0bb5cb4fd6674f583024a53771a848
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-11-03 21:20:27 +0100

    CSD and WSD chromatogram export supplier have been added.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.process/src/org/eclipse/chemclipse/xxd/process/supplier/ChromatogramExportTypeSupplierCSD.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.xxd.process.supplier;
+
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
+import org.eclipse.chemclipse.model.settings.IProcessSettings;
+import org.eclipse.chemclipse.model.types.DataType;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.chemclipse.processing.core.ProcessingInfo;
+import org.eclipse.chemclipse.xxd.process.support.IProcessTypeSupplier;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ChromatogramExportTypeSupplierCSD extends AbstractProcessTypeSupplier implements IProcessTypeSupplier {
+
+	public static final String CATEGORY = "Chromatogram Export";
+	private static final Logger logger = Logger.getLogger(ChromatogramExportTypeSupplierCSD.class);
+
+	public ChromatogramExportTypeSupplierCSD() {
+		super(CATEGORY, new DataType[]{DataType.MSD});
+		// IChromatogramConverterSupport support = ChromatogramConverterCSD.getChromatogramConverterSupport(); // TODO
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public IProcessingInfo applyProcessor(IChromatogramSelection chromatogramSelection, String processorId, IProcessSettings processSettings, IProgressMonitor monitor) {
+
+		return new ProcessingInfo(); // TODO
+	}
+}
