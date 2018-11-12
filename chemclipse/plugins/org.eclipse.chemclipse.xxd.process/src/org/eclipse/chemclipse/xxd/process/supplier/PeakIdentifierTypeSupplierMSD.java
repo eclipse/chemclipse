8fcf00a44d019b429aac6b16b191e8997fc44297
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-11-12 23:05:20 +0100

    The MSD identifier support has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.process/src/org/eclipse/chemclipse/xxd/process/supplier/PeakIdentifierTypeSupplierMSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.process/src/org/eclipse/chemclipse/xxd/process/supplier/PeakIdentifierTypeSupplierMSD.java
@@ -22,7 +22,6 @@ import org.eclipse.chemclipse.model.settings.IProcessSettings;
 import org.eclipse.chemclipse.model.types.DataType;
 import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
-import org.eclipse.chemclipse.processing.core.ProcessingInfo;
 import org.eclipse.chemclipse.xxd.process.support.IProcessTypeSupplier;
 import org.eclipse.chemclipse.xxd.process.support.ProcessorSupplier;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -58,8 +57,7 @@ public class PeakIdentifierTypeSupplierMSD extends AbstractProcessTypeSupplier i
 		if(chromatogramSelection instanceof IChromatogramSelectionMSD) {
 			IChromatogramSelectionMSD chromatogramSelectionMSD = (IChromatogramSelectionMSD)chromatogramSelection;
 			if(processSettings instanceof IPeakIdentifierSettingsMSD) {
-				processingInfo = new ProcessingInfo(); // TODO REMOVE
-				// processingInfo = PeakIdentifierMSD.identify(chromatogramSelectionMSD, (IPeakIdentifierSettingsMSD)processSettings, processorId, monitor); // TODO
+				processingInfo = PeakIdentifierMSD.identify(chromatogramSelectionMSD, (IPeakIdentifierSettingsMSD)processSettings, processorId, monitor);
 			} else {
 				processingInfo = PeakIdentifierMSD.identify(chromatogramSelectionMSD, processorId, monitor);
 			}
