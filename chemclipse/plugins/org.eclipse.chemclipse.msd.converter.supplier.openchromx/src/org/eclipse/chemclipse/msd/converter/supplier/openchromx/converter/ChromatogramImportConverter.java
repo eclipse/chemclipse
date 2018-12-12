a6acc22e7c34ff1fed2ea2fb5e1cf57105ed83dc
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-12 14:54:02 +0100

    The chromatogram converter has been refactored.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.openchromx/src/org/eclipse/chemclipse/msd/converter/supplier/openchromx/converter/ChromatogramImportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.openchromx/src/org/eclipse/chemclipse/msd/converter/supplier/openchromx/converter/ChromatogramImportConverter.java
@@ -13,10 +13,10 @@ package org.eclipse.chemclipse.msd.converter.supplier.openchromx.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramImportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.model.core.IChromatogramOverview;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
 import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDReader;
 import org.eclipse.chemclipse.msd.converter.supplier.openchromx.internal.converter.SpecificationValidator;
 import org.eclipse.chemclipse.msd.converter.supplier.openchromx.internal.support.IConstants;
@@ -25,7 +25,7 @@ import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
+public class ChromatogramImportConverter extends AbstractChromatogramImportConverter implements IChromatogramImportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramImportConverter.class);
 	private static final String DESCRIPTION = "ChemClipseX Import Converter";
