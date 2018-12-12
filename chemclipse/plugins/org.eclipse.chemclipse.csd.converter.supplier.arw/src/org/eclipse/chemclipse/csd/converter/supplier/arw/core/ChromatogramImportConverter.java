b12d86e317570d9eff4f42fae96826c83260dfe7
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-12 14:54:03 +0100

    The chromatogram converter has been refactored.

--- chemclipse/plugins/org.eclipse.chemclipse.csd.converter.supplier.arw/src/org/eclipse/chemclipse/csd/converter/supplier/arw/core/ChromatogramImportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.csd.converter.supplier.arw/src/org/eclipse/chemclipse/csd/converter/supplier/arw/core/ChromatogramImportConverter.java
@@ -13,8 +13,8 @@ package org.eclipse.chemclipse.csd.converter.supplier.arw.core;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramImportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
-import org.eclipse.chemclipse.csd.converter.chromatogram.AbstractChromatogramCSDImportConverter;
 import org.eclipse.chemclipse.csd.converter.io.IChromatogramCSDReader;
 import org.eclipse.chemclipse.csd.converter.supplier.arw.internal.support.SpecificationValidator;
 import org.eclipse.chemclipse.csd.converter.supplier.arw.io.ChromatogramReader;
@@ -24,7 +24,7 @@ import org.eclipse.chemclipse.model.core.IChromatogramOverview;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramImportConverter extends AbstractChromatogramCSDImportConverter implements IChromatogramImportConverter {
+public class ChromatogramImportConverter extends AbstractChromatogramImportConverter implements IChromatogramImportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramImportConverter.class);
 	private static final String DESCRIPTION = "Arw Import Converter";
