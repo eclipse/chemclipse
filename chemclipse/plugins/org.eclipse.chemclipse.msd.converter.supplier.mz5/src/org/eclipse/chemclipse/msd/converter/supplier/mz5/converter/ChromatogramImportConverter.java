390cac9104298b65548a788059f1bfe67e3dcd38
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-12 14:54:02 +0100

    The chromatogram converter has been refactored.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mz5/src/org/eclipse/chemclipse/msd/converter/supplier/mz5/converter/ChromatogramImportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mz5/src/org/eclipse/chemclipse/msd/converter/supplier/mz5/converter/ChromatogramImportConverter.java
@@ -13,13 +13,13 @@ package org.eclipse.chemclipse.msd.converter.supplier.mz5.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramImportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.chemclipse.processing.core.ProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
+public class ChromatogramImportConverter extends AbstractChromatogramImportConverter implements IChromatogramImportConverter {
 
 	private static final String DESCRIPTION = "mz5 Import Converter";
 
