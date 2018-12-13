733ad4b051be10d38812fc764242dc0a6d72e098
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-13 07:54:12 +0100

    The chromatogram converter handling has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.csv/src/org/eclipse/chemclipse/msd/converter/supplier/csv/core/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.csv/src/org/eclipse/chemclipse/msd/converter/supplier/csv/core/ChromatogramExportConverter.java
@@ -13,9 +13,11 @@ package org.eclipse.chemclipse.msd.converter.supplier.csv.core;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
 import org.eclipse.chemclipse.logging.core.Logger;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
 import org.eclipse.chemclipse.msd.converter.supplier.csv.internal.converter.SpecificationValidator;
 import org.eclipse.chemclipse.msd.converter.supplier.csv.io.core.ChromatogramWriter;
@@ -23,20 +25,21 @@ import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter implements IChromatogramExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 	private static final String DESCRIPTION = "CSV Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		file = SpecificationValidator.validateSpecification(file, "csv");
 		IProcessingInfo processingInfo = super.validate(file);
-		if(!processingInfo.hasErrorMessages()) {
+		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD) {
+			IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
 			IChromatogramMSDWriter writer = new ChromatogramWriter();
 			try {
-				writer.writeChromatogram(file, chromatogram, monitor);
+				writer.writeChromatogram(file, chromatogramMSD, monitor);
 				processingInfo.setProcessingResult(file);
 			} catch(Exception e) {
 				logger.warn(e);
