d3e83855f98244311c9c6dda2a53d3a29b0ef65f
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-13 07:54:13 +0100

    The chromatogram converter handling has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/csd/converter/supplier/jcampdx/converter/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/csd/converter/supplier/jcampdx/converter/ChromatogramExportConverter.java
@@ -13,35 +13,38 @@ package org.eclipse.chemclipse.csd.converter.supplier.jcampdx.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.support.IConstants;
-import org.eclipse.chemclipse.csd.converter.chromatogram.AbstractChromatogramCSDExportConverter;
 import org.eclipse.chemclipse.csd.converter.io.IChromatogramCSDWriter;
 import org.eclipse.chemclipse.csd.converter.supplier.jcampdx.io.ChromatogramWriter;
 import org.eclipse.chemclipse.csd.model.core.IChromatogramCSD;
 import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.chemclipse.xxd.converter.supplier.jcampdx.internal.converter.SpecificationValidator;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramExportConverter extends AbstractChromatogramCSDExportConverter implements IChromatogramExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 	private static final String DESCRIPTION = "JCAMP-DX Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogramCSD chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		/*
 		 * Validate the file.
 		 */
 		file = SpecificationValidator.validateSpecification(file, "JDX");
 		IProcessingInfo processingInfo = super.validate(file);
-		if(!processingInfo.hasErrorMessages()) {
+		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramCSD) {
+			IChromatogramCSD chromatogramCSD = (IChromatogramCSD)chromatogram;
 			IChromatogramCSDWriter writer = new ChromatogramWriter();
 			monitor.subTask(IConstants.EXPORT_CHROMATOGRAM);
 			try {
-				writer.writeChromatogram(file, chromatogram, monitor);
+				writer.writeChromatogram(file, chromatogramCSD, monitor);
 				processingInfo.setProcessingResult(file);
 			} catch(Exception e) {
 				logger.warn(e);
--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/msd/converter/supplier/jcampdx/converter/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/msd/converter/supplier/jcampdx/converter/ChromatogramExportConverter.java
@@ -13,10 +13,12 @@ package org.eclipse.chemclipse.msd.converter.supplier.jcampdx.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.support.IConstants;
 import org.eclipse.chemclipse.logging.core.Logger;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
 import org.eclipse.chemclipse.msd.converter.supplier.jcampdx.io.ChromatogramWriter;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
@@ -24,21 +26,22 @@ import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.chemclipse.xxd.converter.supplier.jcampdx.internal.converter.SpecificationValidator;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter implements IChromatogramExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 	private static final String DESCRIPTION = "JCAMP-DX Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		file = SpecificationValidator.validateSpecification(file, "JDX");
 		IProcessingInfo processingInfo = super.validate(file);
-		if(!processingInfo.hasErrorMessages()) {
+		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD) {
+			IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
 			IChromatogramMSDWriter writer = new ChromatogramWriter();
 			monitor.subTask(IConstants.EXPORT_CHROMATOGRAM);
 			try {
-				writer.writeChromatogram(file, chromatogram, monitor);
+				writer.writeChromatogram(file, chromatogramMSD, monitor);
 				processingInfo.setProcessingResult(file);
 			} catch(Exception e) {
 				logger.warn(e);
