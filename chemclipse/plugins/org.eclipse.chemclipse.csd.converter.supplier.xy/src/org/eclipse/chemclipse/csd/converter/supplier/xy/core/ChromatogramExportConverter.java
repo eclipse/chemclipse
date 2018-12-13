6a41f48f666d5e14843fca2ffe2a7fd0e9fa4ec7
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-13 07:54:13 +0100

    The chromatogram converter handling has been improved.

--- chemclipse/plugins/org.eclipse.chemclipse.csd.converter.supplier.xy/src/org/eclipse/chemclipse/csd/converter/supplier/xy/core/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.csd.converter.supplier.xy/src/org/eclipse/chemclipse/csd/converter/supplier/xy/core/ChromatogramExportConverter.java
@@ -13,30 +13,33 @@ package org.eclipse.chemclipse.csd.converter.supplier.xy.core;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
-import org.eclipse.chemclipse.csd.converter.chromatogram.AbstractChromatogramCSDExportConverter;
 import org.eclipse.chemclipse.csd.converter.io.IChromatogramCSDWriter;
 import org.eclipse.chemclipse.csd.converter.supplier.xy.internal.support.SpecificationValidator;
 import org.eclipse.chemclipse.csd.converter.supplier.xy.io.ChromatogramWriter;
 import org.eclipse.chemclipse.csd.model.core.IChromatogramCSD;
 import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramExportConverter extends AbstractChromatogramCSDExportConverter implements IChromatogramExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 	private static final String DESCRIPTION = "XY Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogramCSD chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		file = SpecificationValidator.validateSpecification(file);
 		IProcessingInfo processingInfo = super.validate(file);
-		if(!processingInfo.hasErrorMessages()) {
+		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramCSD) {
+			IChromatogramCSD chromatogramCSD = (IChromatogramCSD)chromatogram;
 			IChromatogramCSDWriter writer = new ChromatogramWriter();
 			try {
-				writer.writeChromatogram(file, chromatogram, monitor);
+				writer.writeChromatogram(file, chromatogramCSD, monitor);
 			} catch(Exception e) {
 				logger.warn(e);
 				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
