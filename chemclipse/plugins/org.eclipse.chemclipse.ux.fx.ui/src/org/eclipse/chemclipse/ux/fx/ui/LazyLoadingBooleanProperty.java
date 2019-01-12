e7e204664eab08fa55f79ba6d7e48644b65ddd59
Alexander Kerner <alexanderkerner24@gmail.com>
2019-01-12 19:52:26 +0100

    Define loading and error values
    Define loading and error values for LazyLoadingBooleanProperty.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingBooleanProperty.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.fx.ui/src/org/eclipse/chemclipse/ux/fx/ui/LazyLoadingBooleanProperty.java
@@ -9,26 +9,41 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.fx.ui;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public abstract class LazyLoadingBooleanProperty extends LazyLoadingObjectProperty<Boolean> {
 
-	public LazyLoadingBooleanProperty() {
-		super();
+	private static final Logger logger = Logger.getLogger(LazyLoadingBooleanProperty.class);
+	public static final Boolean DEFAULT_LOADING_VALUE = Boolean.FALSE;
+	public static final Boolean DEFAULT_ERROR_VALUE = Boolean.FALSE;
 
+	public LazyLoadingBooleanProperty() {
+
+		super();
 	}
 
 	public LazyLoadingBooleanProperty(final Boolean initialValue) {
-		super(initialValue);
 
+		super(initialValue);
 	}
 
 	public LazyLoadingBooleanProperty(final Object bean, final String name, final Boolean initialValue) {
-		super(bean, name, initialValue);
 
+		super(bean, name, initialValue);
 	}
 
 	public LazyLoadingBooleanProperty(final Object bean, final String name) {
-		super(bean, name);
 
+		super(bean, name);
 	}
 
+	@Override
+	protected Boolean getFailedValue(final Throwable t) {
+
+		if(logger.isEnabledFor(Level.ERROR)) {
+			logger.error(t.getLocalizedMessage(), t);
+		}
+		return DEFAULT_ERROR_VALUE;
+	}
 }
