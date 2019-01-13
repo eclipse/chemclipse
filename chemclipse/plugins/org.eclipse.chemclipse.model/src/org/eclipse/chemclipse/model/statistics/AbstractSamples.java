8c20cbe9cb1d2892e5441e66027b33c2898a3760
Jan Holy <jan.holy91@gmail.com>
2019-01-13 10:50:38 +0100

    FOODAUTH-50 - move statistics object data model
    Change-Id: I5a34690d6ce1498e7398c534cb1b0959b8860d88
    Signed-off-by: Jan Holy <jan.holy91@gmail.com>

---
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/statistics/AbstractSamples.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2018 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Jan Holy - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.model.statistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractSamples<V extends IVariable, S extends ISample> implements ISamples<V, S> {
+
+	private List<S> samples;
+	private List<V> variables;
+
+	public AbstractSamples() {
+
+		samples = new ArrayList<>();
+		variables = new ArrayList<>();
+	}
+
+	@Override
+	public List<S> getSampleList() {
+
+		return samples;
+	}
+
+	@Override
+	public List<V> getVariables() {
+
+		return variables;
+	}
+}
