2be0cafb70850f5624a6e07ca3bf825feb8bd7d8
Jan Holy <jan.holy91@gmail.com>
2019-01-19 11:21:51 +0100

    remove multifile selection from settings dialog
    Change-Id: I89dadca475c14fac142c7eccc3cda663182ec60b
    Signed-off-by: Jan Holy <jan.holy91@gmail.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/ui/preferences/PreferencePage.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/ui/preferences/PreferencePage.java
@@ -13,7 +13,6 @@ package org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.ui.preferen
 
 import org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.ui.Activator;
-import org.eclipse.chemclipse.support.ui.preferences.editors.FileTableEditor;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
@@ -24,6 +23,7 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	public PreferencePage() {
+
 		super(GRID);
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
 		setDescription("PCA Preferences");
@@ -40,8 +40,6 @@ public class PreferencePage extends FieldEditorPreferencePage implements IWorkbe
 		addField(new ComboFieldEditor(PreferenceSupplier.P_ALGORITHM_TYPE, "Algorithm Type:", PreferenceSupplier.ALGORITHM_TYPES, getFieldEditorParent()));
 		addIntegerEditor(PreferenceSupplier.P_NUMBER_OF_COMPONENTS, "Number of Components", PreferenceSupplier.MIN_NUMBER_OF_COMPONENTS, PreferenceSupplier.MAX_NUMBER_OF_COMPONENTS);
 		addField(new BooleanFieldEditor(PreferenceSupplier.P_AUTO_REEVALUATE, "Auto Re-evaluate after Change", getFieldEditorParent()));
-		//
-		addField(new FileTableEditor("example", "MultiFileInput", new String[0], getFieldEditorParent()));
 	}
 
 	private void addIntegerEditor(String name, String labelText, int min, int max) {
