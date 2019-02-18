cfaff7d818aff3e8203604aa311784dc6e01798b
Alexander Kerner <alexanderkerner24@gmail.com>
2019-02-18 17:30:00 +0100

    [DEVELOP-1026] optimize generics

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.meannormalizer.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/meannormalizer/core/ChromatogramImporterTestCase.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.meannormalizer.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/meannormalizer/core/ChromatogramImporterTestCase.java
@@ -1,18 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2018 Lablicate GmbH.
+ * Copyright (c) 2008, 2018, 2019 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Alexander Kerner - Generics
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.meannormalizer.core;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.meannormalizer.PathResolver;
 import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.meannormalizer.TestPathHelper;
 import org.eclipse.chemclipse.msd.converter.chromatogram.ChromatogramConverterMSD;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
@@ -35,8 +37,8 @@ public class ChromatogramImporterTestCase extends TestCase {
 		/*
 		 * Import
 		 */
-		File fileImport = new File(TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_1));
-		org.eclipse.chemclipse.processing.core.IProcessingInfo processingInfo = ChromatogramConverterMSD.getInstance().convert(fileImport, EXTENSION_POINT_ID, new NullProgressMonitor());
+		File fileImport = new File(PathResolver.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_1));
+		org.eclipse.chemclipse.processing.core.IProcessingInfo<IChromatogramMSD> processingInfo = ChromatogramConverterMSD.getInstance().convert(fileImport, EXTENSION_POINT_ID, new NullProgressMonitor());
 		chromatogram = processingInfo.getProcessingResult(IChromatogramMSD.class);
 		chromatogramSelection = new ChromatogramSelectionMSD(chromatogram);
 	}
