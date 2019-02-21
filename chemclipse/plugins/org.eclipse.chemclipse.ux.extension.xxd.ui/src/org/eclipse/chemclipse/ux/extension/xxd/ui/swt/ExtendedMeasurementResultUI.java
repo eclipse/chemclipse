e32da4ffef1396626b2917e7bbcb74a573b224c7
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-02-21 16:57:54 +0100

    This measurement results view drives me crazy. Catched exception.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/ExtendedMeasurementResultUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/ExtendedMeasurementResultUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -126,10 +126,14 @@ public class ExtendedMeasurementResultUI {
 
 	public void update(Object object) {
 
-		fetchMeasurementResults(object);
-		updateChromatogramInfo(object);
-		resultProviderId = "";
-		updateMeasurementResults();
+		try {
+			fetchMeasurementResults(object);
+			updateChromatogramInfo(object);
+			resultProviderId = "";
+			updateMeasurementResults();
+		} catch(Exception e) {
+			logger.warn(e);
+		}
 	}
 
 	private void initialize(Composite parent) {
