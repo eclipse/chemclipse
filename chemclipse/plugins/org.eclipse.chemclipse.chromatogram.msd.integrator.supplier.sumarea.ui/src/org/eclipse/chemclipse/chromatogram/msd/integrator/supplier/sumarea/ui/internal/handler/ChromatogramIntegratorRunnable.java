beb0998d248e8d3cf3b4d375d97c1a7b46c16ea1
Alexander Kerner <alexanderkerner24@gmail.com>
2019-03-09 18:05:01 +0100

    Squashed commit of the following:
    commit 72d18b014e25b5233929e8056e93cfe99e25ce9c
    Author: Alexander Kerner <alexanderkerner24@gmail.com>
    Date:   Sat Feb 23 13:35:04 2019 +0100
    
        [fix] DEVELOP-1026
    
    commit 58f4565011ef8f814ace6fc904838c391b75742d
    Author: Alexander Kerner <alexanderkerner24@gmail.com>
    Date:   Sat Feb 23 11:52:47 2019 +0100
    
        [fix] DEVELOP-1026
    
    Change-Id: Iec135131d3759065da394d4c538b80bd0e0e827d

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.integrator.supplier.sumarea.ui/src/org/eclipse/chemclipse/chromatogram/msd/integrator/supplier/sumarea/ui/internal/handler/ChromatogramIntegratorRunnable.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.integrator.supplier.sumarea.ui/src/org/eclipse/chemclipse/chromatogram/msd/integrator/supplier/sumarea/ui/internal/handler/ChromatogramIntegratorRunnable.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 Lablicate GmbH.
- * 
+ * Copyright (c) 2011, 2018, 2019 Lablicate GmbH.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Alexander Kerner - Generics
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.msd.integrator.supplier.sumarea.ui.internal.handler;
 
@@ -19,13 +20,13 @@ import org.eclipse.chemclipse.chromatogram.xxd.integrator.ui.notifier.Integratio
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
-import org.eclipse.chemclipse.processing.core.exceptions.TypeCastException;
 import org.eclipse.chemclipse.processing.ui.support.ProcessingInfoViewSupport;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 
 public class ChromatogramIntegratorRunnable implements IRunnableWithProgress {
 
+	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(ChromatogramIntegratorRunnable.class);
 	private static final String CHROMATOGRAM_INTEGRATOR_ID = "org.eclipse.chemclipse.chromatogram.msd.integrator.supplier.sumarea.chromatogramIntegrator";
 	@SuppressWarnings("rawtypes")
@@ -42,17 +43,11 @@ public class ChromatogramIntegratorRunnable implements IRunnableWithProgress {
 		try {
 			monitor.beginTask("Sumarea Integrator", IProgressMonitor.UNKNOWN);
 			//
-			IProcessingInfo processingInfo = ChromatogramIntegrator.integrate(chromatogramSelection, CHROMATOGRAM_INTEGRATOR_ID, monitor);
+			IProcessingInfo<IChromatogramIntegrationResults> processingInfo = ChromatogramIntegrator.integrate(chromatogramSelection, CHROMATOGRAM_INTEGRATOR_ID, monitor);
 			ProcessingInfoViewSupport.updateProcessingInfo(processingInfo, false);
-			/*
-			 * Try to set the results.
-			 */
-			try {
-				IChromatogramIntegrationResults chromatogramIntegrationResults = processingInfo.getProcessingResult(IChromatogramIntegrationResults.class);
-				IntegrationResultUpdateNotifier.fireUpdateChange(chromatogramIntegrationResults);
-			} catch(TypeCastException e) {
-				logger.warn(e);
-			}
+			IChromatogramIntegrationResults chromatogramIntegrationResults = processingInfo.getProcessingResult();
+			IntegrationResultUpdateNotifier.fireUpdateChange(chromatogramIntegrationResults);
+
 		} finally {
 			monitor.done();
 		}
