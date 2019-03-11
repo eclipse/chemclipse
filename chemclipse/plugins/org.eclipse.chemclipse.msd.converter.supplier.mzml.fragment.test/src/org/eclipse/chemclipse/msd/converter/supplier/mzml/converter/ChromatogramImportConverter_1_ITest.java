4a799dd3cfac5a506e2db1dc7969e831e92cee46
kerner1000 <alexanderkerner24@gmail.com>
2019-03-11 19:02:24 +0100

    [fix] DEVELOP-1026
    Change-Id: Id42500c8407d7e02181918afb06b142f1042b681

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzml.fragment.test/src/org/eclipse/chemclipse/msd/converter/supplier/mzml/converter/ChromatogramImportConverter_1_ITest.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzml.fragment.test/src/org/eclipse/chemclipse/msd/converter/supplier/mzml/converter/ChromatogramImportConverter_1_ITest.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2018 Lablicate GmbH.
- * 
+ * Copyright (c) 2013, 2019 Lablicate GmbH.
+ *
  * All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Alexander Kerner - Generics
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.converter.supplier.mzml.converter;
 
@@ -32,7 +33,7 @@ public class ChromatogramImportConverter_1_ITest extends TestCase {
 		super.setUp();
 		importFile = new File(TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_1));
 		ChromatogramImportConverter converter = new ChromatogramImportConverter();
-		IProcessingInfo processingInfo = converter.convert(importFile, new NullProgressMonitor());
+		IProcessingInfo<IChromatogramMSD> processingInfo = converter.convert(importFile, new NullProgressMonitor());
 		chromatogram = processingInfo.getProcessingResult(IChromatogramMSD.class);
 	}
 
