56da764e66a7811851129c5040cebecda7eb3a37
kerner1000 <alexanderkerner24@gmail.com>
2019-03-11 19:06:12 +0100

    [fix] DEVELOP-1026
    Change-Id: I3b507d69289b8dc8ec6d303d6663d4d6594e242f

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.noise.dyson.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/noise/dyson/core/ChromatogramReaderTestCase.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.noise.dyson.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/noise/dyson/core/ChromatogramReaderTestCase.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Lablicate GmbH.
- * 
+ * Copyright (c) 2014, 2019 Lablicate GmbH.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Alexander Kerner - Generics
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.noise.dyson.core;
 
@@ -34,7 +35,7 @@ public class ChromatogramReaderTestCase extends TestCase {
 
 		super.setUp();
 		fileImport = new File(this.pathImport);
-		org.eclipse.chemclipse.processing.core.IProcessingInfo processingInfo = ChromatogramConverterMSD.getInstance().convert(fileImport, EXTENSION_POINT_ID, new NullProgressMonitor());
+		org.eclipse.chemclipse.processing.core.IProcessingInfo<IChromatogramMSD> processingInfo = ChromatogramConverterMSD.getInstance().convert(fileImport, EXTENSION_POINT_ID, new NullProgressMonitor());
 		chromatogram = processingInfo.getProcessingResult(IChromatogramMSD.class);
 	}
 
