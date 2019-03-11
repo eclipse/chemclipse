6ed6e2bd8e7c1a082949b3a9efe852183e78052b
kerner1000 <alexanderkerner24@gmail.com>
2019-03-11 19:08:32 +0100

    [fix] DEVELOP-1026
    Change-Id: I0c088b8ccbbc511af11c9f238ae553f61fa92384

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.fragment.test/src/org/eclipse/chemclipse/chromatogram/msd/peak/detector/supplier/deconvolution/core/ChromatogramImporterTestCase.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.fragment.test/src/org/eclipse/chemclipse/chromatogram/msd/peak/detector/supplier/deconvolution/core/ChromatogramImporterTestCase.java
@@ -1,26 +1,26 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2018 Lablicate GmbH.
- * 
+ * Copyright (c) 2013, 2019 Lablicate GmbH.
+ *
  * All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Alexander Kerner - Generics
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.core;
 
 import java.io.File;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-
+import org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.TestPathHelper;
 import org.eclipse.chemclipse.msd.converter.chromatogram.ChromatogramConverterMSD;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
 import org.eclipse.chemclipse.msd.model.core.selection.ChromatogramSelectionMSD;
 import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
-import org.eclipse.chemclipse.chromatogram.msd.peak.detector.supplier.deconvolution.TestPathHelper;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 import junit.framework.TestCase;
 
@@ -38,7 +38,7 @@ public class ChromatogramImporterTestCase extends TestCase {
 		 * Import
 		 */
 		File fileImport = new File(TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_1));
-		IProcessingInfo processingInfo = ChromatogramConverterMSD.getInstance().convert(fileImport, EXTENSION_POINT_ID, new NullProgressMonitor());
+		IProcessingInfo<IChromatogramMSD> processingInfo = ChromatogramConverterMSD.getInstance().convert(fileImport, EXTENSION_POINT_ID, new NullProgressMonitor());
 		chromatogram = processingInfo.getProcessingResult(IChromatogramMSD.class);
 		chromatogramSelection = new ChromatogramSelectionMSD(chromatogram);
 	}
