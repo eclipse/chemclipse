6e2d270a0b691c890dfc2f740c65e41a9c7a4384
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-03-14 22:50:45 +0100

    EUWG-100 - a simple quantitation report has been added.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom/src/org/eclipse/chemclipse/chromatogram/xxd/report/supplier/openchrom/core/Report3.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.chemclipse.chromatogram.xxd.report.settings.IChromatogramReportSettings;
+import org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom.internal.support.SpecificationValidator;
+import org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom.io.ReportWriter3;
+import org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom.settings.ReportSettings;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class Report3 extends AbstractReport {
+
+	private static final Logger logger = Logger.getLogger(Report3.class);
+
+	@Override
+	public IProcessingInfo report(File file, boolean append, List<IChromatogram<? extends IPeak>> chromatograms, IChromatogramReportSettings settings, IProgressMonitor monitor) {
+
+		file = SpecificationValidator.validateSpecification(file);
+		IProcessingInfo processingInfo = super.validate(file);
+		//
+		if(!processingInfo.hasErrorMessages()) {
+			if(settings instanceof ReportSettings) {
+				ReportSettings reportSettings = (ReportSettings)settings;
+				ReportWriter3 chromatogramReport = new ReportWriter3();
+				try {
+					chromatogramReport.generate(file, append, chromatograms, reportSettings, monitor);
+					processingInfo.setProcessingResult(file);
+				} catch(IOException e) {
+					logger.warn(e);
+					processingInfo.addErrorMessage("ChemClipse Chromatogram Report", "The report couldn't be created. An error occured.");
+				}
+			} else {
+				logger.warn("The settings are not of type: " + ReportSettings.class);
+			}
+		}
+		return processingInfo;
+	}
+}
