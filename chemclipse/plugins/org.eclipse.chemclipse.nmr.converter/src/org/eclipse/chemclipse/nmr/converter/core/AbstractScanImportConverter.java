8729d4bd0f29a337c77d4fdc60fa97a1e3f2795c
laeubi <laeubi@laeubi-soft.de>
2019-03-25 15:51:38 +0000

    Add generics
    Change-Id: I202b10978e6b9375d4d22c29eeac5c4319759a03
    Signed-off-by: laeubi <laeubi@laeubi-soft.de>

--- chemclipse/plugins/org.eclipse.chemclipse.nmr.converter/src/org/eclipse/chemclipse/nmr/converter/core/AbstractScanImportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.nmr.converter/src/org/eclipse/chemclipse/nmr/converter/core/AbstractScanImportConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -13,5 +13,5 @@ package org.eclipse.chemclipse.nmr.converter.core;
 
 import org.eclipse.chemclipse.converter.core.AbstractImportConverter;
 
-public abstract class AbstractScanImportConverter extends AbstractImportConverter implements IScanImportConverter {
+public abstract class AbstractScanImportConverter<T> extends AbstractImportConverter implements IScanImportConverter<T> {
 }
