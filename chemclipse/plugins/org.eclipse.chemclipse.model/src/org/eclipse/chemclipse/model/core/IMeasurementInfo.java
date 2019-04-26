c7e18cc1a47568edd08d84ac30a4f0f3dcc24c03
Alexander Kerner <alexkerner1000@gmail.com>
2019-04-26 16:32:35 +0200

    Revert "IMeasurementInfo does not extend Serializable"
    This reverts commit 8990a3a3454a556a940b6ae29277cb62e32140ab.
    
    Change-Id: Ic0b4b4629384da56e7ee476cb6961a0cbe38d8f2

--- chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/core/IMeasurementInfo.java
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/core/IMeasurementInfo.java
@@ -1,22 +1,23 @@
 /*******************************************************************************
  * Copyright (c) 2013, 2018 Lablicate GmbH.
- *
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.model.core;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
 import org.eclipse.chemclipse.model.exceptions.InvalidHeaderModificationException;
 
-public interface IMeasurementInfo {
+public interface IMeasurementInfo extends Serializable {
 
 	String getHeaderData(String key);
 
@@ -31,7 +32,7 @@ public interface IMeasurementInfo {
 	/**
 	 * This map is unmodifiable. Please use the
 	 * setValue method to add values.
-	 *
+	 * 
 	 * @return Map<String, String>
 	 */
 	Map<String, String> getHeaderDataMap();
