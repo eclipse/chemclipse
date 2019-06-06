0997f6da343325ceb958e989d23b9e843bd6a14c
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-06-06 20:35:02 +0200

    The old mass spectrum charts have been removed.

--- chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/events/IPerspectiveAndViewIds.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/events/IPerspectiveAndViewIds.java
@@ -38,7 +38,6 @@ public interface IPerspectiveAndViewIds {
 	 */
 	String VIEW_CHROMATOGRAM_OVERLAY = "org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.ui.part.chromatogramOverlay";
 	String VIEW_CHROMATOGRAM_OVERLAY_SELECTED_IONS = "org.eclipse.chemclipse.ux.extension.msd.ui.part.chromatogramOverlaySelectedIons";
-	String VIEW_SCAN_NOMINAL = "org.eclipse.chemclipse.ux.extension.msd.ui.part.scanNominal";
 	String VIEW_PEAK_TARGETS_MSD = "org.eclipse.chemclipse.ux.extension.msd.ui.part.peakTargetsView";
 	String VIEW_PEAK_TARGETS_CSD = "org.eclipse.chemclipse.ux.extension.csd.ui.part.peakTargetsView";
 	String VIEW_SELECTED_ION_CHROMATOGRAM_NOMINAL = "org.eclipse.chemclipse.ux.extension.msd.ui.part.selectedNominalIonChromtogramView";
