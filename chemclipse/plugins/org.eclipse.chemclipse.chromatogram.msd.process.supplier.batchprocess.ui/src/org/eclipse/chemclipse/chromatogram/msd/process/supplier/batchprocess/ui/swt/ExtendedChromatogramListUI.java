9cab2857c064746048daa826029c7cb8c04ad17a
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-06-11 15:01:05 +0200

    Parts that are not used any longer have been removed.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.process.supplier.batchprocess.ui/src/org/eclipse/chemclipse/chromatogram/msd/process/supplier/batchprocess/ui/swt/ExtendedChromatogramListUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.process.supplier.batchprocess.ui/src/org/eclipse/chemclipse/chromatogram/msd/process/supplier/batchprocess/ui/swt/ExtendedChromatogramListUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -220,7 +220,7 @@ public class ExtendedChromatogramListUI extends Composite {
 			public void widgetSelected(SelectionEvent e) {
 
 				if(batchProcessJob != null) {
-					InputWizardSettings inputWizardSettings = new InputWizardSettings(new InputWizardSettings.DataType[]{InputWizardSettings.DataType.MSD_CHROMATOGRAM, InputWizardSettings.DataType.CSD_CHROMATOGRAM, InputWizardSettings.DataType.WSD_CHROMATOGRAM});
+					InputWizardSettings inputWizardSettings = new InputWizardSettings(new InputWizardSettings.InputDataType[]{InputWizardSettings.InputDataType.MSD_CHROMATOGRAM, InputWizardSettings.InputDataType.CSD_CHROMATOGRAM, InputWizardSettings.InputDataType.WSD_CHROMATOGRAM});
 					inputWizardSettings.setTitle("Chromatogram");
 					inputWizardSettings.setDescription("Select chromatogram(s) to analyze.");
 					inputWizardSettings.setPathPreferences(PreferenceSupplier.INSTANCE().getPreferences(), PreferenceSupplier.P_FILTER_PATH_IMPORT_RECORDS);
