1e09aa174788c55bcf6e709fe97bb4a9c4fbfe0e
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-06-11 14:56:32 +0200

    Parts that are not used any longer have been removed.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.manual.ui/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/manual/ui/handlers/DetectorHandlerMSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.manual.ui/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/manual/ui/handlers/DetectorHandlerMSD.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 Lablicate GmbH.
+ * Copyright (c) 2011, 2019 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,11 +16,10 @@ import java.util.List;
 
 import javax.inject.Named;
 
+import org.eclipse.chemclipse.rcp.app.ui.handlers.PerspectiveSwitchHandler;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
-
-import org.eclipse.chemclipse.rcp.app.ui.handlers.PerspectiveSwitchHandler;
 
 public class DetectorHandlerMSD {
 
@@ -32,7 +31,6 @@ public class DetectorHandlerMSD {
 		 */
 		String perspectiveId = "org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.manual.ui.perspective";
 		List<String> viewIds = new ArrayList<String>();
-		viewIds.add("org.eclipse.chemclipse.ux.extension.msd.ui.part.peakListView");
 		viewIds.add("org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.manual.ui.views.manualDetectedPeakMSD");
 		viewIds.add("org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.manual.ui.views.chromatogramSelectionView");
 		PerspectiveSwitchHandler.focusPerspectiveAndView(perspectiveId, viewIds);
