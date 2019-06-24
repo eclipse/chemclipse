09406d0fd28f31ffc92ea2bb0adc3e3fad5322ff
laeubi <laeubi@laeubi-soft.de>
2019-06-24 09:59:27 +0000

    enable Multiselect
    Change-Id: I239824c1c7eb6df531337b767bfe1aba72a237ce
    Signed-off-by: laeubi <laeubi@laeubi-soft.de>

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/MethodListUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/MethodListUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,12 +8,14 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Christoph Läubrich - force SWT#FULL_SELECTION
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.xxd.ui.swt;
 
 import org.eclipse.chemclipse.support.ui.provider.ListContentProvider;
 import org.eclipse.chemclipse.support.ui.swt.ExtendedTableViewer;
 import org.eclipse.chemclipse.ux.extension.xxd.ui.internal.provider.MethodListLabelProvider;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 public class MethodListUI extends ExtendedTableViewer {
@@ -23,7 +25,7 @@ public class MethodListUI extends ExtendedTableViewer {
 	private int[] bounds = MethodListLabelProvider.BOUNDS;
 
 	public MethodListUI(Composite parent, int style) {
-		super(parent, style);
+		super(parent, style | SWT.FULL_SELECTION);
 		createColumns();
 	}
 
