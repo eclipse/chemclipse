f1e93a0559e5b67df88295554abff275f732ee36
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-09-26 16:22:18 +0200

    Fix preference page for change #29

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/preferences/PreferencePageMethods.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/preferences/PreferencePageMethods.java
@@ -11,13 +11,16 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.xxd.ui.preferences;
 
+import org.eclipse.chemclipse.support.preferences.IPreferenceSupplier;
 import org.eclipse.chemclipse.ux.extension.xxd.ui.Activator;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 public class PreferencePageMethods extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
@@ -28,15 +31,20 @@ public class PreferencePageMethods extends FieldEditorPreferencePage implements 
 		setDescription("");
 	}
 
+	@Override
 	public void createFieldEditors() {
 
-		addField(new DirectoryFieldEditor(PreferenceConstants.P_METHOD_EXPLORER_PATH_ROOT_FOLDER, "Method Folder", getFieldEditorParent()));
+		IPreferenceSupplier methodSupplier = org.eclipse.chemclipse.converter.preferences.PreferenceSupplier.INSTANCE();
+		DirectoryFieldEditor editor = new DirectoryFieldEditor(org.eclipse.chemclipse.converter.preferences.PreferenceSupplier.P_METHOD_EXPLORER_PATH_ROOT_FOLDER, "Method Folder", getFieldEditorParent());
+		editor.setPreferenceStore(new ScopedPreferenceStore(InstanceScope.INSTANCE, methodSupplier.getPreferenceNode()));
+		addField(editor);
 		addField(new StringFieldEditor(PreferenceConstants.P_SELECTED_METHOD_NAME, "Method Name (Selected)", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(PreferenceConstants.P_METHOD_PROCESSOR_SELECTION_CSD, "Show method processor items (CSD)", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(PreferenceConstants.P_METHOD_PROCESSOR_SELECTION_MSD, "Show method processor items (MSD)", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(PreferenceConstants.P_METHOD_PROCESSOR_SELECTION_WSD, "Show method processor items (WSD)", getFieldEditorParent()));
 	}
 
+	@Override
 	public void init(IWorkbench workbench) {
 
 	}
