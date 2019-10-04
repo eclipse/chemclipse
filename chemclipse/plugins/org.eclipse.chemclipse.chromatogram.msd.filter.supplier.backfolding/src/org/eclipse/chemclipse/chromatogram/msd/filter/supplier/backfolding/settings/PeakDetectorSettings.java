03be24c57bb14026ca1f1924e92415265a053250
Alexander Kerner <alexkerner1000@gmail.com>
2019-10-04 16:04:06 +0200

    Align to API changes

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/backfolding/settings/PeakDetectorSettings.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding/src/org/eclipse/chemclipse/chromatogram/msd/filter/supplier/backfolding/settings/PeakDetectorSettings.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 Lablicate GmbH.
+ * Copyright (c) 2011, 2019 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -8,10 +8,15 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Dr. Alexander Kerner - implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.msd.filter.supplier.backfolding.settings;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.eclipse.chemclipse.chromatogram.msd.peak.detector.settings.AbstractPeakDetectorSettingsMSD;
+import org.eclipse.chemclipse.numeric.statistics.WindowSize;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -21,8 +26,15 @@ public class PeakDetectorSettings extends AbstractPeakDetectorSettingsMSD {
 	private IBackfoldingSettings backfoldingSettings;
 	@JsonIgnore
 	private Threshold threshold = Threshold.MEDIUM;
+	@JsonIgnore
+	private WindowSize windowSize = WindowSize.WIDTH_5;
+	@JsonIgnore
+	private Collection<Number> filterIons = new ArrayList<>();
+	@JsonIgnore
+	private FilterMode filterMode = FilterMode.EXCLUDE;
 
 	public PeakDetectorSettings() {
+
 		backfoldingSettings = new BackfoldingSettings();
 	}
 
@@ -41,5 +53,38 @@ public class PeakDetectorSettings extends AbstractPeakDetectorSettingsMSD {
 		if(threshold != null) {
 			this.threshold = threshold;
 		}
+	}
+
+	@Override
+	public WindowSize getMovingAverageWindowSize() {
+
+		return windowSize;
+	}
+
+	public void setMovingAverageWindowSize(WindowSize windowSize) {
+
+		this.windowSize = windowSize;
+	}
+
+	@Override
+	public FilterMode getFilterMode() {
+
+		return filterMode;
+	}
+
+	public void setFilterMode(FilterMode filterMode) {
+
+		this.filterMode = filterMode;
+	}
+
+	@Override
+	public Collection<Number> getFilterIon() {
+
+		return filterIons;
+	}
+
+	public void setFilterIon(Collection<Number> filterIon) {
+
+		this.filterIons = filterIon;
 	}
 }
