e413793d0cf83c9f0e0075b1d5247ca4aa9c20cf
Alexander Kerner <alexander.kerner@lablicate.com>
2019-10-10 17:05:17 +0200

    Fix DEVELOP-1374

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.firstderivative/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/firstderivative/settings/PeakDetectorSettingsMSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.firstderivative/src/org/eclipse/chemclipse/chromatogram/xxd/peak/detector/supplier/firstderivative/settings/PeakDetectorSettingsMSD.java
@@ -12,7 +12,120 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.peak.detector.supplier.firstderivative.settings;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.chemclipse.chromatogram.msd.peak.detector.Threshold;
 import org.eclipse.chemclipse.chromatogram.msd.peak.detector.settings.AbstractPeakDetectorSettingsMSD;
+import org.eclipse.chemclipse.chromatogram.peak.detector.core.FilterMode;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.numeric.statistics.WindowSize;
+import org.eclipse.chemclipse.support.settings.EnumSelectionRadioButtonsSettingProperty;
+import org.eclipse.chemclipse.support.settings.EnumSelectionSettingProperty;
+import org.eclipse.chemclipse.support.settings.FloatSettingsProperty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 public class PeakDetectorSettingsMSD extends AbstractPeakDetectorSettingsMSD {
+
+	private static final Logger logger = Logger.getLogger(PeakDetectorSettingsMSD.class);
+	@JsonProperty(value = "Threshold", defaultValue = "MEDIUM")
+	@EnumSelectionRadioButtonsSettingProperty
+	private Threshold threshold = Threshold.MEDIUM;
+	@JsonProperty(value = "Include Background (VV: true, BV|VB: false)", defaultValue = "false")
+	private boolean includeBackground = false;
+	@JsonProperty(value = "Min S/N Ratio", defaultValue = "0")
+	@FloatSettingsProperty(minValue = 0f, maxValue = Float.MAX_VALUE)
+	private float minimumSignalToNoiseRatio;
+	@JsonProperty(value = "Window Size", defaultValue = "WIDTH_5")
+	@JsonPropertyDescription(value = "Window Size: 3, 5, 7, ..., 45")
+	@EnumSelectionSettingProperty
+	private WindowSize windowSize = WindowSize.WIDTH_5;
+	@JsonProperty(value = "Ion Filter Mode", defaultValue = "EXCLUDE")
+	@EnumSelectionRadioButtonsSettingProperty
+	private FilterMode filterMode = FilterMode.EXCLUDE;
+	@JsonProperty(value = "Filter Ions", defaultValue = "")
+	private String filterIonsString;
+
+	public boolean isIncludeBackground() {
+
+		return includeBackground;
+	}
+
+	public void setIncludeBackground(boolean includeBackground) {
+
+		this.includeBackground = includeBackground;
+	}
+
+	public Threshold getThreshold() {
+
+		return threshold;
+	}
+
+	public void setThreshold(Threshold threshold) {
+
+		this.threshold = threshold;
+	}
+
+	public float getMinimumSignalToNoiseRatio() {
+
+		return minimumSignalToNoiseRatio;
+	}
+
+	public void setMinimumSignalToNoiseRatio(float minimumSignalToNoiseRatio) {
+
+		this.minimumSignalToNoiseRatio = minimumSignalToNoiseRatio;
+	}
+
+	public WindowSize getMovingAverageWindowSize() {
+
+		return windowSize;
+	}
+
+	public void setMovingAverageWindowSize(WindowSize windowSize) {
+
+		this.windowSize = windowSize;
+	}
+
+	public FilterMode getFilterMode() {
+
+		return filterMode;
+	}
+
+	public void setFilterMode(FilterMode filterMode) {
+
+		this.filterMode = filterMode;
+	}
+
+	public String getFilterIonsString() {
+
+		return filterIonsString;
+	}
+
+	public void setFilterIonsString(String filterIonsString) {
+
+		this.filterIonsString = filterIonsString;
+	}
+
+	public Collection<Number> getFilterIon() {
+
+		return parseIons(filterIonsString);
+	}
+
+	static Collection<Number> parseIons(String filterIonsString) {
+
+		List<Number> ionNumbers = new ArrayList<>();
+		String[] split = filterIonsString.trim().split("[\\s.,;]+");
+		for(String s : split) {
+			try {
+				ionNumbers.add(new BigDecimal(s));
+			} catch(NumberFormatException e) {
+				logger.debug("Failed to parse valid input from " + s);
+			}
+		}
+		return ionNumbers;
+	}
 }
