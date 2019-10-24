aa330329c3c4b4903d572cd64e0281e0633e6a8a
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-10-24 10:25:04 +0200

    #75 remove more empty tests

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/process/supplier/pca/core/AllTest.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Christoph Läubrich - initial API and implementation
+*******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.process.supplier.pca.core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({PcaUtilsTest4.class, PeakExtractionSupportTest_1_4.class, ScansExtractionSupportTest1_4.class, ScansExtractionSupportTest2_4.class, ScansExtractionSupportTest3_4.class, ScansExtractionSupportTest4_4.class})
+public class AllTest {
+}
