a0a02471b056203d330a49568ce4ee1fdf917830
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-11-16 19:24:28 +0100

    #112 add missing class

---
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/results/AnalysisSegmentMeasurementResult.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.model.results;
+
+import java.util.List;
+
+import org.eclipse.chemclipse.model.core.IMeasurementResult;
+import org.eclipse.chemclipse.model.support.IAnalysisSegment;
+
+public abstract class AnalysisSegmentMeasurementResult<T extends IAnalysisSegment> implements IMeasurementResult<List<T>> {
+
+	@Override
+	public String getIdentifier() {
+
+		return getClass().getName();
+	}
+
+	@Override
+	public String getDescription() {
+
+		return "";
+	}
+}
