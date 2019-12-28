87ba69f9159a4ace4ba07dc912b276844ba9cb62
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-12-28 14:03:11 +0100

    Fixed #183 - Extend the time range support

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/ranges/TimeRangesUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/ranges/TimeRangesUI.java
@@ -11,7 +11,6 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.xxd.ui.ranges;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,21 +20,14 @@ import org.eclipse.chemclipse.model.ranges.TimeRanges;
 import org.eclipse.chemclipse.model.updates.IUpdateListener;
 import org.eclipse.chemclipse.rcp.ui.icons.core.ApplicationImageFactory;
 import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImage;
-import org.eclipse.chemclipse.support.text.ValueFormat;
 import org.eclipse.chemclipse.support.ui.provider.AbstractLabelProvider;
 import org.eclipse.chemclipse.ux.extension.xxd.ui.internal.validation.TimeRangeInputValidator;
-import org.eclipse.core.databinding.validation.IValidator;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.fieldassist.ControlDecoration;
-import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyAdapter;
-import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -43,14 +35,13 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 
 public class TimeRangesUI extends Composite {
 
 	private ComboViewer comboViewer;
-	private Text textStart;
-	private Text textCenter;
-	private Text textStop;
+	private TimeSpinner timeSpinnerStart;
+	private TimeSpinner timeSpinnerCenter;
+	private TimeSpinner timeSpinnerStop;
 	private Button buttonAdd;
 	private Button buttonDelete;
 	/*
@@ -59,8 +50,6 @@ public class TimeRangesUI extends Composite {
 	 */
 	private TimeRanges timeRanges = null;
 	private TimeRange timeRange = null;
-	//
-	private DecimalFormat decimalFormat = ValueFormat.getDecimalFormatEnglish();
 	//
 	private IUpdateListener updateListener = null;
 
@@ -75,31 +64,31 @@ public class TimeRangesUI extends Composite {
 		updateInput();
 	}
 
-	public void setUpdateListener(IUpdateListener updateListener) {
-
-		this.updateListener = updateListener;
-	}
-
 	/**
 	 * Updates the values of the currently selected time range.
 	 */
 	public void update() {
 
-		updateTimeRange(timeRange);
+		updateTimeRange();
+	}
+
+	public void setUpdateListener(IUpdateListener updateListener) {
+
+		this.updateListener = updateListener;
 	}
 
 	private void createControl() {
 
-		GridLayout gridLayout = new GridLayout(6, false);
+		GridLayout gridLayout = new GridLayout(7, false);
 		gridLayout.marginWidth = 0;
 		gridLayout.marginLeft = 0;
 		gridLayout.marginRight = 0;
 		setLayout(gridLayout);
 		//
 		comboViewer = createComboViewer(this);
-		textStart = createText(this, TimeRange.Marker.START);
-		textCenter = createText(this, TimeRange.Marker.CENTER);
-		textStop = createText(this, TimeRange.Marker.STOP);
+		timeSpinnerStart = createSpinner(this, TimeRange.Marker.START);
+		timeSpinnerCenter = createSpinner(this, TimeRange.Marker.CENTER);
+		timeSpinnerStop = createSpinner(this, TimeRange.Marker.STOP);
 		buttonAdd = createButtonAdd(this);
 		buttonDelete = createButtonDelete(this);
 	}
@@ -124,7 +113,7 @@ public class TimeRangesUI extends Composite {
 		/*
 		 * Select the item.
 		 */
-		combo.setToolTipText("Select an identifier.");
+		combo.setToolTipText("Select a time range.");
 		combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		combo.addSelectionListener(new SelectionAdapter() {
 
@@ -133,8 +122,8 @@ public class TimeRangesUI extends Composite {
 
 				Object object = comboViewer.getStructuredSelection().getFirstElement();
 				if(object instanceof TimeRange) {
-					TimeRange timeRange = (TimeRange)object;
-					updateTimeRange(timeRange);
+					timeRange = (TimeRange)object;
+					updateTimeRange();
 				}
 			}
 		});
@@ -142,54 +131,19 @@ public class TimeRangesUI extends Composite {
 		return comboViewer;
 	}
 
-	private Text createText(Composite composite, TimeRange.Marker marker) {
+	private TimeSpinner createSpinner(Composite composite, TimeRange.Marker marker) {
 
-		Text text = new Text(composite, SWT.BORDER);
-		text.setText("");
-		switch(marker) {
-			case START:
-				text.setToolTipText("Start Retention Time");
-				break;
-			case CENTER:
-				text.setToolTipText("Center Retention Time");
-				break;
-			case STOP:
-				text.setToolTipText("Stop Retention Time");
-				break;
-		}
-		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		//
-		TimeRangeValidator validator = new TimeRangeValidator(marker);
-		ControlDecoration controlDecoration = new ControlDecoration(text, SWT.LEFT | SWT.TOP);
-		//
-		text.addKeyListener(new KeyAdapter() {
+		TimeSpinner timeSpinner = new TimeSpinner(composite, SWT.NONE, marker);
+		timeSpinner.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		timeSpinner.setUpdateListener(new IUpdateListener() {
 
 			@Override
-			public void keyReleased(KeyEvent e) {
+			public void update() {
 
-				super.keyReleased(e);
-				if(timeRange != null) {
-					validator.setTimeRange(timeRange);
-					if(validate(validator, controlDecoration, text)) {
-						switch(marker) {
-							case START:
-								timeRange.updateStart(validator.getRetentionTime());
-								break;
-							case CENTER:
-								timeRange.updateCenter(validator.getRetentionTime());
-								break;
-							case STOP:
-								timeRange.updateStop(validator.getRetentionTime());
-								break;
-						}
-						//
-						fireUpdate();
-					}
-				}
+				fireUpdate();
 			}
 		});
-		//
-		return text;
+		return timeSpinner;
 	}
 
 	private Button createButtonAdd(Composite parent) {
@@ -207,12 +161,13 @@ public class TimeRangesUI extends Composite {
 					InputDialog dialog = new InputDialog(e.display.getActiveShell(), "Time Range", "Create a new time range.", "C10 | 10.2 | 10.4 | 10.6", new TimeRangeInputValidator(timeRanges.keySet()));
 					if(IDialogConstants.OK_ID == dialog.open()) {
 						String item = dialog.getValue();
-						TimeRange timeRange = timeRanges.extractTimeRange(item);
-						if(timeRange != null) {
-							timeRanges.add(timeRange);
+						TimeRange timeRangeNew = timeRanges.extractTimeRange(item);
+						if(timeRangeNew != null) {
+							timeRanges.add(timeRangeNew);
 							updateInput();
-							comboViewer.getCombo().setText(timeRange.getIdentifier());
-							updateTimeRange(timeRange);
+							comboViewer.getCombo().setText(timeRangeNew.getIdentifier());
+							timeRange = timeRangeNew;
+							updateTimeRange();
 						}
 					}
 				}
@@ -269,56 +224,28 @@ public class TimeRangesUI extends Composite {
 			comboViewer.setInput(null);
 		}
 		//
-		updateTimeRange(timeRange);
+		updateTimeRange();
 	}
 
-	private void updateTimeRange(TimeRange timeRange) {
+	private void updateTimeRange() {
 
-		boolean enabled = false;
-		String start = "";
-		String center = "";
-		String stop = "";
-		//
-		if(timeRange != null) {
-			start = getRetentionTimeMinutes(timeRange.getStart());
-			center = getRetentionTimeMinutes(timeRange.getCenter());
-			stop = getRetentionTimeMinutes(timeRange.getStop());
-			enabled = true;
-		}
-		//
-		textStart.setText(start);
-		textCenter.setText(center);
-		textStop.setText(stop);
-		//
-		textStart.setEnabled(enabled);
-		textCenter.setEnabled(enabled);
-		textStop.setEnabled(enabled);
-		buttonDelete.setEnabled(enabled);
-	}
-
-	private String getRetentionTimeMinutes(int milliseconds) {
-
-		return decimalFormat.format(milliseconds / TimeRange.MINUTE_FACTOR);
-	}
-
-	private boolean validate(IValidator validator, ControlDecoration controlDecoration, Text text) {
-
-		IStatus status = validator.validate(text.getText());
-		if(status.isOK()) {
-			controlDecoration.hide();
-			return true;
-		} else {
-			controlDecoration.setImage(FieldDecorationRegistry.getDefault().getFieldDecoration(FieldDecorationRegistry.DEC_CONTENT_PROPOSAL).getImage());
-			controlDecoration.showHoverText(status.getMessage());
-			controlDecoration.show();
-			return false;
-		}
+		timeSpinnerStart.update(timeRange);
+		timeSpinnerCenter.update(timeRange);
+		timeSpinnerStop.update(timeRange);
+		buttonDelete.setEnabled(timeRange != null);
 	}
 
 	private void fireUpdate() {
 
 		if(updateListener != null) {
-			updateListener.update();
+			getDisplay().asyncExec(new Runnable() {
+
+				@Override
+				public void run() {
+
+					updateListener.update();
+				}
+			});
 		}
 	}
 }
