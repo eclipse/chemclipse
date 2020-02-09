83e95efe2fd624861ce0a1d8ce3f5e6bd316bb14
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2020-02-09 08:30:58 +0100

    #256 - migrate from InputPart to Part

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/provider/ISupplierFileEditorSupport.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/provider/ISupplierFileEditorSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Lablicate GmbH.
+ * Copyright (c) 2017, 2020 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Christoph Läubrich - migrate from InputPart to Part
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.ui.provider;
 
@@ -80,7 +81,7 @@ public interface ISupplierFileEditorSupport extends ISupplierFileIdentifier {
 		/*
 		 * Create the input part and prepare it.
 		 */
-		MPart part = MBasicFactory.INSTANCE.createInputPart();
+		MPart part = MBasicFactory.INSTANCE.createPart();
 		part.getTags().add(EPartService.REMOVE_ON_HIDE_TAG);
 		part.setElementId(elementId);
 		part.setContributionURI(contributionURI);
--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/views/DemoWelcomeTile.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/views/DemoWelcomeTile.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2019 Lablicate GmbH.
+ * Copyright (c) 2013, 2020 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -110,7 +110,7 @@ public class DemoWelcomeTile implements TileDefinition {
 		/*
 		 * Create the input part and prepare it.
 		 */
-		MPart part = MBasicFactory.INSTANCE.createInputPart();
+		MPart part = MBasicFactory.INSTANCE.createPart();
 		part.setElementId("org.eclipse.chemclipse.ux.extension.xxd.ui.part.chromatogramEditorMSD");
 		part.setContributionURI("bundleclass://org.eclipse.chemclipse.ux.extension.xxd.ui/org.eclipse.chemclipse.ux.extension.xxd.ui.editors.ChromatogramEditorMSD");
 		part.setIconURI("platform:/plugin/org.eclipse.chemclipse.rcp.ui.icons/icons/16x16/chromatogram.gif");
