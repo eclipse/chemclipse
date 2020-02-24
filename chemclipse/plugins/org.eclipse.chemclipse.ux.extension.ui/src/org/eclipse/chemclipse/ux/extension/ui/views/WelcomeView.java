ff3ad82a9c2d0a6d089a0112770d7d8d6bd612ae
Dr. Philip Wenig <philip.wenig@openchrom.net>
2020-02-24 14:19:46 +0100

    A comment has been removed.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/views/WelcomeView.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/views/WelcomeView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2019 Lablicate GmbH.
+ * Copyright (c) 2013, 2020 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -40,9 +40,6 @@ public class WelcomeView {
 	private static final int DEFAULT_NUMBER_OF_COLUMNS = Integer.getInteger("chemclipse.welcome.columns", 4);
 	@Inject
 	private IEclipseContext eclipseContext;
-	/*
-	 * Main
-	 */
 
 	private class Component implements TileDefinition {
 
