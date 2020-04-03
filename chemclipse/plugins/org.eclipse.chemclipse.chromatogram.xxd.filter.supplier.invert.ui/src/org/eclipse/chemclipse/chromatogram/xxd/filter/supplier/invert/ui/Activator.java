da5a40068c1a26d9d49f282e871fdbaa467e662f
Dr. Philip Wenig <philip.wenig@openchrom.net>
2020-04-03 11:52:42 +0200

    Fixed #301 - Add Invert Filter

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.invert.ui/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/invert/ui/Activator.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.invert.ui;
+
+import org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.invert.preferences.PreferenceSupplier;
+import org.eclipse.chemclipse.support.ui.activator.AbstractActivatorUI;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractActivatorUI {
+
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+
+		super.start(context);
+		initializePreferenceStore(PreferenceSupplier.INSTANCE());
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+
+		return plugin;
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.invert/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/invert/preferences/PreferenceInitializer.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.invert.preferences;
+
+import org.eclipse.chemclipse.support.preferences.AbstractExtendedPreferenceInitializer;
+
+public class PreferenceInitializer extends AbstractExtendedPreferenceInitializer {
+
+	public PreferenceInitializer() {
+		super(PreferenceSupplier.INSTANCE());
+	}
+}
