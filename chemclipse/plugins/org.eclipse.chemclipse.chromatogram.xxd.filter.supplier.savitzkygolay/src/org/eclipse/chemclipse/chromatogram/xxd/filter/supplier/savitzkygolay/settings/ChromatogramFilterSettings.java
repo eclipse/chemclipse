c976afd63089d9f2c8b8d73040a5cfca88d09c7f
Lorenz Gerber <lorenz.gerber@lablicate.com>
2020-04-16 20:59:20 +0200

    add description
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.savitzkygolay/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/savitzkygolay/settings/ChromatogramFilterSettings.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.savitzkygolay/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/savitzkygolay/settings/ChromatogramFilterSettings.java
@@ -30,7 +30,7 @@ public class ChromatogramFilterSettings extends AbstractChromatogramFilterSettin
 	@JsonProperty(value = "Width", defaultValue = "5")
 	@IntSettingsProperty(minValue = PreferenceSupplier.MIN_WIDTH, maxValue = PreferenceSupplier.MAX_WIDTH)
 	private int width = 5;
-	@JsonProperty(value = "Filter individual ion channels", defaultValue = "true")
+	@JsonProperty(value = "Filter individual ion channels (only applicable to MSD data).", defaultValue = "true")
 	@JsonPropertyDescription(value = "Per Ion Filter Calculation.")
 	private boolean perIonCalculation = true;
 
