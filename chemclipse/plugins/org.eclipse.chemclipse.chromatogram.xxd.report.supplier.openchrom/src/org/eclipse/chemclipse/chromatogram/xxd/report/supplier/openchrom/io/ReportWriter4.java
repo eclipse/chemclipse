6456ae4b9d3c55270567e539ae7d56df40905aeb
Lorenz Gerber <lorenz.gerber@lablicate.com>
2020-06-29 16:48:31 +0200

    fix #387 implement a new report to batch export peak tables
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom/src/org/eclipse/chemclipse/chromatogram/xxd/report/supplier/openchrom/io/ReportWriter4.java
@@ -0,0 +1,225 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Lorenz Gerber - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom.io;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.chemclipse.chromatogram.xxd.report.supplier.openchrom.settings.ReportSettings4;
+import org.eclipse.chemclipse.model.comparator.PeakRetentionTimeComparator;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IChromatogramOverview;
+import org.eclipse.chemclipse.model.core.IChromatogramPeak;
+import org.eclipse.chemclipse.model.core.IPeak;
+import org.eclipse.chemclipse.model.core.IPeakModel;
+import org.eclipse.chemclipse.msd.model.core.AbstractIon;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.msd.model.core.IIon;
+import org.eclipse.chemclipse.msd.model.core.IPeakMassSpectrum;
+import org.eclipse.chemclipse.msd.model.core.IPeakModelMSD;
+import org.eclipse.chemclipse.msd.model.core.comparator.IonAbundanceComparator;
+import org.eclipse.chemclipse.support.comparator.SortOrder;
+import org.eclipse.chemclipse.support.text.ValueFormat;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ReportWriter4 {
+
+	private static final int NUMBER_OF_IONS_TO_PRINT = 5;
+	private static final String DELIMITER = "\t";
+	private static final String ION_DELIMITER = " ";
+	private static final String NO_VALUE = "--";
+	//
+	private DecimalFormat decimalFormat = ValueFormat.getDecimalFormatEnglish("0.0####");
+	//
+	private IonAbundanceComparator ionComparator = new IonAbundanceComparator(SortOrder.DESC);
+	private PeakRetentionTimeComparator peakComparator = new PeakRetentionTimeComparator(SortOrder.ASC);
+
+	public void generate(File file, boolean append, List<IChromatogram<? extends IPeak>> chromatograms, ReportSettings4 reportSettings, IProgressMonitor monitor) throws IOException {
+
+		try (PrintWriter printWriter = new PrintWriter(new FileWriter(file, append))) {
+			for(IChromatogram<? extends IPeak> chromatogram : chromatograms) {
+				printHeader(printWriter, chromatogram, monitor);
+				reportChromatogram(printWriter, chromatogram, monitor);
+			}
+		}
+	}
+
+	private void printHeader(PrintWriter printWriter, IChromatogramOverview chromatogramOverview, IProgressMonitor monitor) {
+
+		printHeaderLine(printWriter, "Name", chromatogramOverview.getName());
+	}
+
+	private void printHeaderLine(PrintWriter printWriter, String key, String value) {
+
+		printWriter.print(key);
+		printWriter.print(": ");
+		printWriter.println(value);
+	}
+
+	/**
+	 * The report chromatogram is responsible how to print each section.
+	 * 
+	 * @param printWriter
+	 * @param chromatogram
+	 * @param monitor
+	 */
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	private void reportChromatogram(PrintWriter printWriter, IChromatogram chromatogram, IProgressMonitor monitor) {
+
+		List<IPeak> peaks = new ArrayList<IPeak>(chromatogram.getPeaks());
+		Collections.sort(peaks, peakComparator);
+		reportPeaksOverview(printWriter, peaks, "Peaks are not available.", monitor);
+	}
+
+	private void reportPeaksOverview(PrintWriter printWriter, List<IPeak> peaks, String messageNoResults, IProgressMonitor monitor) {
+
+		if(peaks.size() > 0) {
+			printPeaksOverview(printWriter, peaks, monitor);
+		} else {
+			printWriter.println(messageNoResults);
+		}
+	}
+
+	private void printPeaksOverview(PrintWriter printWriter, List<IPeak> peaks, IProgressMonitor monitor) {
+
+		/*
+		 * Headline
+		 */
+		printWriter.print("[#]");
+		printWriter.print(DELIMITER);
+		printWriter.print("RT (Min)");
+		printWriter.print(DELIMITER);
+		printWriter.print("RI");
+		printWriter.print(DELIMITER);
+		printWriter.print("Start RT (Min)");
+		printWriter.print(DELIMITER);
+		printWriter.print("Stop RT (Min)");
+		printWriter.print(DELIMITER);
+		printWriter.print("Tailing");
+		printWriter.print(DELIMITER);
+		printWriter.print("Width (at 50% peak height by inflection points)");
+		printWriter.print(DELIMITER);
+		printWriter.print("Integrated Area");
+		printWriter.print(DELIMITER);
+		printWriter.print("S/N");
+		printWriter.print(DELIMITER);
+		printWriter.print("Leading");
+		printWriter.print(DELIMITER);
+		printWriter.print("Tailing");
+		printWriter.print(DELIMITER);
+		printWriter.print("Scan #");
+		printWriter.print(DELIMITER);
+		printWriter.print("Purity");
+		printWriter.print(DELIMITER);
+		printWriter.print("Integrator");
+		printWriter.print(DELIMITER);
+		printWriter.print("Detector");
+		printWriter.print(DELIMITER);
+		printWriter.print("Model");
+		printWriter.print(DELIMITER);
+		printWriter.print("Trace ...");
+		printWriter.println("");
+		/*
+		 * Print each peak.
+		 */
+		int counter = 1;
+		for(IPeak peak : peaks) {
+			reportPeak(printWriter, peak, counter++, monitor);
+		}
+	}
+
+	private void reportPeak(PrintWriter printWriter, IPeak peak, int number, IProgressMonitor monitor) {
+
+		IPeakModel peakModel = peak.getPeakModel();
+		IChromatogramPeak chromatogramPeak = null;
+		if(peak instanceof IChromatogramPeak) {
+			chromatogramPeak = (IChromatogramPeak)peak;
+		}
+		//
+		printWriter.print("[" + number + "]");
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getRetentionTimeAtPeakMaximum() / IChromatogramMSD.MINUTE_CORRELATION_FACTOR));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getPeakMaximum().getRetentionIndex()));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getStartRetentionTime() / IChromatogramMSD.MINUTE_CORRELATION_FACTOR));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getStopRetentionTime() / IChromatogramMSD.MINUTE_CORRELATION_FACTOR));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getTailing()));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getWidthByInflectionPoints(0.5f) / IChromatogramMSD.MINUTE_CORRELATION_FACTOR));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peak.getIntegratedArea()));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format((chromatogramPeak != null) ? chromatogramPeak.getSignalToNoiseRatio() : NO_VALUE));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getLeading()));
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format(peakModel.getTailing()));
+		printWriter.print(DELIMITER);
+		printWriter.print((chromatogramPeak != null) ? Integer.toString(chromatogramPeak.getScanMax()) : NO_VALUE);
+		printWriter.print(DELIMITER);
+		printWriter.print(decimalFormat.format((chromatogramPeak != null) ? chromatogramPeak.getPurity() : NO_VALUE));
+		printWriter.print(DELIMITER);
+		printWriter.print(peak.getIntegratorDescription());
+		printWriter.print(DELIMITER);
+		printWriter.print(peak.getDetectorDescription());
+		printWriter.print(DELIMITER);
+		printWriter.print(peak.getModelDescription());
+		/*
+		 * Print the highest traces values (m/z abundance or wavelength intensity)
+		 */
+		String highestTraces;
+		if(peakModel instanceof IPeakModelMSD) {
+			IPeakModelMSD peakModelMSD = (IPeakModelMSD)peakModel;
+			highestTraces = extractHighestIons(printWriter, peakModelMSD);
+		} else {
+			highestTraces = NO_VALUE;
+		}
+		printWriter.print(DELIMITER);
+		printWriter.print(highestTraces);
+		/*
+		 * Close the line.
+		 */
+		printWriter.println("");
+	}
+
+	private String extractHighestIons(PrintWriter printWriter, IPeakModelMSD peakModelMSD) {
+
+		IPeakMassSpectrum peakMassSpectrum = peakModelMSD.getPeakMassSpectrum();
+		List<IIon> ions = new ArrayList<IIon>(peakMassSpectrum.getIons());
+		/*
+		 * Check how many ions shall be printed.
+		 */
+		int numberOfIonsToPrint = (ions.size() < NUMBER_OF_IONS_TO_PRINT) ? ions.size() : NUMBER_OF_IONS_TO_PRINT;
+		Collections.sort(ions, ionComparator);
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < numberOfIonsToPrint; i++) {
+			IIon ion = ions.get(i);
+			if(ion.getIon() == AbstractIon.TIC_ION) {
+				builder.append(AbstractIon.TIC_DESCRIPTION);
+			} else {
+				builder.append(decimalFormat.format(ion.getIon()));
+			}
+			builder.append(ION_DELIMITER);
+		}
+		builder.append("...");
+		return builder.toString();
+	}
+}
