1d6bcbdea167de4940be69fa1abf9c0a1d60d9de
Philip Wenig <philip.wenig@openchrom.net>
2020-10-01 15:46:42 +0200

    The extension check has been updated.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.zip/src/org/eclipse/chemclipse/xxd/converter/supplier/zip/converter/MagicNumberMatcher.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.zip/src/org/eclipse/chemclipse/xxd/converter/supplier/zip/converter/MagicNumberMatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2018 Lablicate GmbH.
+ * Copyright (c) 2016, 2020 Lablicate GmbH.
  *
  * All rights reserved.
  * 
@@ -12,22 +12,12 @@ import java.io.File;
 
 import org.eclipse.chemclipse.converter.core.AbstractMagicNumberMatcher;
 import org.eclipse.chemclipse.converter.core.IMagicNumberMatcher;
-import org.eclipse.chemclipse.xxd.converter.supplier.zip.internal.converter.SpecificationValidator;
 
 public class MagicNumberMatcher extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
 
 	@Override
 	public boolean checkFileFormat(File file) {
 
-		boolean isValidFormat = false;
-		try {
-			file = SpecificationValidator.validateSpecification(file);
-			if(file.exists()) {
-				isValidFormat = true;
-			}
-		} catch(Exception e) {
-			// Print no exception.
-		}
-		return isValidFormat;
+		return checkFileExtension(file, ".zip");
 	}
 }
