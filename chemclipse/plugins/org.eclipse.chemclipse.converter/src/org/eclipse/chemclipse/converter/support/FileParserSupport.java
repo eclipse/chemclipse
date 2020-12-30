408b1e782fc25be36c477809faaa51d94ef4d05c
Philip Wenig <philip.wenig@openchrom.net>
2020-12-30 08:39:17 +0100

    Minor improvements added to handle the directory file selection.

--- chemclipse/plugins/org.eclipse.chemclipse.converter/src/org/eclipse/chemclipse/converter/support/FileParserSupport.java
+++ chemclipse/plugins/org.eclipse.chemclipse.converter/src/org/eclipse/chemclipse/converter/support/FileParserSupport.java
@@ -14,24 +14,96 @@ package org.eclipse.chemclipse.converter.support;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FileParserSupport {
 
-	public static List<File> match(File directory, String filePrefix, String fileExtension) {
+	/**
+	 * The first matched/sorted file is returned that matches the prefix and extension pattern.
+	 * Null if none could be matched.
+	 * 
+	 * @param directory
+	 * @param filePrefix
+	 * @param fileExtension
+	 * @return {@link File}
+	 */
+	public static File matchFile(File directory, String filePrefix, String fileExtension) {
 
-		String prefix = filePrefix.toLowerCase();
+		Set<String> filePrefixes = new HashSet<>();
+		filePrefixes.add(filePrefix);
+		List<File> files = match(directory, filePrefixes, fileExtension);
+		return files.size() > 0 ? files.get(0) : null;
+	}
+
+	/**
+	 * The first matched/sorted file is returned that matches the prefix and extension pattern.
+	 * Null if none could be matched.
+	 * 
+	 * @param directory
+	 * @param filePrefix
+	 * @param fileExtension
+	 * @return {@link File}
+	 */
+	public static File matchFile(File directory, Set<String> filePrefixes, String fileExtension) {
+
+		List<File> files = match(directory, filePrefixes, fileExtension);
+		return files.size() > 0 ? files.get(0) : null;
+	}
+
+	/**
+	 * A list of file(s) is returned that matches the prefix and extension pattern.
+	 * 
+	 * @param directory
+	 * @param filePrefix
+	 * @param fileExtension
+	 * @return {@link List}
+	 */
+	public static List<File> match(File directory, Set<String> filePrefixes, String fileExtension) {
+
 		String extension = fileExtension.toLowerCase();
+		Set<String> prefixes = new HashSet<>();
+		for(String prefix : filePrefixes) {
+			prefixes.add(prefix.toLowerCase());
+		}
 		//
 		List<File> files = new ArrayList<>();
 		for(File file : directory.listFiles()) {
 			String name = file.getName().toLowerCase();
-			if(name.startsWith(prefix) && name.endsWith(extension)) {
-				files.add(file);
+			if(name.endsWith(extension)) {
+				exitloop:
+				for(String prefix : prefixes) {
+					if(name.startsWith(prefix)) {
+						files.add(file);
+						break exitloop;
+					}
+				}
 			}
 		}
 		//
 		Collections.sort(files);
 		return files;
+	}
+
+	/**
+	 * Try to get the file given by the directory and name.
+	 * Additional tries to lower and upper case of the name are performed to get the file.
+	 * 
+	 * @param baseFileDirectory
+	 * @param name
+	 * @return {@link File}
+	 */
+	public static File find(String directory, String name) {
+
+		File file = new File(directory + File.separator + name);
+		if(!file.exists()) {
+			file = new File(directory + File.separator + name.toLowerCase());
+			if(!file.exists()) {
+				file = new File(directory + File.separator + name.toUpperCase());
+			}
+		}
+		//
+		return file;
 	}
 }
