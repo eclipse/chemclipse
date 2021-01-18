6bd374b1df1c7fd8e30e45facb93b80c7a5edf18
Philip Wenig <philip.wenig@openchrom.net>
2021-01-18 20:44:27 +0100

    Fixed #474 - Manual "Peak Detector" part is empty

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/ExtendedPeakDetectorUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/ExtendedPeakDetectorUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2020 Lablicate GmbH.
+ * Copyright (c) 2018, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -537,7 +537,7 @@ public class ExtendedPeakDetectorUI extends Composite implements IExtendedPartUI
 		/*
 		 * Add the paint listeners to draw the selected peak range.
 		 */
-		IPlotArea plotArea = getPlotArea();
+		IPlotArea plotArea = chromatogramChart.getBaseChart().getPlotArea();
 		baselineSelectionPaintListener = new BaselineSelectionPaintListener();
 		boxSelectionPaintListener = new BoxSelectionPaintListener();
 		plotArea.addCustomPaintListener(baselineSelectionPaintListener);
