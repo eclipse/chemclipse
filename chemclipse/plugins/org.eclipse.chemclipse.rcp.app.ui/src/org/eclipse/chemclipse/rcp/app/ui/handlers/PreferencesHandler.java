d0e145e412b0384e4c0a528e1256c94b1fd04d52
Philip Wenig <philip.wenig@openchrom.net>
2021-03-25 09:00:14 +0100

    Fixed #546 - Preference Page - get rid of Ant, Java, ... pages

--- chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/handlers/PreferencesHandler.java
+++ chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/handlers/PreferencesHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2018 Lablicate GmbH.
+ * Copyright (c) 2012, 2021 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,6 +11,10 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.rcp.app.ui.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.chemclipse.support.ui.workbench.PreferencesSupport;
 import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.e4.core.commands.ECommandService;
 import org.eclipse.e4.core.commands.EHandlerService;
@@ -25,6 +29,21 @@ public class PreferencesHandler {
 
 		ParameterizedCommand command = commandService.createCommand("org.eclipse.ui.window.preferences", null);
 		if(handlerService.canExecute(command)) {
+			//
+			List<String> preservePreferencePrefixes = new ArrayList<String>();
+			preservePreferencePrefixes.add("org.eclipse.chemclipse");
+			preservePreferencePrefixes.add("org.eclipse.swtchart");
+			//
+			List<String> preservePreferenceNodes = new ArrayList<String>();
+			preservePreferenceNodes.add("org.eclipse.equinox.internal.p2.ui.sdk.ProvisioningPreferencePage"); // Install/Update
+			preservePreferenceNodes.add("org.eclipse.equinox.internal.p2.ui.sdk.SitesPreferencePage"); // Available Software Sites
+			preservePreferenceNodes.add("org.eclipse.equinox.internal.p2.ui.sdk.scheduler.AutomaticUpdatesPreferencePage"); // Automatic Updates
+			preservePreferenceNodes.add("org.eclipse.ui.preferencePages.Workbench"); // General
+			preservePreferenceNodes.add("org.eclipse.ui.net.NetPreferences"); // Network Connections
+			preservePreferenceNodes.add("org.eclipse.jsch.ui.SSHPreferences"); // SSH2
+			//
+			PreferencesSupport.cleanPreferences(preservePreferencePrefixes, preservePreferenceNodes);
+			//
 			handlerService.executeHandler(command);
 		} else {
 			logger.warn("Can't handle to open the preference dialog.");
