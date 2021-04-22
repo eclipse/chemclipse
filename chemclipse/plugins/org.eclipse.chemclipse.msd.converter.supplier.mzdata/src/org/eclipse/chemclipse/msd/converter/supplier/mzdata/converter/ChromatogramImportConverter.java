2b09bfdb2719208d7e9b5e4f0d3f4338ebb1e632
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-04-22 11:10:19 +0200

    Add read support for .mzdata chromatograms.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzdata/src/org/eclipse/chemclipse/msd/converter/supplier/mzdata/converter/ChromatogramImportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzdata/src/org/eclipse/chemclipse/msd/converter/supplier/mzdata/converter/ChromatogramImportConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2020 Lablicate GmbH.
+ * Copyright (c) 2014, 2021 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,35 +8,66 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Christoph Läubrich - add generics
+ * Matthias Mailänder - fix description
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.converter.supplier.mzdata.converter;
 
 import java.io.File;
 
 import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramImportConverter;
+import org.eclipse.chemclipse.converter.support.IConstants;
+import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.model.core.IChromatogramOverview;
+import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDReader;
+import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.support.SpecificationValidator;
+import org.eclipse.chemclipse.msd.converter.supplier.mzdata.io.ChromatogramReader;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
-import org.eclipse.chemclipse.processing.core.ProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public class ChromatogramImportConverter extends AbstractChromatogramImportConverter<IChromatogramMSD> {
 
-	private static final String DESCRIPTION = "mz5 Import Converter";
+	private static final Logger logger = Logger.getLogger(ChromatogramImportConverter.class);
+	private static final String DESCRIPTION = "mzData Import Converter";
 
 	@Override
 	public IProcessingInfo<IChromatogramMSD> convert(File file, IProgressMonitor monitor) {
 
-		IProcessingInfo<IChromatogramMSD> processingInfo = new ProcessingInfo<>();
-		processingInfo.addErrorMessage(DESCRIPTION, "The converter needs to be implemented.");
+		IProcessingInfo<IChromatogramMSD> processingInfo = super.validate(file);
+		if(!processingInfo.hasErrorMessages()) {
+			/*
+			 * Read the chromatogram.
+			 */
+			file = SpecificationValidator.validateSpecification(file);
+			IChromatogramMSDReader reader = new ChromatogramReader();
+			monitor.subTask(IConstants.IMPORT_CHROMATOGRAM);
+			try {
+				IChromatogramMSD chromatogram = reader.read(file, monitor);
+				processingInfo.setProcessingResult(chromatogram);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
 		return processingInfo;
 	}
 
 	@Override
 	public IProcessingInfo<IChromatogramOverview> convertOverview(File file, IProgressMonitor monitor) {
 
-		IProcessingInfo<IChromatogramOverview> processingInfo = new ProcessingInfo<>();
-		processingInfo.addErrorMessage(DESCRIPTION, "The converter needs to be implemented.");
+		IProcessingInfo<IChromatogramOverview> processingInfo = super.validate(file);
+		if(!processingInfo.hasErrorMessages()) {
+			file = SpecificationValidator.validateSpecification(file);
+			IChromatogramMSDReader reader = new ChromatogramReader();
+			try {
+				IChromatogramOverview chromatogramOverview = reader.readOverview(file, monitor);
+				processingInfo.setProcessingResult(chromatogramOverview);
+			} catch(Exception e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, "Something has definitely gone wrong with the file: " + file.getAbsolutePath());
+			}
+		}
 		return processingInfo;
 	}
 }
--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzdata/src/org/eclipse/chemclipse/msd/converter/supplier/mzdata/io/ChromatogramReader.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzdata/src/org/eclipse/chemclipse/msd/converter/supplier/mzdata/io/ChromatogramReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Lablicate GmbH.
+ * Copyright (c) 2014, 2021 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,14 +8,14 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - functional reader for specification version 1.05
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.converter.supplier.mzdata.io;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-
-import org.eclipse.core.runtime.IProgressMonitor;
 
 import org.eclipse.chemclipse.converter.exceptions.FileIsEmptyException;
 import org.eclipse.chemclipse.converter.exceptions.FileIsNotReadableException;
@@ -23,18 +23,52 @@ import org.eclipse.chemclipse.model.core.IChromatogramOverview;
 import org.eclipse.chemclipse.msd.converter.io.AbstractChromatogramMSDReader;
 import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDReader;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.xxd.converter.supplier.io.exception.UnknownVersionException;
+import org.eclipse.core.runtime.IProgressMonitor;
 
 public class ChromatogramReader extends AbstractChromatogramMSDReader implements IChromatogramMSDReader {
 
-	@Override
-	public IChromatogramOverview readOverview(File file, IProgressMonitor monitor) throws FileNotFoundException, FileIsNotReadableException, FileIsEmptyException, IOException {
+	private static final String MZDATA_V_105 = "1.05";
+	private static final String CONTEXT_PATH_V_105 = "org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model";
 
-		return null;
+	public static IChromatogramMSDReader getReader(final File file) throws IOException {
+
+		IChromatogramMSDReader chromatogramReader = null;
+		//
+		final FileReader fileReader = new FileReader(file);
+		final char[] charBuffer = new char[350];
+		fileReader.read(charBuffer);
+		fileReader.close();
+		//
+		final String header = new String(charBuffer);
+		if(header.contains(MZDATA_V_105)) {
+			chromatogramReader = new ChromatogramReaderVersion105(CONTEXT_PATH_V_105);
+		} else {
+			throw new UnknownVersionException();
+		}
+		//
+		return chromatogramReader;
 	}
 
 	@Override
-	public IChromatogramMSD read(File file, IProgressMonitor monitor) throws FileNotFoundException, FileIsNotReadableException, FileIsEmptyException, IOException {
+	public IChromatogramMSD read(final File file, final IProgressMonitor monitor) throws FileNotFoundException, FileIsNotReadableException, FileIsEmptyException, IOException, InterruptedException {
 
-		return null;
+		final IChromatogramMSDReader chromatogramReader = getReader(file);
+		if(chromatogramReader != null) {
+			return chromatogramReader.read(file, monitor);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public IChromatogramOverview readOverview(final File file, final IProgressMonitor monitor) throws FileNotFoundException, FileIsNotReadableException, FileIsEmptyException, IOException {
+
+		final IChromatogramMSDReader chromatogramReader = getReader(file);
+		if(chromatogramReader != null) {
+			return chromatogramReader.readOverview(file, monitor);
+		} else {
+			return null;
+		}
 	}
 }
