2ab623805582ca9a92223817b46acd80f27a4eee
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-04-28 14:48:40 +0200

    Bail out early if the file extension does not even match.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse/src/org/eclipse/chemclipse/msd/converter/supplier/chemclipse/converter/MagicNumberMatcherMSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse/src/org/eclipse/chemclipse/msd/converter/supplier/chemclipse/converter/MagicNumberMatcherMSD.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2018 Lablicate GmbH.
+ * Copyright (c) 2016, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -29,6 +29,9 @@ public class MagicNumberMatcherMSD extends AbstractMagicNumberMatcher implements
 
 		boolean isForceLoadAlternateDetector = PreferenceSupplier.isForceLoadAlternateDetector();
 		boolean isValidFormat = false;
+		if(!checkFileExtension(file, ".ocb")) {
+			return isValidFormat;
+		}
 		try {
 			file = SpecificationValidator.validateSpecification(file);
 			ZipFile zipFile = new ZipFile(file);
