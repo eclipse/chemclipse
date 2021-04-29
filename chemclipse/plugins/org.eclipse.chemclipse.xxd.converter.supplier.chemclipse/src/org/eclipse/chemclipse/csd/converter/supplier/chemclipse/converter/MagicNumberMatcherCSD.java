1c3a184d899987f04e5af35f32d1d9e6d602448a
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-04-29 22:11:43 +0200

    GC-FID is fully supported.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse/src/org/eclipse/chemclipse/csd/converter/supplier/chemclipse/converter/MagicNumberMatcherCSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse/src/org/eclipse/chemclipse/csd/converter/supplier/chemclipse/converter/MagicNumberMatcherCSD.java
@@ -20,14 +20,12 @@ import org.eclipse.chemclipse.converter.core.AbstractMagicNumberMatcher;
 import org.eclipse.chemclipse.converter.core.IMagicNumberMatcher;
 import org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.internal.support.IFormat;
 import org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.internal.support.SpecificationValidator;
-import org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.preferences.PreferenceSupplier;
 
 public class MagicNumberMatcherCSD extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
 
 	@Override
 	public boolean checkFileFormat(File file) {
 
-		boolean isForceLoadAlternateDetector = PreferenceSupplier.isForceLoadAlternateDetector();
 		boolean isValidFormat = false;
 		if(!checkFileExtension(file, ".ocb")) {
 			return isValidFormat;
@@ -44,21 +42,14 @@ public class MagicNumberMatcherCSD extends AbstractMagicNumberMatcher implements
 				ZipEntry zipEntry = zipEntries.nextElement();
 				if(zipEntry.isDirectory()) {
 					String name = zipEntry.getName();
-					if(isForceLoadAlternateDetector) {
-						/*
-						 * Legacy:
-						 * DIR_CHROMATOGRAM
-						 * Versions <= 0.9.0.3
-						 */
-						if(name.equals(IFormat.DIR_CHROMATOGRAM_FID) || name.equals(IFormat.DIR_CHROMATOGRAM_CSD) || name.equals(IFormat.DIR_CHROMATOGRAM_MSD) || name.equals(IFormat.DIR_CHROMATOGRAM_WSD) || name.equals(IFormat.DIR_CHROMATOGRAM)) {
-							isValidFormat = true;
-							break exitloop;
-						}
-					} else {
-						if(name.equals(IFormat.DIR_CHROMATOGRAM_FID) || name.equals(IFormat.DIR_CHROMATOGRAM_CSD)) {
-							isValidFormat = true;
-							break exitloop;
-						}
+					/*
+					 * Legacy:
+					 * DIR_CHROMATOGRAM
+					 * Versions <= 0.9.0.3
+					 */
+					if(name.equals(IFormat.DIR_CHROMATOGRAM_FID) || name.equals(IFormat.DIR_CHROMATOGRAM_CSD)) {
+						isValidFormat = true;
+						break exitloop;
 					}
 				}
 			}
