0a625e19c390d0f4755a00a1848546fe6039170d
eselmeister <philip.wenig@openchrom.net>
2021-06-08 17:05:46 +0200

    Revert "Added support for color compensated real-time PCR data"

--- chemclipse/plugins/org.eclipse.chemclipse.pcr.model/src/org/eclipse/chemclipse/pcr/model/core/IChannel.java
+++ chemclipse/plugins/org.eclipse.chemclipse.pcr.model/src/org/eclipse/chemclipse/pcr/model/core/IChannel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2021 Lablicate GmbH.
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,11 +8,12 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
- * Matthias Mailänder - add color compensation
  *******************************************************************************/
 package org.eclipse.chemclipse.pcr.model.core;
 
 import java.util.List;
+
+import org.eclipse.chemclipse.numeric.core.IPoint;
 
 public interface IChannel {
 
@@ -38,17 +39,15 @@ public interface IChannel {
 
 	void setValid(boolean valid);
 
-	List<Double> getFluorescence();
+	List<Double> getPoints();
 
-	void setFluorescence(List<Double> fluorescence);
+	void setPoints(List<Double> points);
 
-	List<Double> getColorCompensatedFluorescence();
+	IPoint getCrossingPoint();
 
-	void setColorCompensatedFluorescence(List<Double> colorCompensatedFluorescence);
+	void setCrossingPoint(IPoint crossingPoint);
 
-	double getCrossingPoint();
-
-	void setCrossingPoint(double crossingPoint);
+	void setCrossingPoint(double crossingPointX);
 
 	String getDetectionName();
 
--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/PCRPlate.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/PCRPlate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2021 Lablicate GmbH.
+ * Copyright (c) 2019, 2020 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,7 +8,6 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
- * Matthias Mailänder - add color compensation
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.xxd.ui.swt;
 
@@ -18,6 +17,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.chemclipse.numeric.core.IPoint;
 import org.eclipse.chemclipse.pcr.model.core.IChannel;
 import org.eclipse.chemclipse.pcr.model.core.IPlate;
 import org.eclipse.chemclipse.pcr.model.core.IWell;
@@ -242,10 +242,10 @@ public class PCRPlate extends Composite {
 	private void appendChannelCrossingPoint(IChannel channel, StringBuilder builder) {
 
 		if(channel != null) {
-			double crossingPoint = channel.getCrossingPoint();
-			if(crossingPoint > 0.0d) {
+			IPoint crossingPoint = channel.getCrossingPoint();
+			if(crossingPoint != null && crossingPoint.getX() > 0.0d) {
 				DecimalFormat decimalFormat = ValueFormat.getDecimalFormatEnglish();
-				builder.append(decimalFormat.format(crossingPoint));
+				builder.append(decimalFormat.format(crossingPoint.getX()));
 			} else {
 				builder.append("--");
 			}
