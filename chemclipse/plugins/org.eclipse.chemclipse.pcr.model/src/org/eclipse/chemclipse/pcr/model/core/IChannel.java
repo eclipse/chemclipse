c810e093955c3e143f6d86c4c680ff4438fbdb63
eselmeister <philip.wenig@openchrom.net>
2021-06-09 17:47:40 +0200

    Revert "Revert "Added support for color compensated real-time PCR data""

--- chemclipse/plugins/org.eclipse.chemclipse.pcr.model/src/org/eclipse/chemclipse/pcr/model/core/IChannel.java
+++ chemclipse/plugins/org.eclipse.chemclipse.pcr.model/src/org/eclipse/chemclipse/pcr/model/core/IChannel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 Lablicate GmbH.
+ * Copyright (c) 2018, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,12 +8,11 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - add color compensation
  *******************************************************************************/
 package org.eclipse.chemclipse.pcr.model.core;
 
 import java.util.List;
-
-import org.eclipse.chemclipse.numeric.core.IPoint;
 
 public interface IChannel {
 
@@ -39,15 +38,17 @@ public interface IChannel {
 
 	void setValid(boolean valid);
 
-	List<Double> getPoints();
+	List<Double> getFluorescence();
 
-	void setPoints(List<Double> points);
+	void setFluorescence(List<Double> fluorescence);
 
-	IPoint getCrossingPoint();
+	List<Double> getColorCompensatedFluorescence();
 
-	void setCrossingPoint(IPoint crossingPoint);
+	void setColorCompensatedFluorescence(List<Double> colorCompensatedFluorescence);
 
-	void setCrossingPoint(double crossingPointX);
+	double getCrossingPoint();
+
+	void setCrossingPoint(double crossingPoint);
 
 	String getDetectionName();
 
--- chemclipse/plugins/org.eclipse.chemclipse.pcr.model/src/org/eclipse/chemclipse/pcr/model/core/IPlate.java
+++ chemclipse/plugins/org.eclipse.chemclipse.pcr.model/src/org/eclipse/chemclipse/pcr/model/core/IPlate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 Lablicate GmbH.
+ * Copyright (c) 2018, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - add color compensation
  *******************************************************************************/
 package org.eclipse.chemclipse.pcr.model.core;
 
