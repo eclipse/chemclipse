cd60ec5c9dcc707c08a757744d752b8b52102e39
Lorenz Gerber <lorenz.gerber@lablicate.com>
2021-06-11 16:07:21 +0200

    fix #632 refactor for better performance
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.zeroset/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/zeroset/core/ChromatogramFilterWSD.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.zeroset/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/zeroset/core/ChromatogramFilterWSD.java
@@ -12,9 +12,6 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.zeroset.core;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.chemclipse.chromatogram.filter.result.ChromatogramFilterResult;
 import org.eclipse.chemclipse.chromatogram.filter.result.ResultStatus;
 import org.eclipse.chemclipse.chromatogram.filter.settings.IChromatogramFilterSettings;
@@ -25,10 +22,9 @@ import org.eclipse.chemclipse.model.core.IChromatogram;
 import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.model.core.IScan;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.chemclipse.wsd.model.core.IScanSignalWSD;
 import org.eclipse.chemclipse.wsd.model.core.IScanWSD;
-import org.eclipse.chemclipse.wsd.model.core.implementation.ScanSignalWSD;
 import org.eclipse.chemclipse.wsd.model.core.selection.IChromatogramSelectionWSD;
-import org.eclipse.chemclipse.wsd.model.xwc.IExtractedWavelengthSignal;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 @SuppressWarnings("rawtypes")
@@ -61,76 +57,44 @@ public class ChromatogramFilterWSD extends AbstractChromatogramFilterWSD {
 
 	private void adjustMinSignalByWavelength(IChromatogram<? extends IPeak> chromatogram) {
 
-		List<IExtractedWavelengthSignal> extractedSignals = new ArrayList<>();
-		int startWavelength = Integer.MAX_VALUE;
-		int stopWavelength = Integer.MIN_VALUE;
-		/*
-		 * Extract
-		 */
-		for(IScan scan : chromatogram.getScans()) {
-			if(scan instanceof IScanWSD) {
-				IScanWSD scanWSD = (IScanWSD)scan;
-				IExtractedWavelengthSignal extractedWavelengthSignal = scanWSD.getExtractedWavelengthSignal();
-				startWavelength = Math.min(extractedWavelengthSignal.getStartWavelength(), startWavelength);
-				stopWavelength = Math.max(extractedWavelengthSignal.getStopWavelength(), stopWavelength);
-				extractedSignals.add(extractedWavelengthSignal);
-			}
-		}
 		/*
 		 * Detect global min signal
 		 */
-		float minSignal = 0;
-		for(int wavelength = startWavelength; wavelength <= stopWavelength; wavelength++) {
-			minSignal = Math.min(getMinSignal(extractedSignals, wavelength), minSignal);
+		float minSignal = Float.MAX_VALUE;
+		for(IScan scan : chromatogram.getScans()) {
+			if(scan instanceof IScanWSD) {
+				IScanWSD scanWSD = (IScanWSD)scan;
+				minSignal = Math.min(minSignal, getMinSignal(scanWSD));
+			}
 		}
 		/*
-		 * Shift Signal
+		 * Adjust
 		 */
-		for(int wavelength = startWavelength; wavelength <= stopWavelength; wavelength++) {
-			adjustMinSignal(extractedSignals, wavelength, minSignal);
+		if(minSignal < 0) {
+			float offsetSignal = Math.abs(minSignal);
+			for(IScan scan : chromatogram.getScans()) {
+				if(scan instanceof IScanWSD) {
+					IScanWSD scanWSD = (IScanWSD)scan;
+					adjustSignal(scanWSD, offsetSignal);
+				}
+			}
 		}
-		/*
-		 * Transfer the data
-		 */
-		transferData(chromatogram, extractedSignals);
 	}
 
-	private float getMinSignal(List<IExtractedWavelengthSignal> extractedSignals, int wavelength) {
+	private float getMinSignal(IScanWSD scanWSD) {
 
 		float minSignal = Float.MAX_VALUE;
-		for(IExtractedWavelengthSignal extractedSignal : extractedSignals) {
-			minSignal = Math.min(extractedSignal.getAbundance(wavelength), minSignal);
+		for(IScanSignalWSD scanSignal : scanWSD.getScanSignals()) {
+			minSignal = Math.min(minSignal, scanSignal.getAbundance());
 		}
 		//
 		return minSignal;
 	}
 
-	private void adjustMinSignal(List<IExtractedWavelengthSignal> extractedSignals, int wavelength, float minSignal) {
+	private void adjustSignal(IScanWSD scanWSD, float offsetSignal) {
 
-		float offsetSignal = Math.abs(minSignal);
-		for(IExtractedWavelengthSignal extractedSignal : extractedSignals) {
-			extractedSignal.setAbundance(wavelength, extractedSignal.getAbundance(wavelength) + offsetSignal);
-		}
-	}
-
-	private void transferData(IChromatogram<? extends IPeak> chromatogram, List<IExtractedWavelengthSignal> extractedSignals) {
-
-		int index = 0;
-		for(IScan scan : chromatogram.getScans()) {
-			if(scan instanceof IScanWSD) {
-				IScanWSD scanWSD = (IScanWSD)scan;
-				scanWSD.deleteScanSignals();
-				IExtractedWavelengthSignal extractedWavelengthSignal = extractedSignals.get(index);
-				int startWavelength = extractedWavelengthSignal.getStartWavelength();
-				int stopWavelength = extractedWavelengthSignal.getStopWavelength();
-				for(int wavelength = startWavelength; wavelength <= stopWavelength; wavelength++) {
-					float intensity = extractedWavelengthSignal.getAbundance(wavelength);
-					if(intensity != 0.0f) {
-						scanWSD.addScanSignal(new ScanSignalWSD(wavelength, intensity));
-					}
-				}
-				index++;
-			}
+		for(IScanSignalWSD scanSignal : scanWSD.getScanSignals()) {
+			scanSignal.setAbundance(scanSignal.getAbundance() + offsetSignal);
 		}
 	}
 }
