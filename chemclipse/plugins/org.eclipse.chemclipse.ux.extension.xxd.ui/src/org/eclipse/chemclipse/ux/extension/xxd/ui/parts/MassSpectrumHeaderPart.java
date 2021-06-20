3ccfaa2522b8c6963c4e0f2791eecbd58abd4ed4
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-06-20 13:26:29 +0200

    Add the MALDI-TOF MS perspective.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/parts/MassSpectrumHeaderPart.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2021 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.ux.extension.xxd.ui.parts;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.chemclipse.msd.model.core.IVendorMassSpectrum;
+import org.eclipse.chemclipse.support.events.IChemClipseEvents;
+import org.eclipse.chemclipse.ux.extension.xxd.ui.ExtendedMassSpectrumHeaderUI;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class MassSpectrumHeaderPart extends AbstractPart<ExtendedMassSpectrumHeaderUI> {
+
+	private static final String TOPIC = IChemClipseEvents.TOPIC_SCAN_XXD_UPDATE_SELECTION;
+
+	@Inject
+	public MassSpectrumHeaderPart(Composite parent) {
+
+		super(parent, TOPIC);
+	}
+
+	@Override
+	protected ExtendedMassSpectrumHeaderUI createControl(Composite parent) {
+
+		return new ExtendedMassSpectrumHeaderUI(parent, SWT.NONE);
+	}
+
+	@Override
+	protected boolean updateData(List<Object> objects, String topic) {
+
+		if(objects.size() == 1) {
+			Object object = objects.get(0);
+			if(object instanceof IVendorMassSpectrum) {
+				getControl().updateMassSpectrum((IVendorMassSpectrum)object);
+				return true;
+			} else {
+				getControl().updateMassSpectrum(null);
+				return false;
+			}
+		}
+		//
+		return false;
+	}
+
+	@Override
+	protected boolean isUpdateTopic(String topic) {
+
+		return TOPIC.equals(topic);
+	}
+}
---
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/parts/MassSpectrumPeakListPart.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2021 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.ux.extension.xxd.ui.parts;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.msd.swt.ui.components.massspectrum.ExtendedMassSpectrumIonsListUI;
+import org.eclipse.chemclipse.support.events.IChemClipseEvents;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class MassSpectrumPeakListPart extends AbstractPart<ExtendedMassSpectrumIonsListUI> {
+
+	private static final String TOPIC = IChemClipseEvents.TOPIC_SCAN_XXD_UPDATE_SELECTION;
+
+	@Inject
+	public MassSpectrumPeakListPart(Composite parent) {
+
+		super(parent, TOPIC);
+	}
+
+	@Override
+	protected ExtendedMassSpectrumIonsListUI createControl(Composite parent) {
+
+		return new ExtendedMassSpectrumIonsListUI(parent, SWT.NONE);
+	}
+
+	@Override
+	protected boolean updateData(List<Object> objects, String topic) {
+
+		if(objects.size() == 1) {
+			Object object = objects.get(0);
+			if(object instanceof IScanMSD) {
+				getControl().update((IScanMSD)object);
+				return true;
+			}
+		}
+		//
+		return false;
+	}
+
+	@Override
+	protected boolean isUpdateTopic(String topic) {
+
+		return TOPIC.equals(topic);
+	}
+}
