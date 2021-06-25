6a4875fd6e6740fb0fed41d160204cd7d1572ed8
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-06-25 08:25:49 +0200

    Auto-detect chromatograms and mass spectra.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzml/src/org/eclipse/chemclipse/msd/converter/supplier/mzml/converter/MassSpectrumMagicNumberMatcher.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2021 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - auto detection for MALDI files
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.mzml.converter;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.eclipse.chemclipse.converter.core.AbstractMagicNumberMatcher;
+import org.eclipse.chemclipse.converter.core.IMagicNumberMatcher;
+import org.eclipse.chemclipse.msd.converter.supplier.mzml.converter.io.IFormat;
+import org.eclipse.chemclipse.msd.converter.supplier.mzml.internal.converter.IConstants;
+import org.eclipse.chemclipse.msd.converter.supplier.mzml.internal.converter.SpecificationValidator;
+import org.eclipse.chemclipse.msd.converter.supplier.mzml.internal.v110.model.MzML;
+import org.eclipse.chemclipse.msd.converter.supplier.mzml.internal.v110.model.SpectrumListType;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class MassSpectrumMagicNumberMatcher extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
+
+	@Override
+	public boolean checkFileFormat(File file) {
+
+		boolean isValidFormat = false;
+		try {
+			file = SpecificationValidator.validateSpecification(file);
+			if(!file.exists()) {
+				return isValidFormat;
+			}
+			if(!checkFileExtension(file, ".mzML")) {
+				return isValidFormat;
+			}
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document document = documentBuilder.parse(file);
+			NodeList nodeList = document.getElementsByTagName(IConstants.NODE_MZML);
+			//
+			JAXBContext jaxbContext = JAXBContext.newInstance(IFormat.CONTEXT_PATH_V_110);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			MzML mzML = (MzML)unmarshaller.unmarshal(nodeList.item(0));
+			SpectrumListType spectrumList = mzML.getRun().getSpectrumList();
+			if(spectrumList != null && spectrumList.getCount().intValue() == 1)
+				isValidFormat = true;
+		} catch(Exception e) {
+			// fail silently
+		}
+		return isValidFormat;
+	}
+}
--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzml/src/org/eclipse/chemclipse/msd/converter/supplier/mzml/converter/io/IFormat.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzml/src/org/eclipse/chemclipse/msd/converter/supplier/mzml/converter/io/IFormat.java
@@ -16,4 +16,5 @@ public interface IFormat {
 	String CONTEXT_PATH_V_110 = "org.eclipse.chemclipse.msd.converter.supplier.mzml.internal.v110.model";
 	String MZML_V_110 = "1.1.0";
 	String CHROMATOGRAM_VERSION_LATEST = MZML_V_110;
+	String MASS_SPECTRUM_VERSION_LATEST = MZML_V_110;
 }
