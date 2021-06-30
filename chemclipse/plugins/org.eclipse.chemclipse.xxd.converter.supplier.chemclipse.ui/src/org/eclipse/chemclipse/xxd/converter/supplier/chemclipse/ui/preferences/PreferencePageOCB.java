2ee68bd8614924d8f95b67c75c9587ba791c7fd1
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-06-30 14:19:06 +0200

    Use a scale to set the .ocb compression.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui/src/org/eclipse/chemclipse/xxd/converter/supplier/chemclipse/ui/preferences/PreferencePageOCB.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui/src/org/eclipse/chemclipse/xxd/converter/supplier/chemclipse/ui/preferences/PreferencePageOCB.java
@@ -12,11 +12,11 @@
 package org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui.preferences;
 
 import org.eclipse.chemclipse.support.ui.preferences.fieldeditors.SpacerFieldEditor;
-import org.eclipse.chemclipse.support.ui.preferences.fieldeditors.SpinnerFieldEditor;
 import org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.xxd.converter.supplier.chemclipse.ui.Activator;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.ScaleFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -35,11 +35,12 @@ public class PreferencePageOCB extends FieldEditorPreferencePage implements IWor
 	 * GUI blocks needed to manipulate various types of preferences. Each field
 	 * editor knows how to save and restore itself.
 	 */
+	@Override
 	public void createFieldEditors() {
 
 		addField(new SpacerFieldEditor(getFieldEditorParent()));
 		addField(new ComboFieldEditor(PreferenceSupplier.P_CHROMATOGRAM_VERSION_SAVE, "Save (*.ocb) as version:", PreferenceSupplier.getChromatogramVersions(), getFieldEditorParent()));
-		addField(new SpinnerFieldEditor(PreferenceSupplier.P_CHROMATOGRAM_COMPRESSION_LEVEL, "Compression 0 = off, 9 = best", PreferenceSupplier.MIN_COMPRESSION_LEVEL, PreferenceSupplier.MAX_COMPRESSION_LEVEL, getFieldEditorParent()));
+		addField(new ScaleFieldEditor(PreferenceSupplier.P_CHROMATOGRAM_COMPRESSION_LEVEL, "Compression: none to best", getFieldEditorParent(), PreferenceSupplier.MIN_COMPRESSION_LEVEL, PreferenceSupplier.MAX_COMPRESSION_LEVEL, 1, 1));
 	}
 
 	/*
@@ -47,6 +48,7 @@ public class PreferencePageOCB extends FieldEditorPreferencePage implements IWor
 	 * @see
 	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
 	 */
+	@Override
 	public void init(IWorkbench workbench) {
 
 	}
