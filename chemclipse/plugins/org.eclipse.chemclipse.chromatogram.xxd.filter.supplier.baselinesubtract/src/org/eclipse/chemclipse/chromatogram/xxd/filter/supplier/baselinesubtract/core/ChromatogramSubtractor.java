54251b6aecb7ccebe5360cfd75942dc46a015899
Lorenz Gerber <lorenz.gerber@lablicate.com>
2021-07-06 16:20:31 +0200

    fix #677 channel wise subtraction
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.baselinesubtract/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/baselinesubtract/core/ChromatogramSubtractor.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.baselinesubtract/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/baselinesubtract/core/ChromatogramSubtractor.java
@@ -8,14 +8,25 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Lorenz Gerber - channel wise subtraction for WSD / MSD
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.baselinesubtract.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.chemclipse.model.core.IChromatogram;
 import org.eclipse.chemclipse.model.core.IScan;
+import org.eclipse.chemclipse.model.exceptions.AbundanceLimitExceededException;
+import org.eclipse.chemclipse.msd.model.core.IIon;
 import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.msd.model.exceptions.IonLimitExceededException;
+import org.eclipse.chemclipse.wsd.model.core.IScanSignalWSD;
+import org.eclipse.chemclipse.wsd.model.core.IScanWSD;
 
 public class ChromatogramSubtractor {
+
+	private final static boolean CHANNEL_WISE_SUBTRACTION = true;
 
 	public void perform(IChromatogram<?> chromatogramMaster, IChromatogram<?> chromatogramSubtract) {
 
@@ -34,7 +45,13 @@ public class ChromatogramSubtractor {
 			//
 			for(int i = startScan; i <= stopScan; i++) {
 				IScan scanMaster = chromatogramMaster.getScan(i);
-				if(scanMaster != null) {
+				if(scanMaster instanceof IScanMSD && CHANNEL_WISE_SUBTRACTION) {
+					IScanMSD scanMasterMSD = (IScanMSD)scanMaster;
+					subtractMSD(scanMasterMSD, chromatogramSubtract);
+				} else if(scanMaster instanceof IScanWSD && CHANNEL_WISE_SUBTRACTION) {
+					IScanWSD scanMasterWSD = (IScanWSD)scanMaster;
+					subtractWSD(scanMasterWSD, chromatogramSubtract);
+				} else if(scanMaster != null) {
 					int scanNumberSubtract = chromatogramSubtract.getScanNumber(scanMaster.getRetentionTime());
 					IScan scanSubtract = chromatogramSubtract.getScan(scanNumberSubtract);
 					if(scanSubtract != null) {
@@ -57,6 +74,57 @@ public class ChromatogramSubtractor {
 						}
 					}
 				}
+			}
+		}
+	}
+
+	private void subtractWSD(IScanWSD scanMasterWSD, IChromatogram<?> chromatogramSubtract) {
+
+		if(scanMasterWSD != null) {
+			int scanNumberSubtract = chromatogramSubtract.getScanNumber(scanMasterWSD.getRetentionTime());
+			IScanWSD scanSubtract = (IScanWSD)chromatogramSubtract.getScan(scanNumberSubtract);
+			if(scanSubtract != null) {
+				List<IScanSignalWSD> signals = scanMasterWSD.getScanSignals();
+				for(IScanSignalWSD signal : signals) {
+					double wavelength = signal.getWavelength();
+					IScanSignalWSD subtract = scanSubtract.getScanSignal(wavelength).orElse(null);
+					if(subtract != null) {
+						float abundance = signal.getAbundance() - subtract.getAbundance();
+						signal.setAbundance(abundance);
+					}
+				}
+			}
+		}
+	}
+
+	private void subtractMSD(IScanMSD scanMasterMSD, IChromatogram<?> chromatogramSubtract) {
+
+		if(scanMasterMSD != null) {
+			int scanNumberSubtract = chromatogramSubtract.getScanNumber(scanMasterMSD.getRetentionTime());
+			IScanMSD scanSubtract = (IScanMSD)chromatogramSubtract.getScan(scanNumberSubtract);
+			if(scanSubtract != null) {
+				List<IIon> ions = scanMasterMSD.getIons();
+				List<IIon> zeroAbundanceIons = new ArrayList<>();
+				for(IIon ion : ions) {
+					double mass = ion.getIon();
+					IIon subtract;
+					try {
+						subtract = scanSubtract.getIon(mass);
+						if(subtract != null) {
+							float abundance = ion.getAbundance() - subtract.getAbundance();
+							if(abundance <= 0.0) {
+								zeroAbundanceIons.add(ion);
+							} else {
+								ion.setAbundance(abundance);
+							}
+						}
+					} catch(AbundanceLimitExceededException e1) {
+						System.out.println(e1);
+					} catch(IonLimitExceededException e1) {
+						System.out.println(e1);
+					}
+				}
+				zeroAbundanceIons.stream().forEach(x -> scanMasterMSD.removeIon(x));
 			}
 		}
 	}
