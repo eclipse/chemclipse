f1c3ebce12648af96f43f2e954375faf458fdf1e
Philip Wenig <philip.wenig@openchrom.net>
2021-07-14 09:28:28 +0200

    Addressed compiler warning.

--- chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/util/PreferencesUtils.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/util/PreferencesUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,14 +8,15 @@
  * 
  * Contributors:
  * Jan Holy - initial API and implementation
+ * Philip Wenig - generics
  *******************************************************************************/
 package org.eclipse.chemclipse.support.util;
 
 public class PreferencesUtils {
 
-	public static String[][] enumToArray(Class<? extends Enum> enums) {
+	public static String[][] enumToArray(Class<? extends Enum<?>> enums) {
 
-		Enum[] enumValues = enums.getEnumConstants();
+		Enum<?>[] enumValues = enums.getEnumConstants();
 		String[][] convertValues = new String[enumValues.length][2];
 		for(int i = 0; i < enumValues.length; i++) {
 			convertValues[i][0] = enumValues[i].toString();
