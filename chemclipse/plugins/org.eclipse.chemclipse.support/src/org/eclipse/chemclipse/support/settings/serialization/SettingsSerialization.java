af7c25fbfa6284fbccd60bb68fa1700a103a2324
Philip Wenig <philip.wenig@openchrom.net>
2021-09-26 13:01:23 +0200

    #736 - Window Size year in header has been adjusted

--- chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/settings/serialization/SettingsSerialization.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/settings/serialization/SettingsSerialization.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Lablicate GmbH.
+ * Copyright (c) 2019, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  * 
  * Contributors:
  * Christoph Läubrich - initial API and implementation
+ * Matthias Mailänder - add even/odd validation
  *******************************************************************************/
 package org.eclipse.chemclipse.support.settings.serialization;
 
