72fd33151dbed939ff4f5d69189380d31bc9b09f
Philip Wenig <philip.wenig@openchrom.net>
2021-10-18 09:41:24 +0200

    Fixed #786 - Peak Filter - Delete Peak Targets (Labels)

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.model/src/org/eclipse/chemclipse/xxd/model/support/TargetsDeleteOption.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.model/src/org/eclipse/chemclipse/xxd/model/support/TargetsDeleteOption.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Lablicate GmbH.
+ * Copyright (c) 2020, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,16 +11,22 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.xxd.model.support;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import org.eclipse.chemclipse.support.text.ILabel;
 
-public enum TargetsDeleteOption {
-	ALL_TARGETS, //
-	EMPTY_SMILES;
+public enum TargetsDeleteOption implements ILabel {
+	ALL_TARGETS("All Targets"), //
+	EMPTY_SMILES("Targets without SMILES");
 
-	@JsonValue
-	private String option;
+	private String label = "";
 
-	private TargetsDeleteOption() {
-		this.option = toString();
+	private TargetsDeleteOption(String label) {
+
+		this.label = label;
+	}
+
+	@Override
+	public String label() {
+
+		return label;
 	}
 }
