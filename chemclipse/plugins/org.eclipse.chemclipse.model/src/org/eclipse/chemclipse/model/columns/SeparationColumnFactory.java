3e30feec06f4098ade6cc47096acaed9c44546b3
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-11-16 11:51:22 +0100

    Revert "Avoid screaming caps in labels that are not abbreviations."

--- chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/columns/SeparationColumnFactory.java
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/columns/SeparationColumnFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2021 Lablicate GmbH.
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,18 +16,15 @@ import java.util.List;
 
 public class SeparationColumnFactory {
 
-	public static final String TYPE_DEFAULT = "Default";
-	public static final String TYPE_POLAR = "Polar";
-	public static final String TYPE_APOLAR = "Nonpolar";
-	public static final String TYPE_SEMI_POLAR = "Semipolar";
-	/*
-	 * Specific columns might be removed soon as basic types.
-	 * Mappings shall be added to group specific columns into
-	 * one of the three above classes.
-	 */
-	public static final String TYPE_DB1 = "DB-1";
-	public static final String TYPE_DB5 = "DB-5";
-	public static final String TYPE_DB1701 = "DB-1701";
+	public static final String TYPE_DEFAULT = "DEFAULT";
+	//
+	public static final String TYPE_POLAR = "POLAR";
+	public static final String TYPE_APOLAR = "APOLAR";
+	public static final String TYPE_SEMI_POLAR = "SEMI-POLAR";
+	//
+	public static final String TYPE_DB1 = "DB1";
+	public static final String TYPE_DB5 = "DB5";
+	public static final String TYPE_DB1701 = "DB1701";
 	public static final String TYPE_ZB1 = "ZB-1";
 	public static final String TYPE_WAX_PLUS = "WAX+";
 
