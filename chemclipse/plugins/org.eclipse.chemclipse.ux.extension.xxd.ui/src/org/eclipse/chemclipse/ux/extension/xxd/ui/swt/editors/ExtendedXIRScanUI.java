2db579638a01d464f5e7104cc9287c0a7e0cd8a4
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-11-18 10:43:14 +0100

    Fix interface name for IR spectroscopy X axis.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/editors/ExtendedXIRScanUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/editors/ExtendedXIRScanUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2020 Lablicate GmbH.
+ * Copyright (c) 2018, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -124,7 +124,7 @@ public class ExtendedXIRScanUI extends Composite implements IExtendedPartUI {
 			ySeries = new double[size];
 			int index = 0;
 			for(ISignalXIR scanSignal : scanXIR.getProcessedSignals()) {
-				xSeries[index] = scanSignal.getWavelength();
+				xSeries[index] = scanSignal.getWavenumber();
 				ySeries[index] = scanSignal.getIntensity();
 				index++;
 			}
