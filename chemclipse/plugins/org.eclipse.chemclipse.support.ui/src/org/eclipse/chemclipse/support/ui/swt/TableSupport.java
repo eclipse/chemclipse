9bcb5d3d733aca4ca4e63dccae19855b4710fe19
Philip Wenig <philip.wenig@openchrom.net>
2021-12-03 06:53:52 +0100

    Curly brackets have been added for a better readability.

--- chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/swt/TableSupport.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/swt/TableSupport.java
@@ -87,8 +87,10 @@ public class TableSupport {
 
 	private static int[] convertColumnInfo(String columnInfo) {
 
-		if(columnInfo.isEmpty())
+		if(columnInfo.isEmpty()) {
 			return new int[0];
+		}
+		//
 		String[] values = columnInfo.split(VALUE_DELIMITER);
 		int size = values.length;
 		int[] columns = new int[size];
