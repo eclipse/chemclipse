2d6720429adf0e86520bed7307c35caf714e3821
Matthias Mailänder <matthias.mailaender@lablicate.com>
2021-12-16 22:29:32 +0100

    Spelling.

--- chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/workbench/DisplayUtils.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/workbench/DisplayUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2020 Lablicate GmbH.
+ * Copyright (c) 2018, 2021 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -205,7 +205,7 @@ public class DisplayUtils {
 	public static Shell getShell() {
 
 		if(Display.getCurrent() == null) {
-			logger.error("Try to access shell outside of UI-Thread!");
+			logger.error("Tried to access shell outside of UI thread!");
 			Thread.dumpStack();
 			return null;
 		}
