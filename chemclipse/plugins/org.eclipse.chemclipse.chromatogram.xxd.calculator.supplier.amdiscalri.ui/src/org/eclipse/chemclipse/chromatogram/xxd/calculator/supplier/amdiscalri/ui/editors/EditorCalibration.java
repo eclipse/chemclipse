78f77df73749b01690235be7ac25a2fcbb5981ba
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-02-04 13:27:59 +0100

    Sort members.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri.ui/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/amdiscalri/ui/editors/EditorCalibration.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri.ui/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/amdiscalri/ui/editors/EditorCalibration.java
@@ -39,12 +39,12 @@ import org.osgi.service.event.EventHandler;
 
 public class EditorCalibration extends MultiPageEditorPart {
 
+	private static final Logger logger = Logger.getLogger(EditorCalibration.class);
+	//
 	private PageCalibration pageCalibration;
 	private File file;
 	private boolean isDirty = false;
 	private boolean initialize = true;
-	//
-	private static final Logger logger = Logger.getLogger(EditorCalibration.class);
 	//
 	private ISeparationColumnIndices separationColumnIndices;
 	//
@@ -52,13 +52,119 @@ public class EditorCalibration extends MultiPageEditorPart {
 	private List<Object> objects = new ArrayList<>();
 
 	@Override
-	protected void createPages() {
+	public void doSave(IProgressMonitor monitor) {
 
-		pageCalibration = new PageCalibration(getContainer());
-		int pageIndex = addPage(pageCalibration.getControl());
-		setPageText(pageIndex, "Retention Index Calibration (*.cal)");
-		registeredEventHandler = new ArrayList<>();
-		registerEvents();
+		CalibrationFileWriter calibrationFileWriter = new CalibrationFileWriter();
+		calibrationFileWriter.write(file, separationColumnIndices);
+		setDirty(false);
+	}
+
+	@Override
+	public void doSaveAs() {
+
+		FileDialog fileDialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+		fileDialog.setText("Save the *.cal file.");
+		fileDialog.setFilterExtensions(new String[]{"*.cal"});
+		fileDialog.setFilterNames(new String[]{"AMDIS Calibration *.cal"});
+		String pathRetentionIndexFile = fileDialog.open();
+		if(pathRetentionIndexFile != null) {
+			File file = new File(pathRetentionIndexFile);
+			CalibrationFileWriter calibrationFileWriter = new CalibrationFileWriter();
+			calibrationFileWriter.write(file, separationColumnIndices);
+			setDirty(false);
+		}
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+
+		super.init(site, input);
+		String fileName = input.getName();
+		fileName = fileName.substring(0, fileName.length() - 4);
+		setPartName(fileName);
+		if(input instanceof IFileEditorInput) {
+			IFileEditorInput fileEditorInput = (IFileEditorInput)input;
+			this.file = fileEditorInput.getFile().getLocation().toFile();
+		} else {
+			throw new PartInitException("The file could't be loaded.");
+		}
+	}
+
+	@Override
+	public boolean isDirty() {
+
+		return isDirty;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+
+		return true;
+	}
+
+	public void registerEvent(String topic, String property) {
+
+		registerEvent(topic, new String[]{property});
+	}
+
+	public void registerEvent(String topic, String[] properties) {
+
+		IEventBroker eventBroker = Activator.getDefault().getEventBroker();
+		if(eventBroker != null) {
+			registeredEventHandler.add(registerEventHandler(eventBroker, topic, properties));
+		}
+	}
+
+	public void registerEvents() {
+
+		registerEvent(IChemClipseEvents.TOPIC_RI_LIBRARY_UPDATE, IChemClipseEvents.EVENT_BROKER_DATA);
+	}
+
+	public void setDirty(boolean isDirty) {
+
+		this.isDirty = isDirty;
+		firePropertyChange(IEditorPart.PROP_DIRTY);
+	}
+
+	@Override
+	public void setFocus() {
+
+		if(initialize) {
+			initialize = false;
+			CalibrationFileReader calibrationFileReader = new CalibrationFileReader();
+			separationColumnIndices = calibrationFileReader.parse(file);
+			pageCalibration.showData(file, separationColumnIndices);
+		}
+	}
+
+	public void updateObjects(List<Object> objects) {
+
+		if(objects.size() == 1) {
+			Object object = objects.get(0);
+			if(object instanceof Object[]) {
+				Object[] array = (Object[])object;
+				if(array.length == 2) {
+					Object content = array[1];
+					if(content instanceof ISeparationColumnIndices) {
+						ISeparationColumnIndices separationColumnIndices = (ISeparationColumnIndices)content;
+						if(this.separationColumnIndices == separationColumnIndices) {
+							setDirty(separationColumnIndices.isDirty());
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@PreDestroy
+	private void preDestroy() {
+
+		IEventBroker eventBroker = Activator.getDefault().getEventBroker();
+		if(eventBroker != null) {
+			for(EventHandler eventHandler : registeredEventHandler) {
+				eventBroker.unsubscribe(eventHandler);
+			}
+		}
 	}
 
 	private EventHandler registerEventHandler(IEventBroker eventBroker, String topic, String[] properties) {
@@ -90,118 +196,12 @@ public class EditorCalibration extends MultiPageEditorPart {
 	}
 
 	@Override
-	public void doSave(IProgressMonitor monitor) {
+	protected void createPages() {
 
-		CalibrationFileWriter calibrationFileWriter = new CalibrationFileWriter();
-		calibrationFileWriter.write(file, separationColumnIndices);
-		setDirty(false);
-	}
-
-	@Override
-	public void doSaveAs() {
-
-		FileDialog fileDialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
-		fileDialog.setText("Save the *.cal file.");
-		fileDialog.setFilterExtensions(new String[]{"*.cal"});
-		fileDialog.setFilterNames(new String[]{"AMDIS Calibration *.cal"});
-		String pathRetentionIndexFile = fileDialog.open();
-		if(pathRetentionIndexFile != null) {
-			File file = new File(pathRetentionIndexFile);
-			CalibrationFileWriter calibrationFileWriter = new CalibrationFileWriter();
-			calibrationFileWriter.write(file, separationColumnIndices);
-			setDirty(false);
-		}
-	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-
-		return true;
-	}
-
-	@Override
-	public void setFocus() {
-
-		if(initialize) {
-			initialize = false;
-			CalibrationFileReader calibrationFileReader = new CalibrationFileReader();
-			separationColumnIndices = calibrationFileReader.parse(file);
-			pageCalibration.showData(file, separationColumnIndices);
-		}
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-
-		super.init(site, input);
-		String fileName = input.getName();
-		fileName = fileName.substring(0, fileName.length() - 4);
-		setPartName(fileName);
-		if(input instanceof IFileEditorInput) {
-			IFileEditorInput fileEditorInput = (IFileEditorInput)input;
-			this.file = fileEditorInput.getFile().getLocation().toFile();
-		} else {
-			throw new PartInitException("The file could't be loaded.");
-		}
-	}
-
-	public void registerEvent(String topic, String property) {
-
-		registerEvent(topic, new String[]{property});
-	}
-
-	public void registerEvent(String topic, String[] properties) {
-
-		IEventBroker eventBroker = Activator.getDefault().getEventBroker();
-		if(eventBroker != null) {
-			registeredEventHandler.add(registerEventHandler(eventBroker, topic, properties));
-		}
-	}
-
-	public void registerEvents() {
-
-		registerEvent(IChemClipseEvents.TOPIC_RI_LIBRARY_UPDATE, IChemClipseEvents.EVENT_BROKER_DATA);
-	}
-
-	public void updateObjects(List<Object> objects) {
-
-		if(objects.size() == 1) {
-			Object object = objects.get(0);
-			if(object instanceof Object[]) {
-				Object[] array = (Object[])object;
-				if(array.length == 2) {
-					Object content = array[1];
-					if(content instanceof ISeparationColumnIndices) {
-						ISeparationColumnIndices separationColumnIndices = (ISeparationColumnIndices)content;
-						if(this.separationColumnIndices == separationColumnIndices) {
-							setDirty(separationColumnIndices.isDirty());
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@Override
-	public boolean isDirty() {
-
-		return isDirty;
-	}
-
-	public void setDirty(boolean isDirty) {
-
-		this.isDirty = isDirty;
-		firePropertyChange(IEditorPart.PROP_DIRTY);
-	}
-
-	@PreDestroy
-	private void preDestroy() {
-
-		IEventBroker eventBroker = Activator.getDefault().getEventBroker();
-		if(eventBroker != null) {
-			for(EventHandler eventHandler : registeredEventHandler) {
-				eventBroker.unsubscribe(eventHandler);
-			}
-		}
+		pageCalibration = new PageCalibration(getContainer());
+		int pageIndex = addPage(pageCalibration.getControl());
+		setPageText(pageIndex, "Retention Index Calibration (*.cal)");
+		registeredEventHandler = new ArrayList<>();
+		registerEvents();
 	}
 }
