ce923209cecc15dd61cc279a9e07c0b7c1a9f428
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-02-16 17:00:51 +0100

    Fix target label settings not refreshing the chromatogram.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/actions/TargetLabelEditAction.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/actions/TargetLabelEditAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Lablicate GmbH.
+ * Copyright (c) 2020, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -152,6 +152,8 @@ public class TargetLabelEditAction extends Action {
 			baseChart.getPlotArea().removeCustomPaintListener(targetReferenceLabelMarker);
 			settingsWizardListener.setPreviewSettings(null);
 			if(settingsChanged) {
+				labelEditSettings.getChromatogramUI().update();
+				chromatogramChart.getBaseChart().redraw();
 				listeners.forEach(Runnable::run);
 			}
 		}
