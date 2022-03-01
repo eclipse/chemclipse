4834e663c27ae6cd0bcfe8abb82fd1515919b57f
Philip Wenig <philip.wenig@openchrom.net>
2022-03-01 07:20:04 +0100

    #921 - Cleaned up MassSpectrumIdentifier and SampleQuantProcessor
    After reviewing the code, the MassSpectrumIdentifier is not needed any
    longer. The PeakIdentifier has been refactored.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/internal/support/PeakIdentifier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/internal/support/PeakIdentifier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 Lablicate GmbH.
+ * Copyright (c) 2011, 2022 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -21,67 +21,54 @@ public class PeakIdentifier {
 
 	private static List<ISupplier> suppliers;
 
-	/**
-	 * Returns true if the file is a chromatogram as stored in enum
-	 * PeakSupplier. Returns false if not.
-	 * 
-	 * @param file
-	 * @return boolean
-	 */
 	public static boolean isPeak(File file) {
 
 		String extension = file.toString().toLowerCase();
-		String supplierExtension;
 		/*
 		 * All directories are stored in upper cases.
 		 */
 		if(file.isDirectory()) {
 			return false;
 		}
+		//
 		if(suppliers == null) {
 			suppliers = PeakConverterMSD.getPeakConverterSupport().getSupplier();
-		} else {
+		}
+		//
+		if(suppliers != null && !suppliers.isEmpty()) {
 			for(ISupplier supplier : suppliers) {
-				supplierExtension = supplier.getFileExtension().toLowerCase();
-				if(supplierExtension != "" && extension.endsWith(supplierExtension)) {
+				String supplierExtension = supplier.getFileExtension().toLowerCase();
+				if(!supplierExtension.isEmpty() && extension.endsWith(supplierExtension)) {
 					if(supplier.isImportable()) {
 						return true;
-					} else {
-						return false;
 					}
 				}
 			}
 		}
+		//
 		return false;
 	}
 
-	/**
-	 * Returns true if the file is a directory and a peak list. Returns false if
-	 * not.
-	 * 
-	 * @param file
-	 * @return boolean
-	 */
 	public static boolean isPeakDirectory(File file) {
 
 		String directory = file.toString().toUpperCase();
-		String directoryExtension;
 		/*
 		 * All directories are stored in upper cases.
 		 */
 		if(!file.isDirectory()) {
 			return false;
 		}
+		//
 		if(suppliers == null) {
 			suppliers = PeakConverterMSD.getPeakConverterSupport().getSupplier();
-		} else {
+		}
+		//
+		if(suppliers != null && !suppliers.isEmpty()) {
 			for(ISupplier supplier : suppliers) {
-				directoryExtension = supplier.getDirectoryExtension().toUpperCase();
-				if(directoryExtension != "" && directory.endsWith(directoryExtension)) {
+				String directoryExtension = supplier.getDirectoryExtension().toUpperCase();
+				if(!directoryExtension.isEmpty() && directory.endsWith(directoryExtension)) {
 					if(supplier.isImportable()) {
 						return true;
-					} else {
-						return false;
 					}
 				}
 			}
