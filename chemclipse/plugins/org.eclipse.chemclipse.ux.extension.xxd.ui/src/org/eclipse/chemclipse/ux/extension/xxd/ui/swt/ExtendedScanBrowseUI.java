34f472ff3a05c1f18f6c30c202567934ecceefea
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-03-22 22:11:40 +0100

    Code cleanup.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/ExtendedScanBrowseUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/ExtendedScanBrowseUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2021 Lablicate GmbH.
+ * Copyright (c) 2020, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -61,6 +61,7 @@ public class ExtendedScanBrowseUI extends Composite implements IExtendedPartUI {
 	private final ScanDataSupport scanDataSupport = new ScanDataSupport();
 
 	private enum Type {
+
 		EXTERNAL("Editor(s)"), //
 		INTERNAL("Reference(s)"), //
 		BOTH("Both"); //
@@ -358,7 +359,7 @@ public class ExtendedScanBrowseUI extends Composite implements IExtendedPartUI {
 		 */
 		Combo combo = comboViewerSource.getCombo();
 		comboViewerSource.setInput(chromatogramSelections);
-		if(chromatogramSelections.size() > 0) {
+		if(!chromatogramSelections.isEmpty()) {
 			combo.select(0);
 		}
 	}
