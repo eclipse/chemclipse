e4b25af1e6c680edfdcf894b7093adc37b472f75
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-05-04 14:33:13 +0200

    Don't ask multiple times on application exit.

--- chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/ApplicationWorkbenchWindowAdvisor.java
+++ chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/ApplicationWorkbenchWindowAdvisor.java
@@ -11,10 +11,6 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.rcp.app.ui;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -31,27 +27,5 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
 
 		return new ApplicationActionBarAdvisor(configurer);
-	}
-
-	@Override
-	public boolean preWindowShellClose() {
-
-		if(Activator.getDefault().saveDirtyParts()) {
-			Display display = Display.getCurrent();
-			Shell shell = display.getActiveShell();
-			if(shell == null) {
-				// we can't ask the user then
-				return true;
-			}
-			MessageBox messageBox = new MessageBox(shell, SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-			messageBox.setText("Close the application");
-			messageBox.setMessage("Do you want to close the application now?");
-			if(messageBox.open() == SWT.YES) {
-				return super.preWindowShellClose();
-			} else {
-				return false;
-			}
-		}
-		return false;
 	}
 }
