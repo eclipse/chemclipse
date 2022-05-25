3dd81f2ef2908f0912181277da072df57cfb41e3
Philip Wenig <philip.wenig@openchrom.net>
2022-05-25 22:12:29 +0200

    Fixed #1050 - Combined Scan Chromatogram Export Converter

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.amdis/src/org/eclipse/chemclipse/msd/converter/supplier/amdis/converter/msl/ChromatogramExportConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.amdis/src/org/eclipse/chemclipse/msd/converter/supplier/amdis/converter/msl/ChromatogramExportConverter.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2022 Lablicate GmbH.
- * 
+ * Copyright (c) 2022 Lablicate GmbH.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Dr. Philip Wenig - initial API and implementation
+ * Philip Wenig - initial API and implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.converter.supplier.amdis.converter.msl;
 
@@ -18,28 +18,35 @@ import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverte
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.model.core.IChromatogram;
 import org.eclipse.chemclipse.model.core.IPeak;
-import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
-import org.eclipse.chemclipse.msd.converter.supplier.amdis.internal.converter.SpecificationValidatorMSL;
-import org.eclipse.chemclipse.msd.converter.supplier.amdis.io.chromatogram.ChromatogramWriterMSL;
+import org.eclipse.chemclipse.msd.converter.io.IMassSpectraWriter;
+import org.eclipse.chemclipse.msd.converter.supplier.amdis.io.MSLWriter;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.msd.model.core.IScanMSD;
+import org.eclipse.chemclipse.msd.model.core.selection.ChromatogramSelectionMSD;
+import org.eclipse.chemclipse.msd.model.preferences.PreferenceSupplier;
+import org.eclipse.chemclipse.msd.model.support.CalculationType;
+import org.eclipse.chemclipse.msd.model.support.FilterSupport;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramPeakExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
-	private static final Logger logger = Logger.getLogger(ChromatogramPeakExportConverter.class);
-	private static final String DESCRIPTION = "NIST MSL Chromatogram Peak Export Converter";
+	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
+	private static final String DESCRIPTION = "Chromatogram Combined Scan Converter";
 
 	@Override
 	public IProcessingInfo<File> convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
-		file = SpecificationValidatorMSL.validateSpecification(file);
 		IProcessingInfo<File> processingInfo = super.validate(file);
 		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD) {
-			IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
-			IChromatogramMSDWriter writer = new ChromatogramWriterMSL();
 			try {
-				writer.writeChromatogram(file, chromatogramMSD, monitor);
+				IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
+				ChromatogramSelectionMSD chromatogramSelectionMSD = new ChromatogramSelectionMSD(chromatogramMSD);
+				boolean useNormalize = PreferenceSupplier.isUseNormalizedScan();
+				CalculationType calculationType = PreferenceSupplier.getCalculationType();
+				IScanMSD combinedMassSpectrum = FilterSupport.getCombinedMassSpectrum(chromatogramSelectionMSD, null, useNormalize, calculationType);
+				IMassSpectraWriter massSpectraWriter = new MSLWriter();
+				massSpectraWriter.write(file, combinedMassSpectrum, false, monitor);
 				processingInfo.setProcessingResult(file);
 			} catch(Exception e) {
 				logger.warn(e);
