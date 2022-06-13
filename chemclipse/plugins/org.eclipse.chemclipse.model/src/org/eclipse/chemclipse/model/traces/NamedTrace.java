394bcc43fcf99676061a0939d4c43e521e690d6d
Philip Wenig <philip.wenig@openchrom.net>
2022-06-13 10:51:01 +0200

    Removed trailing blank line.

--- chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/traces/NamedTrace.java
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/traces/NamedTrace.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Lablicate GmbH.
+ * Copyright (c) 2020, 2022 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -17,6 +17,7 @@ public class NamedTrace {
 	private String traces = "";
 
 	public NamedTrace(String identifier, String traces) {
+
 		this.identifier = identifier;
 		this.traces = traces;
 	}
