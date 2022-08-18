fbad454c5b66165307e931291575e8182a2c452a
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-08-18 11:10:06 +0200

    Don't deselect peaks when resetting the chromatogram chart.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/editors/ChromatogramResetHandler.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/editors/ChromatogramResetHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,6 +11,10 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.xxd.ui.swt.editors;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.model.selection.IChromatogramSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtchart.extensions.core.ScrollableChart;
@@ -21,17 +25,20 @@ public class ChromatogramResetHandler extends ResetChartHandler {
 	private ExtendedChromatogramUI extendedChromatogramUI;
 
 	public ChromatogramResetHandler(ExtendedChromatogramUI extendedChromatogramUI) {
+
 		this.extendedChromatogramUI = extendedChromatogramUI;
 	}
 
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	@Override
 	public void execute(Shell shell, ScrollableChart scrollableChart) {
 
 		super.execute(shell, scrollableChart);
 		IChromatogramSelection chromatogramSelection = extendedChromatogramUI.getChromatogramSelection();
 		if(chromatogramSelection != null) {
+			List<? extends IPeak> selectedPeaks = new ArrayList<>(chromatogramSelection.getSelectedPeaks());
 			chromatogramSelection.reset(true);
+			chromatogramSelection.setSelectedPeaks(selectedPeaks);
 		}
 	}
 }
