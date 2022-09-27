bef90ed8bc4ec265a137a713ba95a61e86083453
Philip Wenig <philip.wenig@openchrom.net>
2022-09-27 11:28:47 +0200

    Fixed #1149 - Sequence Support - match file extension

--- chemclipse/plugins/org.eclipse.chemclipse.converter/src/org/eclipse/chemclipse/converter/sequence/SequenceConverter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.converter/src/org/eclipse/chemclipse/converter/sequence/SequenceConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2020 Lablicate GmbH.
+ * Copyright (c) 2018, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -53,10 +53,12 @@ public class SequenceConverter {
 		SequenceConverterSupport sequenceConverterSupport = getSequenceConverterSupport();
 		for(ISupplier supplier : sequenceConverterSupport.getSupplier()) {
 			//
-			IProcessingInfo<ISequence<?>> processinInfo = convert(file, supplier.getId(), monitor);
-			ISequence<?> sequence = processinInfo.getProcessingResult();
-			if(sequence != null) {
-				return processinInfo;
+			if(file.getName().toLowerCase().endsWith(supplier.getFileExtension().toLowerCase())) {
+				IProcessingInfo<ISequence<?>> processinInfo = convert(file, supplier.getId(), monitor);
+				ISequence<?> sequence = processinInfo.getProcessingResult();
+				if(sequence != null) {
+					return processinInfo;
+				}
 			}
 		}
 		//
