052695a511c80ed9dd41d86c973b17deade02776
Philip Wenig <philip.wenig@openchrom.net>
2022-11-29 17:14:53 +0100

    Fixed #1199 - LibraryField - add options

--- chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/targets/LibraryField.java
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/targets/LibraryField.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2021 Lablicate GmbH.
+ * Copyright (c) 2020, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -19,6 +19,7 @@ import org.eclipse.chemclipse.model.identifier.ILibraryInformation;
 import org.eclipse.chemclipse.support.text.ILabel;
 
 public enum LibraryField implements ILabel {
+
 	NAME("Name", getLibraryExtractor(ILibraryInformation::getName)), //
 	CAS("CAS", getLibraryExtractor(ILibraryInformation::getCasNumber)), //
 	NAME_CAS("Name (CAS)", getLibraryExtractor(ILibraryInformation::getName, ILibraryInformation::getCasNumber)), //
@@ -26,6 +27,9 @@ public enum LibraryField implements ILabel {
 	SMILES("SMILES", getLibraryExtractor(ILibraryInformation::getSmiles)), //
 	SYNONYMS("Synonyms", getLibraryExtractor(ILibraryInformation::getSynonyms).andThen(elements -> elements != null ? String.join("; ", elements) : null)), //
 	INCHI("InChI", getLibraryExtractor(ILibraryInformation::getInChI)), //
+	INCHIKEY("InChIKey", getLibraryExtractor(ILibraryInformation::getInChIKey)), //
+	MOL_WEIGHT("Mol Weight", getLibraryExtractorDouble(ILibraryInformation::getMolWeight)), //
+	EXACT_MASS("Exact Mass", getLibraryExtractorDouble(ILibraryInformation::getExactMass)), //
 	REFID("RefID", getLibraryExtractor(ILibraryInformation::getReferenceIdentifier)), //
 	NAME_REFID("Name (RefID)", getLibraryExtractor(ILibraryInformation::getName, ILibraryInformation::getReferenceIdentifier)); //
 
@@ -80,6 +84,27 @@ public enum LibraryField implements ILabel {
 			}
 			//
 			return extractor.apply(libraryInformation);
+		};
+	}
+
+	private static <T> Function<IIdentificationTarget, String> getLibraryExtractorDouble(Function<ILibraryInformation, Double> extractor) {
+
+		return target -> {
+			/*
+			 * No target.
+			 */
+			if(target == null) {
+				return null;
+			}
+			/*
+			 * No library information.
+			 */
+			ILibraryInformation libraryInformation = target.getLibraryInformation();
+			if(libraryInformation == null) {
+				return null;
+			}
+			//
+			return Double.toString(extractor.apply(libraryInformation));
 		};
 	}
 
