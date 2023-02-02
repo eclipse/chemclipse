c5fa667bf65ed0e2f1712165e01171f939fdd56a
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-02-02 20:12:58 +0100

    Add an integration test with a minimal file.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzxml.fragment.test/src/org/eclipse/chemclipse/msd/converter/supplier/mzxml/TestPathHelper.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2023 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.mzxml;
+
+/**
+ * THIS CLASS IS NOT SUITED FOR PRODUCTIVE USE!<br/>
+ * IT IS A TESTCLASS!
+ */
+public class TestPathHelper extends PathResolver {
+
+	/*
+	 * IMPORT
+	 */
+	public static final String TESTFILE_IMPORT_TINY1_MZXML20 = "testData/files/import/tiny1.mzXML2.0.mzxml";
+}
