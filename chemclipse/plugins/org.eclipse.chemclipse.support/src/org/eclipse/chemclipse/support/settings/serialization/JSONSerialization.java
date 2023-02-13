0f6dbadad962f74da900170716a40e1b52533272
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-02-13 12:44:18 +0100

    Code cleanup

--- chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/settings/serialization/JSONSerialization.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support/src/org/eclipse/chemclipse/support/settings/serialization/JSONSerialization.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2021 Lablicate GmbH.
+ * Copyright (c) 2019, 2023 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -100,8 +100,7 @@ public class JSONSerialization implements SettingsSerialization {
 		Object[] serializationServices = Activator.getDefault().getSerializationServices();
 		if(serializationServices != null) {
 			for(Object object : serializationServices) {
-				if(object instanceof ISerializationService) {
-					ISerializationService serializationService = (ISerializationService)object;
+				if(object instanceof ISerializationService serializationService) {
 					Class clazz = serializationService.getSupportedClass();
 					simpleModule.addSerializer(clazz, serializationService.getSerializer());
 					simpleModule.addDeserializer(clazz, serializationService.getDeserializer());
