8c39f32db45720ce9a66f4fbfd10207c2f04a02b
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-03-06 13:49:48 +0100

    Don't reference missing files.

--- chemclipse/tests/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/rtshifter/TestPathHelper.java
+++ chemclipse/tests/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter.fragment.test/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/rtshifter/TestPathHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2021 Lablicate GmbH.
+ * Copyright (c) 2011, 2023 Lablicate GmbH.
  * 
  * All rights reserved. This
  * program and the accompanying materials are made available under the terms of
@@ -18,9 +18,4 @@ package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.rtshifter;
  * @author eselmeister
  */
 public class TestPathHelper extends PathResolver {
-
-	/*
-	 * IMPORT
-	 */
-	public static final String TESTFILE_IMPORT_CHROMATOGRAM = "testData/files/import/CHROMATOGRAM.D/DATA.MS";
 }
