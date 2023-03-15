d8b4e669f4f78a5b617de45918d477206765e54b
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-03-15 09:30:00 +0100

    Add an integration test.

---
+++ chemclipse/tests/org.eclipse.chemclipse.msd.converter.supplier.excel.fragment.test/src/org/eclipse/chemclipse/msd/converter/supplier/excel/io/ChromatogramExport_1_ITest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.msd.converter.supplier.excel.io;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.msd.converter.supplier.excel.TestPathHelper;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+public class ChromatogramExport_1_ITest extends ChromatogramReaderTestCase {
+
+	private ChromatogramWriter chromatogramWriter;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		pathImport = TestPathHelper.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_1);
+		chromatogramWriter = new ChromatogramWriter();
+		super.setUp();
+	}
+
+	public void testExport_1() {
+
+		try {
+			new File(TestPathHelper.DIRECTORY_EXPORT_TEST).mkdirs();
+			File file = new File(TestPathHelper.getAbsolutePath(TestPathHelper.DIRECTORY_EXPORT_TEST) + File.separator + "Test.xlsx");
+			chromatogramWriter.writeChromatogram(file, chromatogram, new NullProgressMonitor());
+			assertTrue(file.length() > 0);
+			file.delete();
+		} catch(Exception e) {
+			assertTrue("Exception", false);
+		}
+	}
+}
