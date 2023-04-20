05c8de0c3649f34c57e67d35f7c8ac75c869ea54
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-04-20 19:48:12 +0200

    Remove superfluous sysout calls.

--- chemclipse/plugins/org.eclipse.chemclipse.pcr.report.supplier.tabular.excel/src/org/eclipse/chemclipse/pcr/report/supplier/tabular/excel/preferences/PreferenceSupplier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.pcr.report.supplier.tabular.excel/src/org/eclipse/chemclipse/pcr/report/supplier/tabular/excel/preferences/PreferenceSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 Lablicate GmbH.
+ * Copyright (c) 2022, 2023 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -83,7 +83,6 @@ public class PreferenceSupplier implements IPreferenceSupplier {
 
 		IEclipsePreferences preferences = INSTANCE().getPreferences();
 		ChannelMappings channelMappings = new ChannelMappings();
-		System.out.println(preferences.get(P_CHANNEL_MAPPING, DEF_CHANNEL_MAPPING));
 		channelMappings.load(preferences.get(P_CHANNEL_MAPPING, DEF_CHANNEL_MAPPING));
 		return channelMappings;
 	}
