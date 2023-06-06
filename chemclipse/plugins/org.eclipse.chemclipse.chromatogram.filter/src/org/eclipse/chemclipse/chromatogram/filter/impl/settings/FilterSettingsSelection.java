fae73407dc1dee4a0232d2b19385d3e6e3e28fde
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-06-06 14:25:28 +0200

    Add an abundance range to the chromatogram selection filter.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.filter/src/org/eclipse/chemclipse/chromatogram/filter/impl/settings/FilterSettingsSelection.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.filter/src/org/eclipse/chemclipse/chromatogram/filter/impl/settings/FilterSettingsSelection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Lablicate GmbH.
+ * Copyright (c) 2017, 2023 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - abundance ranges
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.filter.impl.settings;
 
@@ -21,15 +22,34 @@ public class FilterSettingsSelection extends AbstractChromatogramFilterSettings 
 	@JsonProperty(value = "Start RT (Minutes)", defaultValue = "1")
 	@JsonPropertyDescription(value = "Start Retention Time (Minutes), the special value '-Infinity' can be used to express the minimum possible range")
 	private double startRetentionTimeMinutes;
+	//
 	@JsonProperty(value = "Use relative Start RT", defaultValue = "false")
 	@JsonPropertyDescription(value = "If checked, the start retention time is relative to the current selection")
 	private boolean startRelative;
+	//
+	@JsonProperty(value = "Minimum Intensity", defaultValue = "0")
+	@JsonPropertyDescription(value = "The lower Y-axis range limit")
+	private float startAbundance;
+	//
+	@JsonProperty(value = "Use relative intensity minimum", defaultValue = "true")
+	@JsonPropertyDescription(value = "If checked, the start intensity is a percentage relative to max intensity.")
+	private boolean startAbundanceRelative;
+	//
 	@JsonProperty(value = "Stop RT (Minutes)", defaultValue = "10")
 	@JsonPropertyDescription(value = "Stop Retention Time (Minutes), the special value 'Infinity' can be used to express the maximum possible range")
 	private double stopRetentionTimeMinutes;
+	//
 	@JsonProperty(value = "Use relative Stop RT", defaultValue = "false")
 	@JsonPropertyDescription(value = "If checked, the stop retention time is relative to the current selection")
 	private boolean stopRelative;
+	//
+	@JsonProperty(value = "Maximum Intensity", defaultValue = "100")
+	@JsonPropertyDescription(value = "The upper Y-axis range limit")
+	private float stopAbundance;
+	//
+	@JsonProperty(value = "Use relative intensity maximum", defaultValue = "true")
+	@JsonPropertyDescription(value = "If checked, the intensity is a percentage relative to max intensity.")
+	private boolean stopAbundanceRelative;
 
 	public double getStartRetentionTimeMinutes() {
 
@@ -69,5 +89,45 @@ public class FilterSettingsSelection extends AbstractChromatogramFilterSettings 
 	public void setStopRelative(boolean stopRelative) {
 
 		this.stopRelative = stopRelative;
+	}
+
+	public float getStartAbundance() {
+
+		return startAbundance;
+	}
+
+	public void setStartAbundance(float startAbundance) {
+
+		this.startAbundance = startAbundance;
+	}
+
+	public float getStopAbundance() {
+
+		return stopAbundance;
+	}
+
+	public void setStopAbundance(float stopAbundance) {
+
+		this.stopAbundance = stopAbundance;
+	}
+
+	public boolean isStartAbundanceRelative() {
+
+		return startAbundanceRelative;
+	}
+
+	public void setStartAbundanceRelative(boolean startAbundanceRelative) {
+
+		this.startAbundanceRelative = startAbundanceRelative;
+	}
+
+	public boolean isStopAbundanceRelative() {
+
+		return stopAbundanceRelative;
+	}
+
+	public void setStopAbundanceRelative(boolean stopAbundanceRelative) {
+
+		this.stopAbundanceRelative = stopAbundanceRelative;
 	}
 }
