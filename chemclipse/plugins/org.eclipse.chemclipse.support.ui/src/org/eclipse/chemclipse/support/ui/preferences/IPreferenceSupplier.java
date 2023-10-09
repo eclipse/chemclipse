d8e22ab17da941523a21e60f373f6f5bc0885923
Philip Wenig <philip.wenig@openchrom.net>
2023-10-09 15:39:57 +0200

    Fixed #1527 - Preference Supplier - add postfix option

--- chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/preferences/IPreferenceSupplier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.support.ui/src/org/eclipse/chemclipse/support/ui/preferences/IPreferenceSupplier.java
@@ -17,63 +17,78 @@ public interface IPreferenceSupplier {
 
 	IPreferenceStore getPreferences();
 
+	String getPostfix();
+
+	/*
+	 * Handle separate settings
+	 */
+	default String getKey(String key) {
+
+		String postfix = getPostfix();
+		if(postfix.isEmpty()) {
+			return key;
+		} else {
+			return key + postfix;
+		}
+	}
+
 	default boolean getBoolean(String key) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		return preferences.getBoolean(key);
+		return preferences.getBoolean(getKey(key));
 	}
 
 	default void setBoolean(String key, boolean value) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		preferences.setValue(key, value);
+		preferences.setValue(getKey(key), value);
 	}
 
 	default int getInteger(String key) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		return preferences.getInt(key);
+		return preferences.getInt(getKey(key));
 	}
 
 	default void setInteger(String key, int value) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		preferences.setValue(key, value);
+		preferences.setValue(getKey(key), value);
 	}
 
 	default float getFloat(String key) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		return preferences.getFloat(key);
+		return preferences.getFloat(getKey(key));
 	}
 
 	default void setFloat(String key, float value) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		preferences.setValue(key, value);
+		preferences.setValue(getKey(key), value);
 	}
 
 	default double getDouble(String key) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		return preferences.getDouble(key);
+		return preferences.getDouble(getKey(key));
 	}
 
 	default void setDouble(String key, double value) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		preferences.setValue(key, value);
+		preferences.setValue(getKey(key), value);
 	}
 
 	default String get(String key) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		return preferences.getString(key);
+		return preferences.getString(getKey(key));
 	}
 
 	default void set(String key, String value) {
 
 		IPreferenceStore preferences = this.getPreferences();
-		preferences.setValue(key, value);
+		preferences.setValue(getKey(key), value);
 	}
 }
