d8fb91601fd384b3985d5b7ad4aea4efe6b5ab8b
Philip Wenig <philip.wenig@openchrom.net>
2024-01-04 07:12:26 +0100

    Copyright header: class copied from AbstractSupplierFileExplorer.
    See commit:
    1ab1fd1d5250292c3c13c54273bedd43feb6a1b3

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/swt/MultiDataExplorerTreeUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/swt/MultiDataExplorerTreeUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2024 Lablicate GmbH.
+ * Copyright (c) 2013, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,8 +7,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Christoph Läubrich - initial API and implementation
- * Philip Wenig - refactoring
+ * Philip Wenig - initial API and implementation
+ * Christoph Läubrich - update data explorer handling
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.ui.swt;
 
