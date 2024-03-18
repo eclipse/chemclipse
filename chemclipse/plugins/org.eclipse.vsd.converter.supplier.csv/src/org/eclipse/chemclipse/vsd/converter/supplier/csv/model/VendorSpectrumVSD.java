674f8bc3d1ce670d2d0ab009d009c5f58ae21147
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-03-18 11:19:27 +0100

    Add a simple CSV import converter for FT-IR.

---
+++ chemclipse/plugins/org.eclipse.vsd.converter.supplier.csv/src/org/eclipse/chemclipse/vsd/converter/supplier/csv/model/VendorSpectrumVSD.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2024 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.vsd.converter.supplier.csv.model;
+
+import org.eclipse.chemclipse.vsd.model.implementation.SpectrumVSD;
+
+public class VendorSpectrumVSD extends SpectrumVSD implements IVendorSpectrumVSD {
+
+	private static final long serialVersionUID = 3530974485330576143L;
+}
