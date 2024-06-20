6bbbe14eeda252e3fa9afc26f84c0e54de776550
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-06-20 09:27:23 +0200

    Fix typos.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/internal/support/ChromatogramHeatmapData.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/internal/support/ChromatogramHeatmapData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2021 Lablicate GmbH.
+ * Copyright (c) 2018, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -70,7 +70,7 @@ public class ChromatogramHeatmapData {
 		return axisRangeHeight;
 	}
 
-	public void setAxisRangeHight(Range axisRangeHeight) {
+	public void setAxisRangeHeight(Range axisRangeHeight) {
 
 		this.axisRangeHeight = axisRangeHeight;
 	}
