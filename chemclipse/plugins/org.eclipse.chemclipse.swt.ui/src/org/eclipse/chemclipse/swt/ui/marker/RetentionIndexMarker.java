3a100f2bd742168ebcbbb67483e130311ce0e18a
Philip Wenig <philip.wenig@openchrom.net>
2024-07-08 13:06:24 +0200

    Fixed #1799 - Enable custom series for retention index marker
    Due to the hunge range offset in the primary axis data and plot area
    size, some scaling to fine tune the output is needed.

--- chemclipse/plugins/org.eclipse.chemclipse.swt.ui/src/org/eclipse/chemclipse/swt/ui/marker/RetentionIndexMarker.java
+++ chemclipse/plugins/org.eclipse.chemclipse.swt.ui/src/org/eclipse/chemclipse/swt/ui/marker/RetentionIndexMarker.java
@@ -21,20 +21,36 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swtchart.IAxis;
+import org.eclipse.swtchart.LineStyle;
 import org.eclipse.swtchart.Range;
 import org.eclipse.swtchart.extensions.core.BaseChart;
 import org.eclipse.swtchart.extensions.marker.AbstractBaseChartPaintListener;
 import org.eclipse.swtchart.extensions.marker.IBaseChartPaintListener;
+import org.eclipse.swtchart.extensions.model.ElementLine;
+import org.eclipse.swtchart.extensions.model.ElementRectangle;
+import org.eclipse.swtchart.extensions.model.ICustomSeries;
+import org.eclipse.swtchart.extensions.model.IElement;
+import org.eclipse.swtchart.extensions.model.TextElement;
+import org.eclipse.swtchart.extensions.support.ElementSupport;
+import org.eclipse.swtchart.extensions.support.PointPrimary;
+import org.eclipse.swtchart.extensions.support.RectanglePrimary;
 
 public class RetentionIndexMarker extends AbstractBaseChartPaintListener implements IBaseChartPaintListener {
 
 	private boolean showIdentifier = false;
 	private boolean showLabelTop = true;
 	private List<PositionMarker> positionMarkers = new ArrayList<>();
+	private ICustomSeries customSeries = null;
 
 	public RetentionIndexMarker(BaseChart baseChart) {
 
+		this(baseChart, "Retention Index Marker", "Currently used retention indices.");
+	}
+
+	public RetentionIndexMarker(BaseChart baseChart, String label, String description) {
+
 		super(baseChart);
+		this.customSeries = baseChart.createCustomSeries(label, description);
 	}
 
 	public boolean isShowIdentifier() {
@@ -71,10 +87,17 @@ public class RetentionIndexMarker extends AbstractBaseChartPaintListener impleme
 	@Override
 	public void paintControl(PaintEvent e) {
 
+		customSeries.clear();
 		if(isDraw()) {
-			for(PositionMarker positionMarker : positionMarkers) {
-				if(usePositionMarker(positionMarker)) {
-					drawMarker(e, positionMarker, showLabelTop);
+			BaseChart baseChart = getBaseChart();
+			IAxis axisX = baseChart.getAxisSet().getXAxis(BaseChart.ID_PRIMARY_X_AXIS);
+			IAxis axisY = baseChart.getAxisSet().getYAxis(BaseChart.ID_PRIMARY_Y_AXIS);
+			if(axisX != null && axisY != null) {
+				ElementSupport elementSupport = new ElementSupport(axisX.getRange(), axisY.getRange(), e.width, e.height);
+				for(PositionMarker positionMarker : positionMarkers) {
+					if(usePositionMarker(positionMarker)) {
+						drawMarker(e, positionMarker, elementSupport, showLabelTop);
+					}
 				}
 			}
 		}
@@ -85,57 +108,97 @@ public class RetentionIndexMarker extends AbstractBaseChartPaintListener impleme
 		return positionMarker.getRetentionTime() > 0;
 	}
 
-	private void drawMarker(PaintEvent e, PositionMarker positionMarker, boolean top) {
+	private void drawMarker(PaintEvent e, PositionMarker positionMarker, ElementSupport elementSupport, boolean top) {
 
 		int retentionTime = positionMarker.getRetentionTime();
 		int retentionIndex = positionMarker.getRetentionIndex();
+		/*
+		 * Current range
+		 */
+		Range rangeX = elementSupport.getRangeX();
+		double min = rangeX.lower;
+		double max = rangeX.upper;
 		//
-		BaseChart baseChart = getBaseChart();
-		IAxis axisX = baseChart.getAxisSet().getXAxis(BaseChart.ID_PRIMARY_X_AXIS);
-		if(axisX != null) {
-			/*
-			 * Current range
-			 */
-			Range range = axisX.getRange();
-			double min = range.lower;
-			double max = range.upper;
-			//
-			if(retentionTime >= min && retentionTime <= max) {
-				int width = e.width;
-				double rangeX = max - min + 1;
-				double percent = 1.0d / rangeX * (retentionTime - min);
-				int offset = (int)(width * percent);
-				if(offset > 0) {
-					/*
-					 * Settings
-					 */
-					GC gc = e.gc;
-					Color colorBackground = gc.getBackground();
-					Color colorForeground = gc.getForeground();
+		if(retentionTime >= min && retentionTime <= max) {
+			int width = e.width;
+			double widthX = max - min + 1;
+			double percent = 1.0d / widthX * (retentionTime - min);
+			int offset = (int)(width * percent);
+			if(offset > 0) {
+				/*
+				 * Settings
+				 */
+				GC gc = e.gc;
+				Color colorBackground = gc.getBackground();
+				Color colorForeground = gc.getForeground();
+				//
+				if(retentionIndex > 0) {
 					//
 					gc.setForeground(Colors.DARK_GRAY);
 					gc.setLineStyle(SWT.LINE_DASHDOT);
 					gc.drawLine(offset, 0, offset, e.height);
 					//
-					if(retentionIndex > 0) {
-						String label = showIdentifier ? positionMarker.getIdentifier() : Integer.toString(retentionIndex);
-						Point labelSize = gc.textExtent(label);
-						if(top) {
-							gc.setBackground(Colors.DARK_GRAY);
-							gc.fillRectangle(offset - 20, 15, 40, 25);
-							gc.setForeground(Colors.WHITE);
-							gc.drawText(label, offset - (int)(labelSize.x / 2.0d), 19, SWT.DRAW_TRANSPARENT);
-						} else {
-							gc.setBackground(Colors.DARK_GRAY);
-							gc.fillRectangle(offset - 20, e.height - 40, 40, 25);
-							gc.setForeground(Colors.WHITE);
-							gc.drawText(label, offset - (int)(labelSize.x / 2.0d), e.height - 35, SWT.DRAW_TRANSPARENT);
-						}
-					}
+					String label = showIdentifier ? positionMarker.getIdentifier() : Integer.toString(retentionIndex);
+					Point labelSize = gc.textExtent(label);
 					//
-					gc.setBackground(colorBackground);
-					gc.setForeground(colorForeground);
+					int offsetX = offset - 20;
+					int offsetY;
+					int boxWidth = 40;
+					int boxHeight = 25;
+					int labelX = offset - (int)(labelSize.x / 2.0d);
+					int labelY;
+					//
+					if(top) {
+						gc.setBackground(Colors.DARK_GRAY);
+						offsetY = 15;
+						gc.fillRectangle(offsetX, offsetY, boxWidth, boxHeight);
+						gc.setForeground(Colors.WHITE);
+						labelY = 19;
+						gc.drawText(label, labelX, labelY, SWT.DRAW_TRANSPARENT);
+					} else {
+						gc.setBackground(Colors.DARK_GRAY);
+						offsetY = e.height - 40;
+						gc.fillRectangle(offsetX, offsetY, boxWidth, boxHeight);
+						gc.setForeground(Colors.WHITE);
+						labelY = e.height - 35;
+						gc.drawText(label, labelX, labelY, SWT.DRAW_TRANSPARENT);
+					}
+					/*
+					 * Custom Series
+					 */
+					ElementRectangle elementRectangle = new ElementRectangle();
+					RectanglePrimary rectanglePrimary = elementSupport.convertRectangle(offsetX, offsetY, boxWidth, boxHeight);
+					elementRectangle.setX(rectanglePrimary.getX());
+					elementRectangle.setY(rectanglePrimary.getY());
+					elementRectangle.setWidth(rectanglePrimary.getWidth());
+					elementRectangle.setHeight(rectanglePrimary.getHeight() / 4); // TODO - fine tune scaling
+					elementRectangle.setColor(Colors.DARK_GRAY);
+					elementRectangle.setAlpha(255);
+					customSeries.getGraphicElements().add(elementRectangle);
+					//
+					ElementLine elementLine = new ElementLine();
+					elementLine.setX(retentionTime);
+					elementLine.setY(IElement.POSITION_TOP_Y);
+					elementLine.setX2(retentionTime);
+					elementLine.setY2(IElement.POSITION_BOTTOM_Y);
+					elementLine.setColor(Colors.DARK_GRAY);
+					elementLine.setAlpha(255);
+					elementLine.setLineStyle(LineStyle.DASHDOT);
+					elementLine.setLineWidth(2);
+					customSeries.getGraphicElements().add(elementLine);
+					//
+					TextElement textElement = new TextElement();
+					textElement.setLabel(label);
+					textElement.setColor(Colors.WHITE);
+					PointPrimary pointLabel = elementSupport.convertPoint(labelX, labelY);
+					textElement.setX(pointLabel.getX());
+					textElement.setY(pointLabel.getY() - rectanglePrimary.getHeight() / 16); // TODO - fine tune scaling
+					textElement.setRotation(0);
+					customSeries.getTextElements().add(textElement);
 				}
+				//
+				gc.setBackground(colorBackground);
+				gc.setForeground(colorForeground);
 			}
 		}
 	}
